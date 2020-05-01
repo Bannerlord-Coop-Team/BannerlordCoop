16f161af3b8172925b3207aaa5a88a9c781011f6
Araex <->
2020-05-01 11:50:08 +0200

    Documentation

--- source/Coop/Network/ConnectionBase.cs
+++ source/Coop/Network/ConnectionBase.cs
@@ -1,69 +1,71 @@
 ﻿using System;
 using System.IO;
+using JetBrains.Annotations;
 
 namespace Coop.Network
 {
     public enum EConnectionState
     {
+        /** [client side] Client is trying to establish a connection to a server.
+         *
+         * Possible transitions to:
+         * - ClientJoining: The server accepted the request.
+         * - Disconnecting: Request timeout or the server rejected the request.
+         */
         ClientJoinRequesting,
 
-        /** [client side] Client is trying to establish a connection to a server.
-                                 *
-                                 * Possible transitions to:
-                                 * - ClientJoining: The server accepted the request.
-                                 * - Disconnecting: Request timeout or the server rejected the request.
-                                 */
+        /** [client side] Client is joining a server, e.g. downloading data.
+         *
+         * Possible transitions to
+         * - ClientConnected:   Client successfully connected to the server.
+         * - Disconnecting:     Timeout.
+         */
         ClientAwaitingWorldData,
 
-        /** [client side] Client is joining a server, e.g. downloading data.
-                                 *
-                                 * Possible transitions to
-                                 * - ClientConnected:   Client successfully connected to the server.
-                                 * - Disconnecting:     Timeout.
-                                 */
+        /** [client side] Client is connected to a server.
+         *
+         * Possible transitions to:
+         * - Disconnecting:  Timeout or disconnect request (either server or client side).
+         */
         ClientConnected,
 
-        /** [client side] Client is connected to a server.
-                                 *
-                                 * Possible transitions to:
-                                 * - Disconnecting:  Timeout or disconnect request (either server or client side).
-                                 */
+        /** [server side] Server is awaiting a join request from a client.
+         *
+         * Possible transitions to:
+         * - ServerJoining: Join request from client received & approved.
+         * - Disconnecting:  Timeout or request denied.
+         */
         ServerAwaitingClient,
 
-        /** [server side] Server is awaiting a join request from a client.
-                                 *
-                                 * Possible transitions to:
-                                 * - ServerJoining: Join request from client received & approved.
-                                 * - Disconnecting:  Timeout or request denied.
-                                 */
+        /** [server side] Client is joining the server.
+         *
+         * Possible transitions to:
+         * - ServerConnected: Join request from client received & approved.
+         * - Disconnecting:    Timeout or request denied.
+         */
         ServerSendingWorldData,
 
-        /** [server side] Client is joining the server.
-                                 *
-                                 * Possible transitions to:
-                                 * - ServerConnected: Join request from client received & approved.
-                                 * - Disconnecting:    Timeout or request denied.
-                                 */
+        /** [server side] Client is connected to the server.
+         *
+         * Possible transitions to:
+         * - Disconnecting:  Timeout or disconnect request (either server or client side).
+         */
         ServerConnected,
 
-        /** [server side] Client is connected to the server.
-                                 *
-                                 * Possible transitions to:
-                                 * - Disconnecting:  Timeout or disconnect request (either server or client side).
-                                 */
+        /** Connection is being closed.
+         *
+         * Possible transitions to:
+         * - Disconnected:  Connection was closed.
+         */
         Disconnecting,
 
-        /** Connection is being closed.
-                                 *
-                                 * Possible transitions to:
-                                 * - Disconnected:  Connection was closed.
-                                 */
-        Disconnected /** Connection is inactive.
-                                 *
-                                 * Possible transitions to:
-                                 * - ClientJoinRequesting:  [client side] Client wants to establish a connection to a server.
-                                 * - ServerAwaitingClient:  [server side] Client wants to establish a connection to a server.
-                                 */
+        /** Connection is inactive.
+         *
+         * Possible transitions to:
+         * - ClientJoinRequesting:  [client side] Client wants to establish a connection to a server.
+         * - ServerAwaitingClient:  [server side] Client wants to establish a connection to a server.
+         */
+        Disconnected
     }
 
     /// <summary>
@@ -78,11 +80,12 @@ namespace Coop.Network
         /// <summary>
         /// </summary>
         /// <param name="network">Networking implementation.</param>
-        protected ConnectionBase(INetworkConnection network, IGameStatePersistence persistence)
+        /// <param name="persistence">Implementation to relay Persistance packets to.</param>
+        protected ConnectionBase([NotNull] INetworkConnection network, [NotNull] IGameStatePersistence persistence)
         {
             Dispatcher = new PacketDispatcher();
-            Network = network;
-            GameStatePersistence = persistence;
+            Network = network ?? throw new ArgumentNullException(nameof(network));
+            GameStatePersistence = persistence ?? throw new ArgumentNullException(nameof(persistence));
         }
 
         #region Send and Receive
@@ -127,26 +130,25 @@ namespace Coop.Network
 
         public void Receive(ArraySegment<byte> buffer)
         {
-            if (State != EConnectionState.Disconnected)
-            {
-                Protocol.EPacket eType = PacketReader.DecodePacketType(buffer.Array[buffer.Offset]);
-                if (eType == Protocol.EPacket.Persistence)
-                {
-                    GameStatePersistence.Receive(buffer);
-                }
-                else
-                {
-                    if (m_PackageReader == null)
-                    {
-                        m_PackageReader = new PacketReader();
-                    }
+            if (State == EConnectionState.Disconnected) return;
 
-                    Packet packet = m_PackageReader.Read(new ByteReader(buffer));
-                    if (packet != null)
-                    {
-                        m_PackageReader = null;
-                        Dispatcher.Dispatch(State, packet);
-                    }
+            Protocol.EPacket eType = PacketReader.DecodePacketType(buffer.Array[buffer.Offset]);
+            if (eType == Protocol.EPacket.Persistence)
+            {
+                GameStatePersistence.Receive(buffer);
+            }
+            else
+            {
+                if (m_PackageReader == null)
+                {
+                    m_PackageReader = new PacketReader();
+                }
+
+                Packet packet = m_PackageReader.Read(new ByteReader(buffer));
+                if (packet != null)
+                {
+                    m_PackageReader = null;
+                    Dispatcher.Dispatch(State, packet);
                 }
             }
         }
