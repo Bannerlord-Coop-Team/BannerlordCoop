ef9bcd34d67ab72e823261073151beac12bc8c77
Araex <3930554+araex@users.noreply.github.com>
2020-07-11 11:13:18 +0200

    Moved store setup for tests into separate class

--- source/Coop.Tests/Sync/SharedRemoteStore_Test.cs
+++ source/Coop.Tests/Sync/SharedRemoteStore_Test.cs
@@ -1,172 +1,132 @@
-﻿using System.Collections.Generic;
-using Network.Infrastructure;
-using Sync.Store;
+﻿using Sync.Store;
 using Xunit;
 
 namespace Coop.Tests.Sync
 {
     public class SharedRemoteStore_Test
     {
-        private readonly List<ConnectionTestImpl> m_ConnectionsClient =
-            new List<ConnectionTestImpl>();
-
-        private readonly List<RemoteStore> m_StoresClient = new List<RemoteStore>();
-
-        private readonly List<ConnectionTestImpl> m_ConnectionsServer =
-            new List<ConnectionTestImpl>();
-
-        private readonly SharedRemoteStore m_StoreServer = new SharedRemoteStore();
-
-        private void Init(int iNumberOfClients)
-        {
-            for (int i = 0; i < iNumberOfClients; ++i)
-            {
-                ConnectionTestImpl client = new ConnectionTestImpl
-                {
-                    StateImpl = EConnectionState.ClientPlaying
-                };
-                ConnectionTestImpl server = new ConnectionTestImpl
-                {
-                    StateImpl = EConnectionState.ServerPlaying
-                };
-
-                client.NetworkImpl.OnSend += server.Receive;
-                server.NetworkImpl.OnSend += client.Receive;
-                m_StoresClient.Add(new RemoteStore(new Dictionary<ObjectId, object>(), client));
-                m_StoreServer.AddConnection(server);
-
-                m_ConnectionsClient.Add(client);
-                m_ConnectionsServer.Add(server);
-            }
-        }
-
-        private void ExecuteSendsClients()
-        {
-            m_ConnectionsClient.ForEach(c => c.NetworkImpl.ExecuteSends());
-        }
-
-        private void ExecuteSendsServer()
-        {
-            m_ConnectionsServer.ForEach(c => c.NetworkImpl.ExecuteSends());
-        }
+        private readonly TestStores m_TestStores = new TestStores();
 
         [Fact]
         private void DataIsBroadcastToOtherClients()
         {
             string message = "Hello World";
-            Init(2);
+            m_TestStores.Init(2);
 
-            RemoteStore client0 = m_StoresClient[0];
-            RemoteStore client1 = m_StoresClient[1];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
+            RemoteStore client1 = m_TestStores.StoresClient[1];
 
             ObjectId id = client0.Insert(message);
             Assert.Contains(id, client0.Data);
             Assert.DoesNotContain(id, client1.Data);
 
             // Client0 Add -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.DoesNotContain(id, client1.Data);
-            Assert.Contains(id, m_StoreServer.Data);
-            Assert.IsType<string>(m_StoreServer.Data[id]);
-            Assert.Equal(message, m_StoreServer.Data[id] as string);
+            Assert.Contains(id, m_TestStores.StoreServer.Data);
+            Assert.IsType<string>(m_TestStores.StoreServer.Data[id]);
+            Assert.Equal(message, m_TestStores.StoreServer.Data[id] as string);
 
             // Server Add -> Client1
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
             Assert.Contains(id, client1.Data);
             Assert.IsType<string>(client1.Data[id]);
             Assert.Equal(message, client1.Data[id] as string);
 
             // Client1 ACK -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.False(client0.State[id].Acknowledged);
             // Server ACK -> Client 0
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
             Assert.True(client0.State[id].Acknowledged);
 
             // Object is present and equal in all stores
             Assert.Equal(message, client0.Data[id] as string);
             Assert.Equal(message, client1.Data[id] as string);
-            Assert.Equal(message, m_StoreServer.Data[id] as string);
+            Assert.Equal(message, m_TestStores.StoreServer.Data[id] as string);
         }
 
         [Fact]
         private void DataIsReceivedFromClient()
         {
             string message = "Hello World";
-            Init(1);
+            m_TestStores.Init(1);
 
-            RemoteStore client0 = m_StoresClient[0];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
 
             ObjectId id = client0.Insert(message);
-            Assert.DoesNotContain(id, m_StoreServer.Data);
+            Assert.DoesNotContain(id, m_TestStores.StoreServer.Data);
 
-            ExecuteSendsClients();
-            Assert.Contains(id, m_StoreServer.Data);
-            Assert.IsType<string>(m_StoreServer.Data[id]);
-            Assert.Equal(message, m_StoreServer.Data[id] as string);
+            m_TestStores.ExecuteSendsClients();
+            Assert.Contains(id, m_TestStores.StoreServer.Data);
+            Assert.IsType<string>(m_TestStores.StoreServer.Data[id]);
+            Assert.Equal(message, m_TestStores.StoreServer.Data[id] as string);
         }
 
         [Fact]
         private void OnDistributedIsInvoked()
         {
             string message = "Hello World";
-            Init(2);
+            m_TestStores.Init(2);
 
-            RemoteStore client0 = m_StoresClient[0];
-            RemoteStore client1 = m_StoresClient[1];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
+            RemoteStore client1 = m_TestStores.StoresClient[1];
 
             ObjectId id = client0.Insert(message);
 
             ObjectId? handlerArgument = null;
-            m_StoreServer.OnObjectDistributed += objectId => { handlerArgument = objectId; };
+            m_TestStores.StoreServer.OnObjectDistributed += objectId =>
+            {
+                handlerArgument = objectId;
+            };
 
             // Client0 Add -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.False(handlerArgument.HasValue);
 
             // Server Add -> Client1
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
             Assert.False(handlerArgument.HasValue);
 
             // Client1 ACK -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.True(handlerArgument.HasValue);
 
             // Server ACK -> Client 0
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
         }
 
         [Fact]
         private void ServerAckIsDelayedWithMultipleClients()
         {
             string message = "Hello World";
-            Init(2);
+            m_TestStores.Init(2);
 
-            RemoteStore client0 = m_StoresClient[0];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
             ObjectId id = client0.Insert(message);
             Assert.True(client0.State[id].Sent);
             Assert.False(client0.State[id].Acknowledged);
 
             // Client0 Add -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.False(client0.State[id].Acknowledged);
 
             // Server Add -> Client1
-            ExecuteSendsServer();
-            RemoteStore client1 = m_StoresClient[1];
+            m_TestStores.ExecuteSendsServer();
+            RemoteStore client1 = m_TestStores.StoresClient[1];
             Assert.Contains(id, client1.State);
             Assert.False(
                 client0.State[id]
                        .Acknowledged); // Delayed until client 1 ACK is processed by server
 
             // Client1 ACK -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.False(
                 client0.State[id]
                        .Acknowledged); // Delayed until client 1 ACK is processed by server
 
             // Server ACK -> Client 0
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
             Assert.True(
                 client0.State[id]
                        .Acknowledged); // Delayed until client 1 ACK is processed by server
@@ -176,18 +136,18 @@ namespace Coop.Tests.Sync
         private void ServerCanAddObject()
         {
             string message = "Hello World";
-            Init(2);
+            m_TestStores.Init(2);
 
-            RemoteStore client0 = m_StoresClient[0];
-            RemoteStore client1 = m_StoresClient[1];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
+            RemoteStore client1 = m_TestStores.StoresClient[1];
 
-            ObjectId id = m_StoreServer.Insert(message);
-            Assert.Contains(id, m_StoreServer.Data);
+            ObjectId id = m_TestStores.StoreServer.Insert(message);
+            Assert.Contains(id, m_TestStores.StoreServer.Data);
             Assert.DoesNotContain(id, client0.Data);
             Assert.DoesNotContain(id, client1.Data);
 
-            ExecuteSendsServer();
-            Assert.Contains(id, m_StoreServer.Data);
+            m_TestStores.ExecuteSendsServer();
+            Assert.Contains(id, m_TestStores.StoreServer.Data);
             Assert.Contains(id, client0.Data);
             Assert.Contains(id, client1.Data);
         }
@@ -196,19 +156,19 @@ namespace Coop.Tests.Sync
         private void ServerSendsAckWithOneClient()
         {
             string message = "Hello World";
-            Init(1);
+            m_TestStores.Init(1);
 
-            RemoteStore client0 = m_StoresClient[0];
+            RemoteStore client0 = m_TestStores.StoresClient[0];
             ObjectId id = client0.Insert(message);
             Assert.True(client0.State[id].Sent);
             Assert.False(client0.State[id].Acknowledged);
 
             // Client0 Add -> Server
-            ExecuteSendsClients();
+            m_TestStores.ExecuteSendsClients();
             Assert.False(client0.State[id].Acknowledged);
 
             // Server ACK -> Client0
-            ExecuteSendsServer();
+            m_TestStores.ExecuteSendsServer();
             Assert.True(client0.State[id].Acknowledged);
         }
     }
---
+++ source/Coop.Tests/Sync/TestStores.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Network.Infrastructure;
+using Sync.Store;
+
+namespace Coop.Tests.Sync
+{
+    public class TestStores
+    {
+        public List<ConnectionTestImpl> ConnectionsClient { get; } = new List<ConnectionTestImpl>();
+
+        public List<RemoteStore> StoresClient { get; } = new List<RemoteStore>();
+
+        public List<ConnectionTestImpl> ConnectionsServer { get; } = new List<ConnectionTestImpl>();
+
+        public SharedRemoteStore StoreServer { get; } = new SharedRemoteStore();
+
+        public void Init(int iNumberOfClients)
+        {
+            for (int i = 0; i < iNumberOfClients; ++i)
+            {
+                ConnectionTestImpl client = new ConnectionTestImpl
+                {
+                    StateImpl = EConnectionState.ClientPlaying
+                };
+                ConnectionTestImpl server = new ConnectionTestImpl
+                {
+                    StateImpl = EConnectionState.ServerPlaying
+                };
+
+                client.NetworkImpl.OnSend += server.Receive;
+                server.NetworkImpl.OnSend += client.Receive;
+                StoresClient.Add(new RemoteStore(new Dictionary<ObjectId, object>(), client));
+                StoreServer.AddConnection(server);
+
+                ConnectionsClient.Add(client);
+                ConnectionsServer.Add(server);
+            }
+        }
+
+        public void ExecuteSendsClients()
+        {
+            ConnectionsClient.ForEach(c => c.NetworkImpl.ExecuteSends());
+        }
+
+        public void ExecuteSendsServer()
+        {
+            ConnectionsServer.ForEach(c => c.NetworkImpl.ExecuteSends());
+        }
+    }
+}
