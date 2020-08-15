e94d28e31657ee7afc3c1502f3dbacf5412e9300
garrettluskey <garrett.luskey@gmail.com>
2020-08-15 08:56:16 -0500

    Initial commit

---
+++ source/Coop/Mod/Serializers/CustomSerializers/TraitObjectSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.ObjectSystem;
+
+namespace Coop.Mod.Serializers
+{
+    [Serializable]
+    class TraitObjectSerializer : CustomSerializer
+    {
+        string StringId;
+        public TraitObjectSerializer(MBObjectBase obj) : base(obj)
+        {
+            StringId = obj.StringId;
+        }
+
+        public override object Deserialize()
+        {
+
+            TraitObject newTraitObject = new TraitObject(StringId);
+            return base.Deserialize(newTraitObject);
+        }
+    }
+}
--- source/Coop/Mod/Serializers/SerializableFactory.cs
+++ source/Coop/Mod/Serializers/SerializableFactory.cs
@@ -1,5 +1,13 @@
-﻿using Sync.Store;
+﻿﻿using Sync.Store;
 using TaleWorlds.CampaignSystem;
+using Sync.Store;
+using System;
+using System.Runtime.Remoting;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.ObjectSystem;
 
 namespace Coop.Mod.Serializers
 {
@@ -14,6 +22,56 @@ namespace Coop.Mod.Serializers
             {
                 case TroopRoster troop:
                     return new TroopRosterSerializer(troop);
+                case Banner banner:
+                    return new BannerSerializer(banner);
+                case CampaignTime campaignTime:
+                    return new CampaignTimeSerializer(campaignTime);
+                case CharacterObject characterObject:
+                    return new CharacterObjectSerializer(characterObject);
+                case Clan clan:
+                    return new ClanSerializer(clan);
+                case CultureObject cultureObject:
+                    return new CultureObjectSerializer(cultureObject);
+                case DeterministicRandom deterministicRandom:
+                    return new DeterministicRandomSerializer(deterministicRandom);
+                case EquipmentElement equipmentElement:
+                    return new EquipmentElementSerializer(equipmentElement);
+                case Equipment equipment:
+                    return new EquipmentSerializer(equipment);
+                case HeroDeveloper heroDeveloper:
+                    return new HeroDeveloperSerializer(heroDeveloper);
+                case Hero.HeroLastSeenInformation heroLastSeenInfo:
+                    return new HeroLastSeenInformationSerializer(heroLastSeenInfo);
+                case Hero hero:
+                    return new HeroSerializer(hero);
+                case ItemRoster itemRoster:
+                    return new ItemRosterSerializer(itemRoster);
+                case MBGUID mbguid:
+                    return new MBGUIDSerializer(mbguid);
+                case MobilePartiesAroundPositionList partiesAroundPosition:
+                    return new MobilePartiesAroundPositionListSerializer(partiesAroundPosition);
+                case MobileParty mobileParty:
+                    return new MobilePartySerializer(mobileParty);
+                case NavigationPath navPath:
+                    return new NavigationPathSerializer(navPath);
+                case PartyBase partyBase:
+                    return new PartyBaseSerializer(partyBase);
+                case PathFaceRecord pathFaceRecord:
+                    return new PathFaceRecordSerializer(pathFaceRecord);
+                case Settlement settlement:
+                    return new SettlementSerializer(settlement);
+                case TraitObject traitObject:
+                    return new TraitObjectSerializer(traitObject);
+                case TroopRoster troopRoster:
+                    return new TroopRosterSerializer(troopRoster);
+                case CharacterFeats characterFeats:
+                    return new CharacterFeatsSerializer(characterFeats);
+                case CharacterPerks characterPerks:
+                    return new CharacterPerksSerializer(characterPerks);
+                case CharacterSkills characterSkills:
+                    return new CharacterSkillsSerializer(characterSkills);
+                case CharacterTraits characterTraits:
+                    return new CharacterTraitsSerializer(characterTraits);
                 default:
                     return obj;
             }
