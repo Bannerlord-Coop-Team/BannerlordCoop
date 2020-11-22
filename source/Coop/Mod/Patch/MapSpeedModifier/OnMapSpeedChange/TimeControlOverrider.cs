b08920467c0cc4d5373d7ac623b236d1c3e02f01
Ignacio Fernández Soto <igfernandezsoto@gmail.com>
2020-11-22 19:34:42 -0300

    ISSUE-108 Fixed cr issues

--- source/Coop/Mod/Patch/MapSpeedModifier/OnMapSpeedChange/TimeControlOverrider.cs
+++ source/Coop/Mod/Patch/MapSpeedModifier/OnMapSpeedChange/TimeControlOverrider.cs
@@ -3,6 +3,19 @@ using TaleWorlds.CampaignSystem;
 
 namespace Coop.Mod.Patch.MapSpeedModifier.OnMapSpeedChange
 {
+
+    /// <summary>
+    ///     <para>
+    ///         Code patcher through Harmony which overrides new campaign map time speed on it's setter.
+    ///     </para>
+    ///     <para>
+    ///         More about it on <see href="https://github.com/Bannerlord-Coop-Team/BannerlordCoop/issues/108">issue #108</see>
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     This class prefixes setter <c>Campaign::TimeControlMode::set</c> to override it's new value 
+    ///     as <c>MapSpeedResolver::Resolve</c> sees fit.
+    /// </remarks>
     [HarmonyPatch(typeof(Campaign))]
     [HarmonyPatch(nameof(Campaign.TimeControlMode), MethodType.Setter)]
     class TimeControlOverrider
@@ -17,4 +30,5 @@ namespace Coop.Mod.Patch.MapSpeedModifier.OnMapSpeedChange
         }
 
     }
+
 }
--- source/Coop/Mod/Patch/MapSpeedModifier/OnTerrainClick/MapSpeedOnTerrainClickOverrider.cs
+++ source/Coop/Mod/Patch/MapSpeedModifier/OnTerrainClick/MapSpeedOnTerrainClickOverrider.cs
@@ -4,6 +4,19 @@ using TaleWorlds.CampaignSystem;
 
 namespace Coop.Mod.Patch.MapSpeedModifier.OnTerrainClick
 {
+
+    /// <summary>
+    ///     <para>
+    ///         Code patcher through Harmony which patches campaign map time speed on terrain click.
+    ///     </para>
+    ///     <para>
+    ///         More about it on <see href="https://github.com/Bannerlord-Coop-Team/BannerlordCoop/issues/108">issue #108</see>
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     This class prefixes private method <c>MapScreen::OnTerrainClick</c> to replace current <c>TimeControlMode</c> 
+    ///     as <c>MapSpeedResolver::Resolve</c> sees fit.
+    /// </remarks>
     [HarmonyPatch(typeof(MapScreen))]
     [HarmonyPatch("OnTerrainClick")]
     class MapSpeedOnTerrainClickOverrider
@@ -18,4 +31,5 @@ namespace Coop.Mod.Patch.MapSpeedModifier.OnTerrainClick
         }
 
     }
+
 }
