f68cc0a5764c11763c43e434e7e7ad97e58a8573
Diego Miranda Gipoulou <diego.miranda.g@usach.cl>
2021-01-03 00:27:37 -0300

    Time is not being paused when entering a settlement

---
+++ source/Coop/Mod/Patch/CampaignPatches/TimeControlModePatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using Sync;
+using TaleWorlds.CampaignSystem;
+
+namespace Coop.Mod.Patch.CampaignPatches
+{
+
+    /// <summary>
+    ///     <para>
+    ///         Code patcher through Harmony which overrides new campaign map time speed on it's setter.
+    ///     </para>
+    ///     <para>
+    ///         More about it on <see href="https://github.com/Bannerlord-Coop-Team/BannerlordCoop/issues/108">issue #108</see>
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     This class prefixes setter <c>Campaign::TimeControlMode::set</c> to override it's new value 
+    ///     as <c>MapSpeedResolver::Resolve</c> sees fit.
+    /// </remarks>
+    [HarmonyPatch(typeof(Campaign), nameof(Campaign.TimeControlMode))]
+    static class TimeControlModePatch
+    {
+        /// <summary>
+        /// Grants access to the private field <c>TaleWorlds.CampaignSystem.Campaign._timeControlMode</c> to override the setter logic.
+        /// </summary>
+        static AccessTools.FieldRef<Campaign, CampaignTimeControlMode> _timeControlModeRef = AccessTools.FieldRefAccess<Campaign, CampaignTimeControlMode>("_timeControlMode");
+
+        /// <summary>
+        /// Overrides the <c>TaleWorlds.CampaignSystem.Campaign.TimeControlMode</c> setter logic
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Always false to skip original method call</returns>
+        [HarmonyPrefix]
+        [HarmonyPatch(MethodType.Setter)]
+        static bool Prefix(Campaign __instance, ref CampaignTimeControlMode value)
+        {
+            if (__instance != null && !__instance.TimeControlModeLock && 
+                value != __instance.TimeControlMode && TimeControl.CanSyncTimeControlMode)
+            {
+                _timeControlModeRef(__instance) = value;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ source/Coop/Mod/Patch/MobilePartyPatches/ComputeIsWaitingPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+
+namespace Coop.Mod.Patch.MobilePartyPatches
+{
+    [HarmonyPatch(typeof(MobileParty), nameof(MobileParty.ComputeIsWaiting))]
+    class ComputeIsWaitingPatch
+    {
+
+        [HarmonyPrefix]
+        static bool Prefix(MobileParty __instance, ref bool __result)
+        {
+            if (__instance.IsMainParty)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
