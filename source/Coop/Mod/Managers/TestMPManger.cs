12c9dad733469d271aa45e2892ec1ccada66c1ac
araex <3930554+araex@users.noreply.github.com>
2021-01-04 13:48:22 +0100

    added missing files

---
+++ source/Coop/Mod/Managers/TestMPManger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.Missions;
+
+namespace Coop.Mod.Managers
+{
+    class TestMPManger : MultiplayerGameManager
+    {
+
+        public override void OnGameLoaded(Game game, object initializerObject)
+        {
+            base.OnGameLoaded(game, initializerObject);
+            MultiplayerMissions.OpenFreeForAllMission("tutorial_training_field");
+        }
+    }
+}
