6490027a1c031ba63bd361a76fb40f43039ff926
araex <3930554+araex@users.noreply.github.com>
2021-02-27 16:52:57 +0100

    equals for FieldId

--- source/Sync/Value/FieldId.cs
+++ source/Sync/Value/FieldId.cs
@@ -22,5 +22,35 @@
         {
             return $"FieldId {InternalValue}";
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FieldId f)
+            {
+                return Equals(f);
+            }
+
+            return false;
+        }
+
+        public bool Equals(FieldId other)
+        {
+            return InternalValue == other.InternalValue;
+        }
+
+        public override int GetHashCode()
+        {
+            return InternalValue;
+        }
+
+        public static bool operator ==(FieldId f0, FieldId f1)
+        {
+            return f0.Equals(f1);
+        }
+
+        public static bool operator !=(FieldId f0, FieldId f1)
+        {
+            return !(f0 == f1);
+        }
     }
 }
