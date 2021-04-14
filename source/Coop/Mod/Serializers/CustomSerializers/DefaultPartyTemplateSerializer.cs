059b024125b933a0442f1989e7d514c9ff951de6
WesKrug <wesley.krug@salespad.net>
2021-04-13 20:41:46 -0400

    Add defaultTemplateSerializer

---
+++ source/Coop/Mod/Serializers/CustomSerializers/DefaultPartyTemplateSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using System.Reflection;
+
+namespace Coop.Mod.Serializers
+{
+    [Serializable]
+    public class DefaultPartyTemplateSerializer : ICustomSerializer
+    {
+        private string partyTemplateId;
+        public DefaultPartyTemplateSerializer(PartyTemplateObject partyTemplate)
+        {
+
+            if (partyTemplate != null)
+            {
+                partyTemplateId = partyTemplate.StringId;
+            }
+        }
+
+        public object Deserialize()
+        {
+            if (partyTemplateId != null)
+            {
+                return Campaign.Current.CurrentGame.ObjectManager.GetObject<PartyTemplateObject>(partyTemplateId);
+            }
+
+            return null;
+        }
+    }
+}
