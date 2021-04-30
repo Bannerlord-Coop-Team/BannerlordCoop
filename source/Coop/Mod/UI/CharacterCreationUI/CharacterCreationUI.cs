10b3166dff781ffe058c141e6779462f6a8be6b6
Maximilian Boße <max@bosse.io>
2021-04-30 23:23:40 +0200

    [FIX] Adjust to new namespaces for CharacterCreation

--- source/Coop/Mod/UI/CharacterCreationUI/CharacterCreationUI.cs
+++ source/Coop/Mod/UI/CharacterCreationUI/CharacterCreationUI.cs
@@ -4,9 +4,9 @@ using TaleWorlds.Engine.Screens;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade.View.Screen;
 using TaleWorlds.Core.ViewModelCollection;
-using StoryMode.ViewModelCollection.CharacterCreationSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterCreation;
 using TaleWorlds.CampaignSystem.CharacterCreationContent;
-using StoryMode.GauntletUI.CharacterCreationSystem;
+using SandBox.GauntletUI.CharacterCreation;
 
 namespace Coop.Client.UI.CharacterCreationVM
 {
