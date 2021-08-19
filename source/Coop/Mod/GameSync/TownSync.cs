e915ef7e5d55d042046ba3e5492918e2620bd6cc
WesKrug <wesley.krug@salespad.net>
2021-08-19 15:33:05 -0400

    comment out townsync and fix lsiteners

--- source/Coop/Mod/GameSync/TownSync.cs
+++ source/Coop/Mod/GameSync/TownSync.cs
@@ -1,48 +1,49 @@
-﻿using CoopFramework;
-using JetBrains.Annotations;
-using Sync.Behaviour;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaleWorlds.CampaignSystem;
+﻿//using CoopFramework;
+//using JetBrains.Annotations;
+//using Sync.Behaviour;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TaleWorlds.CampaignSystem;
 
-namespace Coop.Mod.Patch.World
-{
-    class TownSync : CoopManaged<TownSync, Town>
-    {
-        static TownSync()
-        {
-            When(GameLoop)
-                .Calls(Setter(nameof(Town.Loyalty)))
-                .Broadcast(() => CoopClient.Instance.Synchronization)
-                .DelegateTo(IsServer);
+//namespace Coop.Mod.Patch.World
+//{
+//    class TownSync : CoopManaged<TownSync, Town>
+//    {
+//        static TownSync()
+//        {
+//            //When(GameLoop)
+//            //    .Calls(Setter(nameof(Town.Loyalty)))
+//            //    .Broadcast(() => CoopClient.Instance.Synchronization)
+//            //    .DelegateTo(IsServer);
 
-            When(GameLoop)
-                .Calls(Setter(nameof(Town.Security)))
-                .Broadcast(() => CoopClient.Instance.Synchronization)
-                .DelegateTo(IsServer);
+//            //When(GameLoop)
+//            //    .Calls(Setter(nameof(Town.Security)))
+//            //    .Broadcast(() => CoopClient.Instance.Synchronization)
+//            //    .DelegateTo(IsServer);
 
-            When(GameLoop)
-                .Calls(Method("CheckDesertionInGarrison"))
-                .DelegateTo(IsServer);
+//            //When(GameLoop)
+//            //    .Calls(Method("CheckDesertionInGarrison"))
+//            //    .DelegateTo(IsServer);
 
-            When(GameLoop)
-                .Calls(Method("DesertOneTroopFromGarrison"))
-                .Broadcast(() => CoopClient.Instance.Synchronization)
-                .DelegateTo(IsServer);
+//            //When(GameLoop)
+//            //    .Calls(Method("DesertOneTroopFromGarrison"))
+//            //    .Broadcast(() => CoopClient.Instance.Synchronization)
+//            //    .DelegateTo(IsServer);
 
-            AutoWrapAllInstances(c => new TownSync(c));
-        }
+//            ApplyStaticPatches();
+//            AutoWrapAllInstances(c => new TownSync(c));
+//        }
 
-        public TownSync([NotNull] Town instance) : base(instance)
-        {
-        }
+//        public TownSync([NotNull] Town instance) : base(instance)
+//        {
+//        }
 
-        private static ECallPropagation IsServer(IPendingMethodCall call)
-        {
-            return Coop.IsServer ? ECallPropagation.CallOriginal : ECallPropagation.Skip;
-        }
-    }
-}
+//        private static ECallPropagation IsServer(IPendingMethodCall call)
+//        {
+//            return Coop.IsServer ? ECallPropagation.CallOriginal : ECallPropagation.Skip;
+//        }
+//    }
+//}
