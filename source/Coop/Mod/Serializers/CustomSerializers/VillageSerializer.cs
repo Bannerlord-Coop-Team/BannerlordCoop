e5a0cf33356e8e353c359af2f0541d4a8f6df7ff
Wakoo <magyar.viktor.2000@gmail.com>
2021-11-22 17:29:11 +0100

    Updated VillageSerializer to use the new Object System.

--- source/Coop/Mod/Serializers/CustomSerializers/VillageSerializer.cs
+++ source/Coop/Mod/Serializers/CustomSerializers/VillageSerializer.cs
@@ -1,34 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.ObjectSystem;
+using static TaleWorlds.CampaignSystem.Village;
 
 namespace Coop.Mod.Serializers
 {
     [Serializable]
-    internal class VillageSerializer : ICustomSerializer
+    internal class VillageSerializer : CustomSerializer
     {
-        private string villageId;
-        public VillageSerializer(Village village)
+        [NonSerialized]
+        Village newvillage;
+
+        Guid bound;
+        VillageStates state;
+
+        /// <summary>
+        /// Serialized Natively Non Serializable Objects (SNNSO)
+        /// </summary>
+        Dictionary<FieldInfo, ICustomSerializer> SNNSO = new Dictionary<FieldInfo, ICustomSerializer>();
+
+        public VillageSerializer(Village village) : base(village)
         {
-            if(villageId != null)
+            List<string> UnmanagedFields = new List<string>();
+
+            foreach (FieldInfo fieldInfo in NonSerializableObjects)
             {
-                villageId = village.StringId;
+                // Get value from fieldInfo
+                object value = fieldInfo.GetValue(village);
+
+                // If value is null, no need to serialize
+                if (value == null)
+                {
+                    continue;
+                }
+
+                // Assign serializer to nonserializable objects
+                switch (fieldInfo.Name)
+                {
+                    case "_bound":
+                        bound = CoopObjectManager.GetGuid((Settlement)value);
+                        break;
+                    case "VillageType":
+                        SNNSO.Add(fieldInfo,new VillageTypeSerializer((VillageType)value));
+                        break;
+                    case "_villageState":
+                        state = (VillageStates)value;
+                        break;
+                    default:
+                        UnmanagedFields.Add(fieldInfo.Name);
+                        break;
+                }
             }
-            
+
+            if (!UnmanagedFields.IsEmpty())
+            {
+                throw new NotImplementedException($"Cannot serialize {UnmanagedFields}");
+            }
+
+            // TODO manage collections
+
+            // Remove non serializable objects before serialization
+            // They are marked as nonserializable in CustomSerializer but still tries to serialize???
+            NonSerializableCollections.Clear();
+            NonSerializableObjects.Clear();
+
         }
 
-        public object Deserialize()
+        public override object Deserialize()
         {
-            if(villageId != null)
-            {
-                return Settlement.Find(villageId);
+            //It calls the base constructor so it creates the VillagemarketData object in it in theory
+            newvillage = MBObjectManager.Instance.CreateObject<Village>();
+
+            foreach (KeyValuePair<FieldInfo, ICustomSerializer> entry in SNNSO)
+            { 
+                entry.Key.SetValue(newvillage, entry.Value.Deserialize());
             }
 
-            return null;
+            newvillage.GetType()
+                .GetField("_villageState", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(newvillage, state);
+
+            base.Deserialize(newvillage);
+
+            return newvillage;
         }
 
-        public void ResolveReferenceGuids()
+
+        public override void ResolveReferenceGuids()
         {
-            throw new NotImplementedException();
+            if (newvillage == null)
+            {
+                throw new NullReferenceException("Deserialize() has not been called before ResolveReferenceGuids().");
+            }
+
+            newvillage.GetType()
+                .GetField("_bound", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(newvillage,CoopObjectManager.GetObject(bound));
         }
     }
 }
