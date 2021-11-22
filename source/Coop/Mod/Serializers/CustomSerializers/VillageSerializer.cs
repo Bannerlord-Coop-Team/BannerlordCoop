ad21347416cfaee20da9f0051c00650cd648f584
Wakoo <magyar.viktor.2000@gmail.com>
2021-11-22 16:10:07 +0100

    Revert "Started to Update VillagerSerializer"
    This reverts commit feb435b2d3824c6380ff30378e9ffb3e1946e06e.

--- source/Coop/Mod/Serializers/CustomSerializers/VillageSerializer.cs
+++ source/Coop/Mod/Serializers/CustomSerializers/VillageSerializer.cs
@@ -6,89 +6,14 @@ namespace Coop.Mod.Serializers
     [Serializable]
     internal class VillageSerializer : ICustomSerializer
     {
-        Guid culture;
-        public VillageSerializer(Village village) : base(village)
+        private string villageId;
+        public VillageSerializer(Village village)
         {
-            List<string> UnmanagedFields = new List<string>();
-
-            foreach (FieldInfo fieldInfo in NonSerializableObjects)
+            if(villageId != null)
             {
-                // Get value from fieldInfo
-                object value = fieldInfo.GetValue(village);
-
-                // If value is null, no need to serialize
-                if (value == null)
-                {
-                    continue;
-                }
-
-                // Assign serializer to nonserializable objects
-                switch (fieldInfo.Name)
-                {
-                    case "<Culture>k__BackingField":
-                        culture = CoopObjectManager.GetGuid((CultureObject)value);
-                        break;
-                    case "<ClaimedBy>k__BackingField":
-                        SNNSO.Add(fieldInfo, new CampaignTimeSerializer((CampaignTime)value));
-                        break;
-                    case "_nextLocatable":
-                        foreach (Guid supporters in CoopObjectManager.GetGuids((MBReadOnlyList<Hero>)value))
-                        {
-                            Supporters.Add(supporters);
-                        }
-                        break;
-                    case "_settlementComponents":
-                        foreach (Guid companions in CoopObjectManager.GetGuids((MBReadOnlyList<Hero>)value))
-                        {
-                            Companions.Add(companions);
-                        }
-                        break;
-                    case "_boundVillages":
-                        foreach (Guid commanderheroes in CoopObjectManager.GetGuids((MBReadOnlyList<Hero>)value))
-                        {
-                            CommanderHeroes.Add(commanderheroes);
-                        }
-                        break;
-                    case "_name":
-                        basictroop = CoopObjectManager.GetGuid(value);
-                        break;
-                    case "_lastAttackerParty":
-                        // Assigned by SetHeroReference on deserialization
-                        leader = CoopObjectManager.GetGuid(value);
-                        break;
-                    case "_siegeEngineMissiles":
-                        SNNSO.Add(fieldInfo, new BannerSerializer((Banner)value));
-                        break;
-                    case "<Town>k_BackingField":
-                        home = CoopObjectManager.GetGuid(value);
-                        break;
-                    case "<Village>k__BackingField":
-                        SNNSO.Add(fieldInfo, new CampaignTimeSerializer((CampaignTime)value));
-                        break;
-                    case "<Hideout>k_BackingField":
-                        SNNSO.Add(fieldInfo, new DefaultPartyTemplateSerializer((PartyTemplateObject)value));
-                        break;
-                    case "<SiegeLanes>k_BackingField":
-                        kingdom = CoopObjectManager.GetGuid(value);
-                        break;
-                    default:
-                        UnmanagedFields.Add(fieldInfo.Name);
-                        break;
-                }
+                villageId = village.StringId;
             }
-
-            if (!UnmanagedFields.IsEmpty())
-            {
-                throw new NotImplementedException($"Cannot serialize {UnmanagedFields}");
-            }
-
-            // TODO manage collections
-
-            // Remove non serializable objects before serialization
-            // They are marked as nonserializable in CustomSerializer but still tries to serialize???
-            NonSerializableCollections.Clear();
-            NonSerializableObjects.Clear();
-
+            
         }
 
         public object Deserialize()
