cd8186a04992f551fab96463674dae50427fde1e
masesk <radon5k@gmail.com>
2022-01-10 17:02:29 -0800

    launch server as headless

--- source/Launcher/Launcher.Designer.cs
+++ source/Launcher/Launcher.Designer.cs
@@ -32,25 +32,15 @@ namespace Launcher
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BannerlordCoopLauncher));
-            this.TopBar = new System.Windows.Forms.Label();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.BottomBar = new System.Windows.Forms.Label();
             this.JoinGame = new System.Windows.Forms.Button();
             this.HostGame = new System.Windows.Forms.Button();
+            this.server_status = new System.Windows.Forms.Label();
+            this.TopBar = new System.Windows.Forms.Label();
             this.Logo = new System.Windows.Forms.PictureBox();
+            this.BottomBar = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SuspendLayout();
-            // 
-            // TopBar
-            // 
-            this.TopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(16)))), ((int)(((byte)(14)))));
-            this.TopBar.Location = new System.Drawing.Point(-2, -3);
-            this.TopBar.Name = "TopBar";
-            this.TopBar.Size = new System.Drawing.Size(802, 47);
-            this.TopBar.TabIndex = 0;
-            this.TopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseDown);
-            this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
-            this.TopBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseUp);
             // 
             // CloseButton
             // 
@@ -65,14 +55,6 @@ namespace Launcher
             this.CloseButton.Text = "✖";
             this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
-            // 
-            // BottomBar
-            // 
-            this.BottomBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(16)))), ((int)(((byte)(14)))));
-            this.BottomBar.Location = new System.Drawing.Point(-2, 348);
-            this.BottomBar.Name = "BottomBar";
-            this.BottomBar.Size = new System.Drawing.Size(802, 105);
-            this.BottomBar.TabIndex = 2;
             // 
             // JoinGame
             // 
@@ -100,16 +82,50 @@ namespace Launcher
             this.HostGame.UseVisualStyleBackColor = true;
             this.HostGame.Click += new System.EventHandler(this.HostGame_Click);
             // 
+            // server_status
+            // 
+            this.server_status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.server_status.AutoSize = true;
+            this.server_status.BackColor = System.Drawing.Color.Transparent;
+            this.server_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.server_status.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.server_status.Location = new System.Drawing.Point(28, 316);
+            this.server_status.Name = "server_status";
+            this.server_status.Size = new System.Drawing.Size(105, 15);
+            this.server_status.TabIndex = 7;
+            this.server_status.Text = "SERVER STATUS";
+            // 
+            // TopBar
+            // 
+            this.TopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(16)))), ((int)(((byte)(14)))));
+            this.TopBar.Location = new System.Drawing.Point(-2, -3);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(802, 47);
+            this.TopBar.TabIndex = 0;
+            this.TopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseDown);
+            this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
+            this.TopBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseUp);
+            // 
             // Logo
             // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
             this.Logo.Image = global::Launcher.Properties.Resources.logo;
-            this.Logo.InitialImage = ((System.Drawing.Image)(resources.GetObject("Logo.InitialImage")));
+            this.Logo.InitialImage = null;
             this.Logo.Location = new System.Drawing.Point(1, 47);
             this.Logo.Name = "Logo";
-            this.Logo.Size = new System.Drawing.Size(799, 386);
+            this.Logo.Size = new System.Drawing.Size(799, 298);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.Logo.TabIndex = 6;
             this.Logo.TabStop = false;
             this.Logo.WaitOnLoad = true;
+            // 
+            // BottomBar
+            // 
+            this.BottomBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(16)))), ((int)(((byte)(14)))));
+            this.BottomBar.Location = new System.Drawing.Point(-2, 348);
+            this.BottomBar.Name = "BottomBar";
+            this.BottomBar.Size = new System.Drawing.Size(802, 105);
+            this.BottomBar.TabIndex = 2;
             // 
             // BannerlordCoopLauncher
             // 
@@ -118,6 +134,7 @@ namespace Launcher
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(31)))), ((int)(((byte)(25)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.server_status);
             this.Controls.Add(this.HostGame);
             this.Controls.Add(this.JoinGame);
             this.Controls.Add(this.BottomBar);
@@ -130,19 +147,21 @@ namespace Launcher
             this.Name = "BannerlordCoopLauncher";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BannerlordCoopLauncher";
+            this.Load += new System.EventHandler(this.BannerlordCoopLauncher_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label TopBar;
-        private System.Windows.Forms.Button CloseButton;
-        private Label BottomBar;
+        private Button CloseButton;
         private Button JoinGame;
         private Button HostGame;
+        private Label server_status;
+        private Label TopBar;
         private PictureBox Logo;
+        private Label BottomBar;
     }
 }
 
--- source/Launcher/Launcher.cs
+++ source/Launcher/Launcher.cs
@@ -2,23 +2,85 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Launcher
 {
+
+
+
     public partial class BannerlordCoopLauncher : Form
     {
         private bool mouseDown;
         private Point lastLocation;
         private string mbPath;
+        private bool serverRunning = false;
+        private int bannerlordId = -1;
 
+
+        [DllImport("User32")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+       
 
         public BannerlordCoopLauncher()
         {
             InitializeComponent();
+            // find mb path relative to where this exectuable should be
             this.mbPath = Path.GetFullPath($@"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace(@"file:\", "")}\..\..\..\..\");
+
+            // try to read bannerlord pid started by this tool
+            try
+            {
+                bannerlordId = Int32.Parse(File.ReadAllLines("pid")[0]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                bannerlordId = -1;
+            }
+            catch (IOException)
+            {
+                bannerlordId = -1;
+            }
+
+
+            if(bannerlordId != -1)
+            {
+                HostGame.Text = "STOP SERVER";
+                serverRunning = true;
+                this.Invalidate();
+            }
+
+
+            this.mbPath = @"E:\Created Content\MB170\";
         }
 
+        private void DisableSafemode()
+        {
+            string configFile = $@"{System.Environment.GetEnvironmentVariable("USERPROFILE")}\Documents\Mount and Blade II Bannerlord\Configs\engine_config.txt";
+            string[] lines = File.ReadAllLines(configFile);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split('=');
+                if (parts[0].Trim().Equals("safely_exited"))
+                {
+                    if (parts[1].Trim().Equals("1"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        lines[i] = $"{parts[0]}= 1";
+                        File.WriteAllLines(configFile, lines);
+                        break;
+                    }
+                    
+                }
+                
+            }
+            
+        }
 
         private void TopBar_MouseUp(object sender, MouseEventArgs e)
         {
@@ -28,6 +90,7 @@ namespace Launcher
         {
             mouseDown = true;
             lastLocation = e.Location;
+
         }
         private void TopBar_MouseMove(object sender, MouseEventArgs e)
         {
@@ -42,14 +105,23 @@ namespace Launcher
 
         private void StartGame(string additionalArgs)
         {
+            DisableSafemode();
             Process process = new Process();
             process.StartInfo.WorkingDirectory = this.mbPath + @"\bin\Win64_Shipping_Client\";
-            process.StartInfo.UseShellExecute = true;
             process.StartInfo.CreateNoWindow = false;
             process.StartInfo.FileName = "\"" + mbPath + @"\bin\Win64_Shipping_Client\Bannerlord.exe" + "\"";
-            process.StartInfo.Arguments = $"/singleplayer {additionalArgs} _MODULES_*Native*SandBoxCore*CustomBattle*SandBox*StoryMode*Coop*_MODULES_";
+            process.StartInfo.Arguments = $"/singleplayer {additionalArgs} _MODULES_*Native*SandBoxCore*CustomBattle*SandBox*StoryMode*_MODULES_";
+            process.StartInfo.CreateNoWindow = true;
             process.Start();
-            process.Close();
+            while (process.MainWindowHandle == IntPtr.Zero)
+            {
+                Thread.Sleep(100);
+            }
+            ShowWindow(process.MainWindowHandle, 0);
+            bannerlordId = process.Id;
+            serverRunning = true;
+            this.Invalidate();
+            File.WriteAllLines("pid", new string[] { bannerlordId.ToString() });
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
@@ -63,7 +135,22 @@ namespace Launcher
         /// <param name="e"></param>
         private void HostGame_Click(object sender, EventArgs e)
         {
-            StartGame("/server");
+            if (!serverRunning)
+            {
+                StartGame("/server /headless");
+                ((Button)sender).Text = "STOP SERVER";
+               
+            }
+            else
+            {
+                Process.GetProcessById(bannerlordId).Kill();
+                ((Button)sender).Text = "HOST GAME";
+                serverRunning = false;
+                File.Delete("pid");
+                this.Invalidate();
+
+            }
+            
         }
         /// <summary>
         /// Start a new process to run the application, then close it. Launch the game in sever mode
@@ -73,6 +160,30 @@ namespace Launcher
         private void JoinGame_Click(object sender, EventArgs e)
         {
             StartGame("/client");
+        }
+
+        private void BannerlordCoopLauncher_Load(object sender, EventArgs e)
+        {
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.BannerlordCoopLauncher_Paint);
+        }
+
+        private void BannerlordCoopLauncher_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+            System.Drawing.SolidBrush myBrush;
+            if (serverRunning)
+            {
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Green);
+            }
+            else
+            {
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            }
+            g.FillEllipse(myBrush, new Rectangle(20, 320, 8, 8));
+            myBrush.Dispose();
+            g.Dispose();
+
         }
     }
 }
