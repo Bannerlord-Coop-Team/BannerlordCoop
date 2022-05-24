ca759eb638fc5cd09d1fe2f448651aae739dd113
DELAPORTE Thomas <thomas.d@arpa3.fr>
2022-05-24 19:13:16 +0200

    feat: fief food stocks setter RPC

---
+++ source/Coop/Mod/GameSync/Fief/FiefPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace Coop.Mod.GameSync.Fief
+{
+    [HarmonyPatch(typeof(TaleWorlds.CampaignSystem.Fief), "FoodStocks", MethodType.Setter)]
+    class FiefPatch
+    {
+        static void Postfix(TaleWorlds.CampaignSystem.Fief __instance)
+        {
+            if (!Coop.IsServer)
+            {
+                return;
+            }
+
+            FiefSync.BroadcastChangeFoodStock(__instance);
+        }
+    }
+}
---
+++ source/Coop/Mod/GameSync/Fief/FiefSync.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Coop.Mod.GameSync.Hideout;
+using Coop.Mod.Persistence.Party;
+using CoopFramework;
+using RailgunNet.System.Types;
+using Sync.Call;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace Coop.Mod.GameSync.Fief
+{
+    class FiefSync
+    {
+        private static Invokable OnChangeFoodStockRPC;
+
+        /// <summary>
+        ///     Initialize RPCs on client and server side.
+        /// </summary>
+        [PatchInitializer]
+        private static void InitRPC()
+        {
+            OnChangeFoodStockRPC = new Invokable(typeof(FiefSync).GetMethod(nameof(FiefSync.OnChangeFoodStock),
+                BindingFlags.NonPublic | BindingFlags.Static));
+        }
+
+        private static void OnChangeFoodStock(Guid fiefGuid, float foodStocks)
+        {
+            if (Coop.IsServer)
+            {
+                return;
+            }
+
+            TaleWorlds.CampaignSystem.Fief fief = CoopObjectManager.GetObject<TaleWorlds.CampaignSystem.Fief>(fiefGuid);
+
+            if (fief == null)
+            {
+                return;
+            }
+
+            InformationManager.DisplayMessage(new InformationMessage($"New food stocks {fief.Name}: {foodStocks}"));
+            fief.FoodStocks = foodStocks;
+        }
+
+        public static void BroadcastChangeFoodStock(TaleWorlds.CampaignSystem.Fief fief)
+        {
+            if (fief != null)
+            {
+                Guid fiefGuid = CoopObjectManager.GetGuid(fief);
+
+                if (fiefGuid == Guid.Empty)
+                {
+                    return;
+                }
+
+                CoopServer.Instance.Synchronization.Broadcast(OnChangeFoodStockRPC.Id, null, new object[] { fiefGuid, fief.FoodStocks });
+            }
+        }
+    }
+}
