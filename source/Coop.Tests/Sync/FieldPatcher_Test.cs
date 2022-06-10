feaccebf2d1802692cefd46e112eb1972c3f157e
Garrett Luskey <garrett.luskey@gmail.com>
2022-06-10 14:54:00 -0500

    Initial Field sync

---
+++ source/Coop.Tests/Sync/FieldPatcher_Test.cs
@@ -0,0 +1,184 @@
+﻿using HarmonyLib;
+using HarmonyTranspilerTools;
+using Sync.Patch;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Coop.Tests.Sync
+{
+    class TestClass
+    {
+        public int MyVar = 0;
+        public int MyVar2 = 0;
+        public void AssignFieldWithParam(int x = 5)
+        {
+            //FieldPatcher_Test.InterceptMethod(x, ref MyVar);
+            MyVar = x;
+        }
+
+        public void AssignMultiFieldsWithParam(int x = 5)
+        {
+            //FieldPatcher_Test.InterceptMethod(x, ref MyVar);
+            MyVar = x;
+            MyVar2 = MyVar * x + 1000;
+        }
+
+        public void AssignMultiFieldsWithParamLarge(int x = 5)
+        {
+            int y = 50;
+            MyVar = x + y;
+            AssignByRef(ref MyVar2, x);
+            MyVar = 10;
+            MyVar = Return100();
+            MyVar = MyVar2;
+        }
+
+        public void AssignFieldByRef(int x = 5)
+        {
+            AssignByRef(ref MyVar, x);
+        }
+
+        public int Return100()
+        {
+            return 100;
+        }
+        public void AssignByReturn()
+        {
+            MyVar = Return100();
+        }
+
+        public void AssignMultiFieldsByRefLarge(int x = 5)
+        {
+            AssignByRef(ref MyVar2, x);
+            AssignByRef(ref MyVar, x);
+            AssignByRef(ref MyVar2, x);
+            AssignByRef(ref MyVar, x);
+            MyVar2 = Return100();
+            AssignByRef(ref MyVar, x);
+            AssignByRef(ref MyVar2, x);
+        }
+
+        private void AssignByRef(ref int x, int value)
+        {
+            x = value;
+        }
+    }
+    public class FieldPatcher_Test
+    {
+        static int Calls = 0;
+        static int Temp;
+        public static void InterceptMethod(int value, ref int variable)
+        {
+            Calls += 1;
+        }
+
+        [Fact]
+        private void SingleAssignmentInterception()
+        {
+            MethodInfo caller = typeof(TestClass).GetMethod(nameof(TestClass.AssignFieldWithParam));
+            MethodInfo intercept = typeof(FieldPatcher_Test).GetMethod(nameof(FieldPatcher_Test.InterceptMethod));
+            FieldInfo field = typeof(TestClass).GetField(nameof(TestClass.MyVar));
+
+            FieldPatchFactory.hookMethod = intercept;
+            FieldPatchFactory.GeneratePatch(field, caller, intercept);
+            FieldPatchFactory.ReplaceMethod(caller);
+
+            TestClass testClass = new TestClass();
+
+            testClass.AssignFieldWithParam(100);
+
+            Assert.Equal(1, Calls);
+            Assert.Equal(0, testClass.MyVar);
+        }
+
+        [Fact]
+        private void MultiAssignmentInterception()
+        {
+            MethodInfo caller = typeof(TestClass).GetMethod(nameof(TestClass.AssignMultiFieldsWithParam));
+            MethodInfo intercept = typeof(FieldPatcher_Test).GetMethod(nameof(FieldPatcher_Test.InterceptMethod));
+            FieldInfo field = typeof(TestClass).GetField(nameof(TestClass.MyVar));
+            FieldInfo field2 = typeof(TestClass).GetField(nameof(TestClass.MyVar2));
+
+            FieldPatchFactory.hookMethod = intercept;
+            FieldPatchFactory.GeneratePatch(field, caller, intercept);
+            FieldPatchFactory.GeneratePatch(field2, caller, intercept);
+            FieldPatchFactory.ReplaceMethod(caller);
+
+            TestClass testClass = new TestClass();
+
+            testClass.AssignMultiFieldsWithParam(100);
+
+            Assert.Equal(2, Calls);
+            Assert.Equal(0, testClass.MyVar);
+        }
+
+        [Fact]
+        private void LargeAssignmentInterception()
+        {
+            MethodInfo caller = typeof(TestClass).GetMethod(nameof(TestClass.AssignMultiFieldsWithParamLarge));
+            MethodInfo intercept = typeof(FieldPatcher_Test).GetMethod(nameof(FieldPatcher_Test.InterceptMethod));
+            FieldInfo field = typeof(TestClass).GetField(nameof(TestClass.MyVar));
+
+            FieldPatchFactory.hookMethod = intercept;
+            FieldPatchFactory.GeneratePatch(field, caller, intercept);
+            FieldPatchFactory.ReplaceMethod(caller);
+
+            TestClass testClass = new TestClass();
+
+            testClass.AssignMultiFieldsWithParamLarge(100);
+
+            Assert.Equal(4, Calls);
+            Assert.Equal(0, testClass.MyVar);
+        }
+
+        [Fact]
+        private void ByRefAssignmentInterception()
+        {
+            MethodInfo caller = typeof(TestClass).GetMethod(nameof(TestClass.AssignFieldByRef));
+            MethodInfo intercept = typeof(FieldPatcher_Test).GetMethod(nameof(FieldPatcher_Test.InterceptMethod));
+            FieldInfo field = typeof(TestClass).GetField(nameof(TestClass.MyVar));
+            FieldInfo tmpField = typeof(FieldPatcher_Test).GetField(nameof(Temp), BindingFlags.NonPublic | BindingFlags.Static);
+
+            FieldPatchFactory.hookMethod = intercept;
+            FieldPatchFactory.tempField = tmpField;
+            FieldPatchFactory.GeneratePatch(field, caller, intercept);
+            FieldPatchFactory.ReplaceMethod(caller);
+
+            TestClass testClass = new TestClass();
+
+            testClass.AssignFieldByRef(100);
+
+            Assert.Equal(1, Calls);
+            Assert.Equal(0, testClass.MyVar);
+        }
+
+        [Fact]
+        private void ByRefAssignmentInterceptionLarge()
+        {
+            MethodInfo caller = typeof(TestClass).GetMethod(nameof(TestClass.AssignMultiFieldsByRefLarge));
+            MethodInfo intercept = typeof(FieldPatcher_Test).GetMethod(nameof(FieldPatcher_Test.InterceptMethod));
+            FieldInfo field = typeof(TestClass).GetField(nameof(TestClass.MyVar));
+            FieldInfo tmpField = typeof(FieldPatcher_Test).GetField(nameof(Temp), BindingFlags.NonPublic | BindingFlags.Static);
+
+            FieldPatchFactory.hookMethod = intercept;
+            FieldPatchFactory.tempField = tmpField;
+            FieldPatchFactory.GeneratePatch(field, caller, intercept);
+            FieldPatchFactory.ReplaceMethod(caller);
+
+            TestClass testClass = new TestClass();
+
+            testClass.AssignMultiFieldsByRefLarge(100);
+
+            Assert.Equal(3, Calls);
+            Assert.Equal(0, testClass.MyVar);
+        }
+
+    }
+}
---
+++ source/Sync/Patch/FieldPatchFactory.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using HarmonyTranspilerTools;
+
+namespace Sync.Patch
+{
+    public class FieldPatchFactory
+    {
+        private static MethodInfo _transpiler = typeof(FieldPatchFactory).GetMethod("Transpiler", BindingFlags.NonPublic | BindingFlags.Static);
+
+        private static Dictionary<MethodBase, List<FieldInfo>> _fields = new Dictionary<MethodBase, List<FieldInfo>>();
+
+        public static MethodInfo hookMethod { get; set; }
+        public static FieldInfo tempField { get; set; }
+
+        public static void ReplaceMethod(MethodInfo method)
+        {
+            lock (Patcher.HarmonyLock)
+            {
+                var patch = new HarmonyMethod(_transpiler)
+                {
+                    priority = SyncPriority.MethodPatchGenerated,
+#if DEBUG
+                    debug = true
+#endif
+                };
+                Patcher.HarmonyInstance.Patch(method, transpiler: patch);
+            }
+        }
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, MethodBase original)
+        {
+            List<CodeInstruction> lInstrs = instr.ToList();
+            foreach(FieldInfo field in _fields[original])
+            {
+                ReplaceNativeAssign(lInstrs, field, hookMethod);
+            }
+            
+            return lInstrs;
+        }
+
+        public static void GeneratePatch(
+            FieldInfo field,
+            MethodInfo caller,
+            MethodInfo interceptMethod)
+        {
+            if (_fields.ContainsKey(caller))
+            {
+                _fields[caller].Add(field);
+            }
+            else
+            {
+                _fields.Add(caller, new List<FieldInfo> { field });
+            }
+        }
+
+        static void ReplaceNativeAssign(List<CodeInstruction> codeInstructions, FieldInfo field, MethodInfo hookMethod)
+        {
+            // Validation
+            if (hookMethod.GetParameters().Length != 2)
+            {
+                throw new InvalidOperationException("Invalid parameters. " +
+                    "Hook method expects (int value, ref int field) parameters");
+            }
+
+            Type parameterValueType = hookMethod.GetParameters()[0].ParameterType;
+
+            if (!parameterValueType.IsAssignableFrom(field.FieldType))
+            {
+                throw new InvalidCastException("Invalid parameter type. " +
+                    $"expected {field.FieldType} but got {parameterValueType}");
+            }
+
+            Type fieldReferenceType = hookMethod.GetParameters()[1].ParameterType;
+
+            if (!fieldReferenceType.IsAssignableFrom(field.FieldType.MakeByRefType()))
+            {
+                throw new InvalidCastException("Invalid parameter type. " +
+                    $"expected {field.FieldType} but got {fieldReferenceType}");
+            }
+
+            CodeInstruction[] assignmentInterceptBlock =
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldflda, field),
+                new CodeInstruction(OpCodes.Call, hookMethod),
+            };
+
+            CodeInstruction[] byRefInterceptBlockPre =
+            {
+                new CodeInstruction(OpCodes.Ldsflda, tempField),
+            };
+
+            CodeInstruction[] byRefInterceptBlockPost =
+            {
+                new CodeInstruction(OpCodes.Ldsfld, tempField),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldflda, field),
+                new CodeInstruction(OpCodes.Call, hookMethod),
+            };
+
+            Stack<int> classRefernceStack = new Stack<int>();
+
+            for (int i = 0; i < codeInstructions.Count; i++)
+            {
+                
+
+                var instr = codeInstructions[i];
+                if (instr.opcode == OpCodes.Ldarg_0)
+                {
+                    classRefernceStack.Push(i);
+                }
+                else if(instr.opcode == OpCodes.Ldfld)
+                {
+                    classRefernceStack.Pop();
+                }
+                else if(instr.opcode == OpCodes.Ldflda)
+                {
+                    if (instr.operand as FieldInfo == field)
+                    {
+                        for (int j = i; j < codeInstructions.Count; j++)
+                        {
+                            if (codeInstructions[j].opcode == OpCodes.Call &&
+                                codeInstructions[j].operand as MethodInfo != hookMethod)
+                            {
+                                codeInstructions.RemoveAt(i);
+                                codeInstructions.InsertRange(i, byRefInterceptBlockPre);
+                                codeInstructions.RemoveAt(classRefernceStack.Pop());
+
+                                i += byRefInterceptBlockPre.Length - 2;
+
+                                j += byRefInterceptBlockPre.Length - 2;
+
+                                codeInstructions.InsertRange(j + 1, byRefInterceptBlockPost);
+                                break;
+                            }
+                        }
+
+                        
+
+                    }
+                    else
+                    {
+                        classRefernceStack.Pop();
+                    }
+                }
+                else if(instr.opcode == OpCodes.Stfld)
+                {
+                    if (instr.operand as FieldInfo == field)
+                    {
+                        codeInstructions.RemoveAt(i);
+                        codeInstructions.InsertRange(i, assignmentInterceptBlock);
+                        codeInstructions.RemoveAt(classRefernceStack.Pop());
+                        i += assignmentInterceptBlock.Length - 2;
+                    }
+                    else
+                    {
+                        classRefernceStack.Pop();
+                    }
+                }
+            }
+        }
+    }
+}
