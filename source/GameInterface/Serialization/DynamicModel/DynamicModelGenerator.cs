08f601be368bcccaea07d6b8e4a00b8565dc58d8
Garrett Luskey <garrett.luskey@gmail.com>
2022-08-14 16:44:37 -0500

    Added exclude by name

--- source/GameInterface/Serialization/DynamicModel/DynamicModelGenerator.cs
+++ source/GameInterface/Serialization/DynamicModel/DynamicModelGenerator.cs
@@ -10,6 +10,12 @@ namespace GameInterface.Serialization.DynamicModel
 {
     public class DynamicModelGenerator : IDynamicModelGenerator
     {
+
+        /// <summary>
+        /// Creates a dynamic serialization model for protobuf of the provided type
+        /// </summary>
+        /// <typeparam name="T">Type to create model</typeparam>
+        /// <param name="exclude">Excluded fields by type</param>
         public void CreateDynamicSerializer<T>(IEnumerable<Type> exclude = null)
         {
             FieldInfo[] fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
@@ -19,6 +25,26 @@ namespace GameInterface.Serialization.DynamicModel
                 HashSet<Type> excludesSet = exclude.ToHashSet();
 
                 fields = fields.Where(f => excludesSet.Contains(f.FieldType) == false).ToArray();
+            }
+
+            string[] fieldNames = fields.Select(f => f.Name).ToArray();
+            RuntimeTypeModel.Default.Add(typeof(T), true).Add(fieldNames);
+        }
+
+        /// <summary>
+        /// Creates a dynamic serialization model for protobuf of the provided type
+        /// </summary>
+        /// <typeparam name="T">Type to create model</typeparam>
+        /// <param name="exclude">Excluded fields by name</param>
+        public void CreateDynamicSerializer<T>(IEnumerable<string> exclude = null)
+        {
+            FieldInfo[] fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (exclude != null)
+            {
+                HashSet<string> excludesSet = exclude.ToHashSet();
+
+                fields = fields.Where(f => excludesSet.Contains(f.Name) == false).ToArray();
             }
 
             string[] fieldNames = fields.Select(f => f.Name).ToArray();
