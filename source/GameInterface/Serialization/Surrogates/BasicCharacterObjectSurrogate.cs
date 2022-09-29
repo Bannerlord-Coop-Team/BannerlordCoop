9adca203a4962a751c87521c6773533070a9b01e
Garrett Luskey <garrett.luskey@gmail.com>
2022-09-29 14:41:48 -0500

    Added starter files

---
+++ source/GameInterface/Serialization/Surrogates/BasicCharacterObjectSurrogate.cs
@@ -0,0 +1,69 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+namespace GameInterface.Serialization.Surrogates
+{
+    [ProtoContract]
+    public readonly struct BasicCharacterObjectSurrogate
+    {
+        [ProtoMember(1)]
+        TextObject _basicName { get; }
+        // ProtoMember(2)]
+        // ...
+
+        #region Reflection
+        private static readonly BindingFlags PrivateFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+
+        private static readonly Dictionary<string, FieldInfo> Fields = new Dictionary<string, FieldInfo>
+        {
+            {nameof(_basicName), typeof(BasicCharacterObject).GetField(nameof(_basicName), PrivateFlags) },
+            //...
+        };
+
+        #endregion
+
+        public BasicCharacterObjectSurrogate(BasicCharacterObject basicCharacterObject)
+        {
+            _basicName = basicCharacterObject.Name;
+            // TODO ...
+        }
+
+        private BasicCharacterObject Deserialize()
+        {
+            BasicCharacterObject newBasicCharacterObject = new BasicCharacterObject();
+
+            Fields[nameof(_basicName)].SetValue(newBasicCharacterObject, _basicName);
+            // TODO ...
+
+            return newBasicCharacterObject;
+        }
+
+        /// <summary>
+        ///     TODO
+        /// </summary>
+        /// <param name="basicCharacterObject">TODO</param>
+        /// <returns>TODO</returns>
+        public static implicit operator BasicCharacterObjectSurrogate(BasicCharacterObject basicCharacterObject)
+        {
+            return new BasicCharacterObjectSurrogate(basicCharacterObject);
+        }
+
+        /// <summary>
+        ///     TODO
+        /// </summary>
+        /// <param name="surrogate">TODO</param>
+        /// <returns>TODO</returns>
+        public static implicit operator BasicCharacterObject(BasicCharacterObjectSurrogate surrogate)
+        {
+            return surrogate.Deserialize();
+        }
+    }
+}
