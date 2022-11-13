75cc4c2a751b7683dae35f3c7137d00177658c56
EgardA <veryhasted@gmail.com>
2022-11-13 18:19:01 +0100

    Battle Loading on Position

--- source/Missions/Test/MissionTestGameManager.cs
+++ source/Missions/Test/MissionTestGameManager.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.AgentOrigins;
 using TaleWorlds.CampaignSystem.Encounters;
+using TaleWorlds.CampaignSystem.Map;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Settlements.Locations;
@@ -59,22 +60,22 @@ namespace Coop.Mod.Missions
         {
             base.OnLoadFinished();
             //get the settlement first
-            Settlement settlement = Settlement.Find("town_ES3");
 
-            CharacterObject characterObject = CharacterObject.PlayerCharacter;
-            LocationEncounter locationEncounter = new TownEncounter(settlement);
+            PlayerEncounter.Start();
+            PlayerEncounter.Current.SetupFields(PartyBase.MainParty, MobileParty.AllBanditParties.First().Party);
+            PlayerEncounter.StartBattle();
+            Mission mission = SandBoxMissions.OpenBattleMission(PlayerEncounter.GetBattleSceneForMapPatch(Campaign.Current.MapSceneWrapper.GetMapPatchAtPosition(MobileParty.MainParty.Position2D)));
+            mission.AddMissionBehavior(new MissionNetworkBehavior(m_Client));
 
             // create an encounter of the town with the player
-            EncounterManager.StartSettlementEncounter(MobileParty.MainParty, settlement);
+            //EncounterManager.StartSettlementEncounter(MobileParty.MainParty, settlement);
+            //EncounterManager.StartPartyEncounter(MobileParty.MainParty.Party, MobileParty.AllBanditParties.First().Party);
 
             //Set our encounter to the created encounter
-            PlayerEncounter.LocationEncounter = locationEncounter;
+            //PlayerEncounter.LocationEncounter = locationEncounter;
 
-            int upgradeLevel = settlement.Town?.GetWallLevel() ?? 1;
-            Location tavern = LocationComplex.Current.GetLocationWithId("tavern");
-            string scene = tavern.GetSceneName(upgradeLevel);
-            Mission mission = SandBoxMissions.OpenIndoorMission(scene, tavern);
-            mission.AddMissionBehavior(new MissionNetworkBehavior(m_Client));
+            //Mission mission = SandBoxMissions.OpenBattleMission(new MissionInitializerRecord("battle_terrain_g"));
+            //mission.AddMissionBehavior(new MissionNetworkBehavior(m_Client));
 
             //PlayerEncounter.EnterSettlement();
 
@@ -83,10 +84,10 @@ namespace Coop.Mod.Missions
             //return arena scenae name of current town
             //         int upgradeLevel = settlement.IsTown ? settlement.Town.GetWallLevel() : 1;
 
-            //         //Open a new arena mission with the scene; commented out because we are not doing Arena testing right now
+            //Open a new arena mission with the scene; commented out because we are not doing Arena testing right now
             //string civilianUpgradeLevelTag = Campaign.Current.Models.LocationModel.GetCivilianUpgradeLevelTag(upgradeLevel);
-            //         Mission currentMission = MissionState.OpenNew("ArenaDuelMission", SandBoxMissions.CreateSandBoxMissionInitializerRecord(locationWithId.GetSceneName(upgradeLevel), "", false), (Mission mission) => new MissionBehavior[]
-            //            {
+            //Mission currentMission = MissionState.OpenNew("ArenaDuelMission", SandBoxMissions.CreateSandBoxMissionInitializerRecord(locationWithId.GetSceneName(upgradeLevel), "", false), (Mission mission) => new MissionBehavior[]
+            //   {
             //                             new MissionOptionsComponent(),
             //                             //new ArenaDuelMissionController(CharacterObject.PlayerCharacter, false, false, null, 1), //this was the default controller that spawned the player and 1 opponent. Not very useful
             //                             new MissionFacialAnimationHandler(),
@@ -101,7 +102,7 @@ namespace Coop.Mod.Missions
             //                             new EquipmentControllerLeaveLogic(),
             //                             new MissionAgentHandler(locationWithId, null),
             //                             new MissionNetworkBehavior(),
-            //            }, true, true);
+            //   }, true, true);
 
             //MouseManager.ShowCursor(false);
 
