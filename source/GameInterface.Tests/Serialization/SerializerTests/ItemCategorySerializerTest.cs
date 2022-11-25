6dc559c78d9c5381be4be1911e0ebc5d107867c1
EgardA <veryhasted@gmail.com>
2022-11-25 22:00:20 +0100

    ItemCategoryBinaryPackage

---
+++ source/GameInterface.Tests/Serialization/SerializerTests/ItemCategorySerializerTest.cs
@@ -0,0 +1,56 @@
+﻿using GameInterface.Serialization.Impl;
+using GameInterface.Serialization;
+using TaleWorlds.Core;
+using Xunit;
+using System.Runtime.Serialization;
+using Common.Extensions;
+using System.Reflection;
+
+namespace GameInterface.Tests.Serialization.SerializerTests
+{
+    public class ItemCategorySerializationTest
+    {
+        [Fact]
+        public void ItemCategory_Serialize()
+        {
+            ItemCategory testItemCategory = (ItemCategory)FormatterServices.GetUninitializedObject(typeof(ItemCategory));
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            ItemCategoryBinaryPackage package = new ItemCategoryBinaryPackage(testItemCategory, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+        }
+
+        [Fact]
+        public void ItemCategory_Full_Serialization()
+        {
+            ItemCategory testItemCategory = (ItemCategory)FormatterServices.GetUninitializedObject(typeof(ItemCategory));
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            ItemCategoryBinaryPackage package = new ItemCategoryBinaryPackage(testItemCategory, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+
+            object obj = BinaryFormatterSerializer.Deserialize(bytes);
+
+            Assert.IsType<ItemCategoryBinaryPackage>(obj);
+
+            ItemCategoryBinaryPackage returnedPackage = (ItemCategoryBinaryPackage)obj;
+
+            ItemCategory newItemCategory = returnedPackage.Unpack<ItemCategory>();
+
+            foreach(FieldInfo field in typeof(ItemCategory).GetAllInstanceFields())
+            {
+                Assert.Equal(field.GetValue(testItemCategory), field.GetValue(newItemCategory));
+            }
+        }
+    }
+}
---
+++ source/GameInterface/Serialization/Impl/ItemCategoryBinaryPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TaleWorlds.Core;
+
+namespace GameInterface.Serialization.Impl
+{
+    [Serializable]
+    public class ItemCategoryBinaryPackage : BinaryPackageBase<ItemCategory>
+    {
+        string stringId;
+        public ItemCategoryBinaryPackage(ItemCategory obj, BinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+        {
+
+        }
+        public override void Pack()
+        {
+            stringId = Object.StringId;
+        }
+        protected override void UnpackInternal()
+        {
+            Object = new ItemCategory(stringId);
+        }
+    }
+}
