91f7725beda2405bdc79757a3682444da4b912a2
zzzzzzzzott <biscuitbailey2002@gmail.com>
2022-11-25 21:36:30 -0600

    Resolves #352 and #330, 330 may need more testing.

---
+++ source/GameInterface.Tests/Serialization/SerializerTests/TradeItemComponentSerializationTest.cs
@@ -0,0 +1,51 @@
+﻿using GameInterface.Serialization;
+using GameInterface.Serialization.Impl;
+using System.Linq;
+using TaleWorlds.Core;
+using Xunit;
+
+namespace GameInterface.Tests.Serialization.SerializerTests
+{
+    public class TradeItemComponentSerializationTest
+    {
+        [Fact]
+        public void TradeItemComponent_Serialize()
+        {
+            TradeItemComponent tradeItemComponent = new TradeItemComponent();
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            TradeItemComponentBinaryPackage package = new TradeItemComponentBinaryPackage(tradeItemComponent, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+            
+            Assert.NotEmpty(bytes);
+        }
+
+        [Fact]
+        public void StaticBodyProperties_Full_Serialization()
+        {
+            TradeItemComponent tradeItemComponent = new TradeItemComponent();
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            TradeItemComponentBinaryPackage package = new TradeItemComponentBinaryPackage(tradeItemComponent, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+
+            object obj = BinaryFormatterSerializer.Deserialize(bytes);
+
+            Assert.IsType<TradeItemComponentBinaryPackage>(obj);
+
+            TradeItemComponentBinaryPackage returnedPackage = (TradeItemComponentBinaryPackage)obj;
+
+            TradeItemComponent newtradeItemComponent = returnedPackage.Unpack<TradeItemComponent>();
+
+            Assert.Equal(tradeItemComponent.MoraleBonus, newtradeItemComponent.MoraleBonus);
+        }
+    }
+}
