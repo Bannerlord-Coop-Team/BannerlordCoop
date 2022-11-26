ffb71073c958991c6fda6c95ead9224b413c9780
Garrett Luskey <garrett.luskey@gmail.com>
2022-11-26 15:30:49 -0600

    Added hashset support

---
+++ source/GameInterface.Tests/Serialization/SerializerTests/MonsterSerializationTest.cs
@@ -0,0 +1,65 @@
+﻿using Common.Extensions;
+using GameInterface.Serialization;
+using GameInterface.Serialization.Impl;
+using System.Linq;
+using System.Reflection;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using Xunit;
+
+namespace GameInterface.Tests.Serialization.SerializerTests
+{
+    public class MonsterSerializationTest
+    {
+        [Fact]
+        public void Monster_Serialize()
+        {
+            Monster monster = new Monster();
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            MonsterBinaryPackage package = new MonsterBinaryPackage(monster, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+        }
+
+        [Fact]
+        public void Monster_Full_Serialization()
+        {
+            Monster monster = new Monster();
+
+            FieldInfo _monsterMissionData = typeof(Monster).GetField("_monsterMissionData", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            _monsterMissionData.SetValue(monster, new MonsterMissionData(monster));
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            MonsterBinaryPackage package = new MonsterBinaryPackage(monster, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+
+            object obj = BinaryFormatterSerializer.Deserialize(bytes);
+
+            Assert.IsType<MonsterBinaryPackage>(obj);
+
+            MonsterBinaryPackage returnedPackage = (MonsterBinaryPackage)obj;
+
+            Monster newMonster = returnedPackage.Unpack<Monster>();
+
+            foreach(FieldInfo field in typeof(Monster).GetAllInstanceFields(MonsterBinaryPackage.Excludes))
+            {
+                Assert.Equal(field.GetValue(monster), field.GetValue(newMonster));
+            }
+
+            Assert.NotEqual(_monsterMissionData.GetValue(monster), _monsterMissionData.GetValue(newMonster));
+        }
+    }
+}
---
+++ source/GameInterface/Serialization/Impl/MonsterBinaryPackage.cs
@@ -0,0 +1,45 @@
+﻿using Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TaleWorlds.Core;
+
+namespace GameInterface.Serialization.Impl
+{
+    /// <summary>
+    /// Binary package for Monster
+    /// </summary>
+    [Serializable]
+    public class MonsterBinaryPackage : BinaryPackageBase<Monster>
+    {
+
+        public static string[] Excludes = new string[]
+        {
+            "_monsterMissionData",
+        };
+
+
+        public MonsterBinaryPackage(Monster obj, BinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+        {
+        }
+
+        public override void Pack()
+        {
+            foreach (FieldInfo field in ObjectType.GetAllInstanceFields(Excludes))
+            {
+                object obj = field.GetValue(Object);
+                StoredFields.Add(field, BinaryPackageFactory.GetBinaryPackage(obj));
+            }
+        }
+
+        protected override void UnpackInternal()
+        {
+            TypedReference reference = __makeref(Object);
+            foreach (FieldInfo field in StoredFields.Keys)
+            {
+                field.SetValueDirect(reference, StoredFields[field].Unpack());
+            }
+        }
+    }
+}
