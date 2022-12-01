5a9ab40bfe425c7cba093e49e7b031302dcbff4b
Viktor Magyar <magyar.viktor.2000@gmail.com>
2022-12-01 13:24:36 +0100

    Added ItemRostre,ItemRosterElement,Hideout,EquipmentElement,Equipment and BanditParty BinaryPackage.

---
+++ source/GameInterface.Tests/Serialization/SerializerTests/ItemRosterSerializerTest.cs
@@ -0,0 +1,57 @@
+﻿using GameInterface.Serialization.Native;
+using GameInterface.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TaleWorlds.CampaignSystem.Roster;
+using GameInterface.Serialization.Impl;
+
+namespace GameInterface.Tests.Serialization.SerializerTests
+{
+    public class ItemRosterSerializerTest
+    {
+        [Fact]
+        public void ItemRoster_Serialize()
+        {
+            ItemRoster itemRoster = new ItemRoster();
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            ItemRosterBinaryPackage package = new ItemRosterBinaryPackage(itemRoster, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+        }
+
+        [Fact]
+        public void ItemRoster_Full_Serialization()
+        {
+            ItemRoster itemRoster = new ItemRoster();
+
+            BinaryPackageFactory factory = new BinaryPackageFactory();
+            ItemRosterBinaryPackage package = new ItemRosterBinaryPackage(itemRoster, factory);
+
+            package.Pack();
+
+            byte[] bytes = BinaryFormatterSerializer.Serialize(package);
+
+            Assert.NotEmpty(bytes);
+
+            object obj = BinaryFormatterSerializer.Deserialize(bytes);
+
+            Assert.IsType<ItemRosterBinaryPackage>(obj);
+
+            ItemRosterBinaryPackage returnedPackage = (ItemRosterBinaryPackage)obj;
+
+            ItemRoster newRoster = returnedPackage.Unpack<ItemRoster>();
+
+            //Assert Stuff
+            Assert.Equal(itemRoster, newRoster);
+        }
+    }
+}
---
+++ source/GameInterface/Serialization/Impl/EquipmentElementBinaryPackage.cs
@@ -0,0 +1,36 @@
+﻿using Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+
+namespace GameInterface.Serialization.Impl
+{
+    public class EquipmentElementBinaryPackage : BinaryPackageBase<EquipmentElement>
+    {
+        public EquipmentElementBinaryPackage(EquipmentElement obj, BinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+        {
+        }
+
+        public override void Pack()
+        {
+            foreach (FieldInfo field in ObjectType.GetAllInstanceFields())
+            {
+                object obj = field.GetValue(Object);
+                StoredFields.Add(field, BinaryPackageFactory.GetBinaryPackage(obj));
+            }
+        }
+
+        protected override void UnpackInternal()
+        {
+            TypedReference reference = __makeref(Object);
+            foreach (KeyValuePair<FieldInfo, IBinaryPackage> element in StoredFields)
+            {
+                element.Key.SetValueDirect(reference, element.Value.Unpack());
+            }
+        }
+    }
+}
