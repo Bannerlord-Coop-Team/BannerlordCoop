ea9d9150cab4a7fd9da6749f83895d5f4e507df2
Garrett Luskey <garrett.luskey@gmail.com>
2022-12-10 13:08:49 -0600

    backend updates with troop roster binary package

--- source/Common/Extentions.cs
+++ source/Common/Extentions.cs
@@ -8,5 +8,16 @@ namespace Common
 {
     public static class Extentions
     {
+        public static string ValuesToString<T>(this HashSet<T> hashset)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(var value in hashset)
+            {
+                stringBuilder.AppendLine(value.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
---
+++ source/GameInterface/Serialization/Impl/PartyBaseBinaryPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace GameInterface.Serialization.Impl
+{
+    /// <summary>
+    /// Binary package for PartyBase
+    /// </summary>
+    [Serializable]
+    public class PartyBaseBinaryPackage : BinaryPackageBase<PartyBase>
+    {
+        public PartyBaseBinaryPackage(PartyBase obj, BinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+        {
+        }
+
+        public override void Pack()
+        {
+            // TODO implement
+        }
+
+        protected override void UnpackInternal()
+        {
+            // TODO implement
+        }
+    }
+}
