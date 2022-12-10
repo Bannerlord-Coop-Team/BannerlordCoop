62a625bf147e476a21787eb15f2cef080138ee16
Garrett Luskey <garrett.luskey@gmail.com>
2022-12-10 17:30:43 -0600

    Initial commit

---
+++ source/GameInterface/Serialization/Impl/WorkshopBinaryPackage.cs
@@ -0,0 +1,37 @@
+﻿using Common.Extensions;
+using System;
+using System.Reflection;
+using TaleWorlds.CampaignSystem.Settlements.Workshops;
+using TaleWorlds.Library;
+
+namespace GameInterface.Serialization.Impl
+{
+    /// <summary>
+    /// Binary package for Workshop
+    /// </summary>
+    [Serializable]
+    public class WorkshopBinaryPackage : BinaryPackageBase<Workshop>
+    {
+        public WorkshopBinaryPackage(Workshop obj, BinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+        {
+        }
+
+        public override void Pack()
+        {
+            foreach (FieldInfo field in ObjectType.GetAllInstanceFields())
+            {
+                object obj = field.GetValue(Object);
+                StoredFields.Add(field, BinaryPackageFactory.GetBinaryPackage(obj));
+            }
+        }
+
+        protected override void UnpackInternal()
+        {
+            TypedReference reference = __makeref(Object);
+            foreach (FieldInfo field in StoredFields.Keys)
+            {
+                field.SetValueDirect(reference, StoredFields[field].Unpack());
+            }
+        }
+    }
+}
