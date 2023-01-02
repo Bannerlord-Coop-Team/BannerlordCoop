ff7c0a31402e548a5259f39ba74505c8cdc3322c
EgardA <veryhasted@gmail.com>
2023-01-02 21:30:56 +0100

    pause patch

---
+++ source/Missions/Patch/PausePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace Missions.Patch
+{
+    [HarmonyPatch(typeof(GameNetwork), "IsMultiplayer", MethodType.Getter)]
+    public class PausePatch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
