fc7a3b137b0edbbdc4e10cdd8f048ffa1e75314f
CielCat <development@cielcat.ch>
2023-02-04 18:07:11 +0100

    - removed Polly - refactored classes - added missing comments

--- source/Missions/Services/Agents/Messages/AgentMovementDelta.cs
+++ source/Missions/Services/Agents/Messages/AgentMovementDelta.cs
@@ -6,18 +6,26 @@ using TaleWorlds.MountAndBlade;
 namespace Missions.Services.Agents.Messages
 {
     /// <summary>
-    /// Represents the delta of an agent's movement in a set time limit.
+    /// Represents the delta of an agent's movement between the time it was created and the time it was last updated.
     /// </summary>
     internal sealed class AgentMovementDelta
     {
         private Vec3 _lookDirectionDelta = Vec3.Zero;
         private Vec2 _inputVectorDelta = Vec2.Zero;
 
-        private AgentActionData _actionDataDelta = null;
-        private AgentMountData _mountDataDelta = null;
+        private AgentActionData _actionDataDelta;
+        private AgentMountData _mountDataDelta;
 
         private Vec3 _currentPosition;
+
+        /// <summary>
+        /// Guid of the associated <see cref="Agent"/>.
+        /// </summary>
         public Guid Guid { get; }
+
+        /// <summary>
+        /// The <see cref="Agent"/> whose movement is about to be updated.
+        /// </summary>
         public Agent Agent { get; }
 
         /// <summary>
@@ -30,45 +38,64 @@ namespace Missions.Services.Agents.Messages
             Guid = guid;
             Agent = agent;
             _currentPosition = agent.Position;
+
+            _actionDataDelta = new AgentActionData(agent);
+            _mountDataDelta = new AgentMountData(agent);
         }
 
+        /// <summary>
+        /// Re-calculate this <see cref="AgentMovementDelta"/>.
+        /// </summary>
+        /// <param name="change"></param>
         public void CalculateMovement(LookDirectionChanged change)
         {
             _lookDirectionDelta += change.LookDirection;
-        }        
+        }
 
+        /// <summary>
+        /// Re-calculate this <see cref="AgentMovementDelta"/>.
+        /// </summary>
+        /// <param name="change"></param>
         public void CalculateMovement(MovementInputVectorChanged change)
         {
             _inputVectorDelta += change.InputVector;
         }
 
+        /// <summary>
+        /// Re-calculate this <see cref="AgentMovementDelta"/>.
+        /// </summary>
+        /// <param name="change"></param>
         public void CalculateMovement(ActionDataChanged change)
         {
-            if (_actionDataDelta is null)
-            {
-                _actionDataDelta = change.AgentActionData;
-            }
-            else
-            {
-                // TODO: find best way to apply delta
-            }
+            _actionDataDelta = change.AgentActionData;
         }
 
+        /// <summary>
+        /// Re-calculate this <see cref="AgentMovementDelta"/>.
+        /// </summary>
+        /// <param name="change"></param>
         public void CalculateMovement(MountDataChanged change)
         {
-            if (_mountDataDelta is null)
-            {
-                _mountDataDelta = change.AgentMountData;
-            }
-            else
-            {
-                // TODO: find best way to apply delta
-            }
+            _mountDataDelta = change.AgentMountData;
         }
 
-        public MovementPacket GetPacketFromDelta()
+        /// <summary>
+        /// Generate a <see cref="MovementPacket"/> from this <see cref="AgentMovementDelta"/>.
+        /// </summary>
+        /// <returns></returns>
+        public MovementPacket GetPacket()
         {
-            return new MovementPacket(Guid, new AgentData(_currentPosition, Agent.GetMovementDirection(), _lookDirectionDelta, _inputVectorDelta, null, _actionDataDelta, _mountDataDelta));
+            return new MovementPacket(
+                Guid, 
+                new AgentData(
+                    _currentPosition,
+                    Agent.GetMovementDirection(),
+                    _lookDirectionDelta, 
+                    _inputVectorDelta,
+                    new AgentEquipmentData(Agent),
+                    _actionDataDelta, 
+                    _mountDataDelta)
+                );
         }
     }
 }
--- source/Missions/Services/Agents/Packets/MovementHandler.cs
+++ source/Missions/Services/Agents/Packets/MovementHandler.cs
@@ -5,14 +5,10 @@ using LiteNetLib;
 using Missions.Services.Agents.Messages;
 using Missions.Services.Network;
 using Missions.Services.Network.Messages;
-using Polly;
-using Polly.RateLimit;
 using ProtoBuf;
 using Serilog;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -54,15 +50,15 @@ namespace Missions.Services.Agents.Packets
     public class MovementHandler : IPacketHandler, IDisposable
     {
         private const int PACKETS = 30;
-        private readonly static TimeSpan PACKET_TIME_SPAN = TimeSpan.FromSeconds(1);
+        private readonly static TimeSpan TIME_BETWEEN_PACKETS = TimeSpan.FromSeconds(1);
+
+        private static int PACKET_UPDATE_RATE = (int)Math.Round(TIME_BETWEEN_PACKETS.TotalMilliseconds / PACKETS);
 
         private static readonly ILogger Logger = LogManager.GetLogger<LiteNetP2PClient>();
 
         private readonly LiteNetP2PClient _client;
         private readonly IMessageBroker _messageBroker;
         private readonly INetworkAgentRegistry _agentRegistry;
-
-        private Dictionary<Guid, ISyncPolicy> _agentIdToPolicy = new Dictionary<Guid, ISyncPolicy>();
 
         private Dictionary<Guid, AgentMovementDelta> _agentMovementDeltas = new Dictionary<Guid, AgentMovementDelta>();
 
@@ -83,8 +79,8 @@ namespace Missions.Services.Agents.Packets
 
             _client.AddHandler(this);
 
-            // start the SendMessage every PACKET_TIME_SPAN.Seconds, PACKET_TIME_SPAN.Seconds after it was initialized
-            _senderTimer = new Timer(SendMessage, null, PACKET_TIME_SPAN.Seconds, PACKET_TIME_SPAN.Seconds);
+            // start the SendMessage every PACKET_UPDATE_RATE milliseconds, TIME_BETWEEN_PACKETS.Seconds after it was initialized
+            _senderTimer = new Timer(SendMessage, null, TIME_BETWEEN_PACKETS.Seconds, PACKET_UPDATE_RATE);
         }
 
         ~MovementHandler()
@@ -174,43 +170,8 @@ namespace Missions.Services.Agents.Packets
         {
             foreach (var delta in PopAllDeltas())
             {
-                SendPacket(delta.Guid, delta.GetPacketFromDelta());
+                _client.SendAll(delta.GetPacket());
             }
-        }
-
-        private void SendPacket(Guid guid, MovementPacket movementPacket)
-        {
-            // TODO: is Polly really necessary any more? => tendency is no
-            try
-            {
-                var policy = GetRateLimit(guid);
-                policy.Execute(() => _client.SendAll(movementPacket));
-            } 
-            catch (RateLimitRejectedException ex)
-            {
-                // this means we can't send any more packets,
-                // which is the desired behaviour in this case
-            }
-        }
-
-        private ISyncPolicy GetRateLimit(Guid guid)
-        {
-            if (_agentIdToPolicy.TryGetValue(guid, out var policy))
-            {
-                return policy;
-            }
-
-            // create a new instance of ISyncPolicy:
-            // the reason for this is that every agent should be able to send
-            // up to PACKETS in PACKET_TIME_SPAN.
-            //
-            // If we would reuse the same policy over and over again,
-            // we would only send PACKETS packets in PACKET_TIME_SPAN for all agents.
-            var newPolicy = Policy.RateLimit(PACKETS, PACKET_TIME_SPAN);
-
-            _agentIdToPolicy.Add(guid, newPolicy);
-
-            return newPolicy;
         }
 
         public void HandlePacket(NetPeer peer, IPacket packet)
