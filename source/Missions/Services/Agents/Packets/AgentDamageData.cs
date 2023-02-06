41c58048f2d65e9e90a767faa5412b3ee9f80cc4
masesk <radon5k@gmail.com>
2023-02-06 14:30:21 -0800

    init agent damage

---
+++ source/Missions/Services/Agents/Packets/AgentDamageData.cs
@@ -0,0 +1,29 @@
+﻿using Common.Messaging;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missions.Services.Agents.Packets
+{
+    public class AgentDamageData : INetworkEvent
+    {
+        public AgentDamageData(Guid sourceAgent, Guid targetAgent, double damage)
+        {
+            SourceAgent = sourceAgent;
+            TargetAgent = targetAgent;
+            Damage = damage;
+
+        }
+
+        [ProtoMember(1)]
+        public Guid SourceAgent { get; }
+        [ProtoMember(2)]
+        public Guid TargetAgent { get; }
+
+        [ProtoMember(3)]
+        public double Damage { get; }
+    }
+}
