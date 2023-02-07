67555cbcfec5cfada55c81b10459de5d05ea589e
Garrett Luskey <garrett.luskey@gmail.com>
2023-02-06 20:32:54 -0600

    Added Comments to Network Classes

--- source/Missions/Services/Network/NetworkAgentRegistry.cs
+++ source/Missions/Services/Network/NetworkAgentRegistry.cs
@@ -15,17 +15,70 @@ namespace Missions.Services.Network
 {
     public interface INetworkAgentRegistry
     {
+        /// <summary>
+        /// Used to easily convert Agent to Id
+        /// </summary>
+        /// <remarks>
+        /// Works with controlled and non-controlled agents
+        /// </remarks>
         IReadOnlyDictionary<Agent, Guid> AgentToId { get; }
+        /// <summary>
+        /// Agents directly controlled by the client
+        /// </summary>
         IReadOnlyDictionary<Guid, Agent> ControlledAgents { get; }
+        /// <summary>
+        /// Agents not controlled by the client.
+        /// These are controlled by network events.
+        /// </summary>
         IReadOnlyDictionary<NetPeer, AgentGroupController> OtherAgents { get; }
 
+        /// <summary>
+        /// Clears all data
+        /// </summary>
         void Clear();
+
+        /// <summary>
+        /// Register a controlled agent.
+        /// </summary>
+        /// <remarks>
+        /// A controlled agent is meant to be managed by this
+        /// client.
+        /// </remarks>
+        /// <param name="agentId">Id to assign the agent</param>
+        /// <param name="agent">Agent to add</param>
+        /// <returns>True if addition was successful, false otherwise</returns>
         bool RegisterControlledAgent(Guid agentId, Agent agent);
+
+        /// <summary>
+        /// Register a network controlled agent.
+        /// </summary>
+        /// <remarks>
+        /// A network controlled agent is meant to be managed across
+        /// the network.
+        /// </remarks>
+        /// <param name="peer">Peer that will control this agent</param>
+        /// <param name="agentId">Id assigned to the agent</param>
+        /// <param name="agent">Agent to register</param>
+        /// <returns>True if addition was successful, false otherwise</returns>
         bool RegisterNetworkControlledAgent(NetPeer peer, Guid agentId, Agent agent);
+
+        /// <summary>
+        /// Remove a controlled agent.
+        /// </summary>
+        /// <param name="agentId">Agent id to remove</param>
+        /// <returns>True if removal was successful, false otherwise</returns>
         bool RemoveControlledAgent(Guid agentId);
+
+        /// <summary>
+        /// Remove peer from network controllers.
+        /// This removes all controlled agents under this peer.
+        /// </summary>
+        /// <param name="peer">Peer to remove</param>
+        /// <returns>True if removal was successful, false otherwise</returns>
         bool RemovePeer(NetPeer peer);
     }
 
+    /// <inheritdoc/>
     public class NetworkAgentRegistry : INetworkAgentRegistry
     {
         private static readonly ILogger Logger = LogManager.GetLogger<NetworkAgentRegistry>();
@@ -43,12 +96,15 @@ namespace Missions.Services.Network
         }
         private static INetworkAgentRegistry _instance;
 
+        /// <inheritdoc/>
         public IReadOnlyDictionary<Agent, Guid> AgentToId => _agentToId;
         private readonly Dictionary<Agent, Guid> _agentToId = new Dictionary<Agent, Guid>();
 
+        /// <inheritdoc/>
         public IReadOnlyDictionary<Guid, Agent> ControlledAgents => _controlledAgents;
         private readonly Dictionary<Guid, Agent> _controlledAgents = new Dictionary<Guid, Agent>();
 
+        /// <inheritdoc/>
         public IReadOnlyDictionary<NetPeer, AgentGroupController> OtherAgents => _otherAgents;
         private readonly Dictionary<NetPeer, AgentGroupController> _otherAgents = new Dictionary<NetPeer, AgentGroupController>();
         private readonly IMessageBroker _messageBroker;
@@ -59,6 +115,7 @@ namespace Missions.Services.Network
             _messageBroker.Subscribe<AgentDeleted>(Handle_AgentDeleted);
         }
 
+        // TODO move to different file
         private void Handle_AgentDeleted(MessagePayload<AgentDeleted> payload)
         {
             Agent affectedAgent = payload.What.Agent;
@@ -68,6 +125,7 @@ namespace Missions.Services.Network
             }
         }
 
+        /// <inheritdoc/>
         public bool RegisterControlledAgent(Guid agentId, Agent agent)
         {
             if (_agentToId.ContainsKey(agent)) return false;
@@ -79,6 +137,7 @@ namespace Missions.Services.Network
             return true;
         }
 
+        /// <inheritdoc/>
         public bool RegisterNetworkControlledAgent(NetPeer peer, Guid agentId, Agent agent)
         {
             if (_otherAgents.TryGetValue(peer, out AgentGroupController controller))
@@ -100,6 +159,7 @@ namespace Missions.Services.Network
             return true;
         }
 
+        /// <inheritdoc/>
         public bool RemoveControlledAgent(Guid agentId)
         {
             if (_controlledAgents.TryGetValue(agentId, out Agent agent))
@@ -109,6 +169,7 @@ namespace Missions.Services.Network
             return false;
         }
 
+        /// <inheritdoc/>
         public bool RemoveNetworkControlledAgent(Guid agentId)
         {
             return _otherAgents.Values.Any(group =>
@@ -125,6 +186,7 @@ namespace Missions.Services.Network
             });
         }
 
+        /// <inheritdoc/>
         public bool RemovePeer(NetPeer peer)
         {
             bool result = true;
@@ -140,6 +202,7 @@ namespace Missions.Services.Network
             return result;
         }
 
+        /// <inheritdoc/>
         public void Clear()
         {
             _agentToId.Clear();
