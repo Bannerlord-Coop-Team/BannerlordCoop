a23d984eaa309e9d0c0de1d78d21498fb532751d
masesk <radon5k@gmail.com>
2023-02-08 11:06:33 -0800

    add comments

--- source/Missions/Services/Arena/Patches/ArenaPatches.cs
+++ source/Missions/Services/Arena/Patches/ArenaPatches.cs
@@ -16,11 +16,21 @@ namespace Missions.Services.Arena.Patches
     {
         static bool Prefix(Agent attacker, Agent victim, GameEntity realHitEntity, Blow b, ref AttackCollisionData collisionData, in MissionWeapon attackerWeapon, ref CombatLogData combatLogData)
         {
+            // first, check if the attacker exists in the agent to ID groud, if not, no networking is needed (not a network agent)
             if (!NetworkAgentRegistry.Instance.AgentToId.TryGetValue(attacker, out Guid attackerId)) return true;
+
+            // next, check if the attacker is one of ours, if not, no networking is needed (not our agent dealing damage)
             if (!NetworkAgentRegistry.Instance.ControlledAgents.ContainsKey(attackerId)) return true;
+
             AgentDamageData _agentDamageData;
+
+            // get the victim GUI
             NetworkAgentRegistry.Instance.AgentToId.TryGetValue(victim, out Guid victimId);
+
+            // construct a agent damage data
             _agentDamageData = new AgentDamageData(attackerId, victimId, b.InflictedDamage, collisionData, b);
+
+            // publish the event
             NetworkMessageBroker.Instance.PublishNetworkEvent(_agentDamageData);
 
             return true;
