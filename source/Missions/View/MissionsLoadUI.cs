914501b45c5927c7cf23c665ce468c30b6afe5b0
masesk <radon5k@gmail.com>
2023-02-17 00:07:42 -0800

    add comments

--- source/Missions/View/MissionsLoadUI.cs
+++ source/Missions/View/MissionsLoadUI.cs
@@ -9,7 +9,6 @@ using TaleWorlds.Core;
 using TaleWorlds.Engine.GauntletUI;
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
-using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View;
 using TaleWorlds.SaveSystem;
@@ -17,35 +16,67 @@ using TaleWorlds.SaveSystem.Load;
 using TaleWorlds.ScreenSystem;
 using TaleWorlds.TwoDimension;
 
-namespace Missions.Services.Arena.View
+namespace Missions.View
 {
-
-    class ArenaSelectedGameVM : SavedGameVM
+	/// <summary>
+	/// VM for the game selection for missions
+	/// </summary>
+    class MissionSelectedGameVM : SavedGameVM
 	{
+		/// <summary>
+		/// Class instance for when the game is deleted
+		/// </summary>
 		private readonly Action<SavedGameVM> _onDelete;
+
+		/// <summary>
+		/// Class instance for when the game is selected
+		/// </summary>
 		private readonly Action<SavedGameVM> _onSelection;
-		public ArenaSelectedGameVM(SaveGameFileInfo save, bool isSaving, Action<SavedGameVM> onDelete, Action<SavedGameVM> onSelection, Action onCancelLoadSave, Action onDone) :
+
+		/// <summary>
+		/// Selected save game VM for Missions
+		/// </summary>
+		/// <param name="save">Information of the game save</param>
+		/// <param name="isSaving">True if the user selected to save. Should always be false here</param>
+		/// <param name="onDelete">Callback when the save game is deleted</param>
+		/// <param name="onSelection">Callback when the save game is selected</param>
+		/// <param name="onCancelLoadSave">Callback when the save load is cancelled</param>
+		/// <param name="onDone">Callback when finished</param>
+		public MissionSelectedGameVM(SaveGameFileInfo save, bool isSaving, Action<SavedGameVM> onDelete, Action<SavedGameVM> onSelection, Action onCancelLoadSave, Action onDone) :
 			base(save, isSaving, onDelete, onSelection, onCancelLoadSave, onDone)
 		{
 			_onDelete = onDelete;
 			_onSelection = onSelection;
 		}
 
+		/// <summary>
+		/// Callback when the save is deleted. Passed in from the MissionLoadVM
+		/// </summary>
 		private new void ExecuteDelete()
 		{
 			_onDelete(this);
 		}
 
+		/// <summary>
+		/// Callback when the save is selected. Passed in from the MissionLoadVM
+		/// </summary>
 		private new void ExecuteSelection()
 		{
 			_onSelection(this);
 		}
 
+		/// <summary>
+		/// Callback when the save is loaded
+		/// </summary>
 		public new void ExecuteSaveLoad()
 		{
 			
 		}
 
+		/// <summary>
+		/// Callback when game is started
+		/// </summary>
+		/// <param name="loadResult"></param>
 		private void StartGame(LoadResult loadResult)
 		{
 			if (Game.Current != null)
@@ -57,10 +88,19 @@ namespace Missions.Services.Arena.View
 		}
 
 	}
-
-	class ArenaLoadUI : SaveLoadVM
+	/// <summary>
+	/// MissionLoadVM extends SaveLoadVM
+	/// </summary>
+	class MissionLoadVM : SaveLoadVM
 	{
+		/// <summary>
+		/// Class instance for save game load callback
+		/// </summary>
 		Action<SaveGameFileInfo> _loadAction;
+
+		/// <summary>
+		/// Hard coded list of approved modules
+		/// </summary>
 		private static readonly HashSet<string> allowedModules = new HashSet<string>()
 		{
 			"Native",
@@ -71,7 +111,15 @@ namespace Missions.Services.Arena.View
 			"BirthAndDeath",
 			"MissionTestMod",
 		};
-		public ArenaLoadUI(Action<SaveGameFileInfo> loadAction) : base(false, false)
+
+		/// <summary>
+		/// Loop through all save games
+		/// Determine is they are valid save games
+		/// They are valid save games if they contain the Coop Module AND do not 
+		/// contain any non-approved modules
+		/// </summary>
+		/// <param name="loadAction">Callback when the user selects to load the game</param>
+		public MissionLoadVM(Action<SaveGameFileInfo> loadAction) : base(false, false)
 		{
 			_loadAction = loadAction;
 			IsVisualDisabled = false;
@@ -98,7 +146,7 @@ namespace Missions.Services.Arena.View
                     }
 					if(!containsNonApprovedMods && containsCoop)
                     {
-						ArenaSelectedGameVM gameVm = new ArenaSelectedGameVM(save.Save, IsSaving, new Action<SavedGameVM>(OnDeleteSavedGame), new Action<SavedGameVM>(OnSaveSelection), new Action(OnCancelLoadSave), new Action(ExecuteDone));
+						MissionSelectedGameVM gameVm = new MissionSelectedGameVM(save.Save, IsSaving, new Action<SavedGameVM>(OnDeleteSavedGame), new Action<SavedGameVM>(OnSaveSelection), new Action(OnCancelLoadSave), new Action(ExecuteDone));
 						newSaves.Add(gameVm);
                     }
 					
@@ -116,9 +164,14 @@ namespace Missions.Services.Arena.View
 			RefreshValues();
 		}
 
+		/// <summary>
+		/// When a save is clicked on, this callback gets triggered
+		/// Sets the save selection in view
+		/// </summary>
+		/// <param name="saveGame">Click on save game</param>
 		private void OnSaveSelection(SavedGameVM saveGame)
 		{
-			ArenaSelectedGameVM save = (ArenaSelectedGameVM)saveGame;
+			MissionSelectedGameVM save = (MissionSelectedGameVM)saveGame;
 			if (save != CurrentSelectedSave)
 			{
 				if (CurrentSelectedSave != null)
@@ -137,7 +190,12 @@ namespace Missions.Services.Arena.View
 			}
 
 		}
-
+		/// <summary>
+		/// Call back for when the load save is clicked
+		/// This should invoke the passed passed in load callback
+		/// Generally, this triggers the next stage after the character selection as it returns
+		/// the save game back to the subscriber
+		/// </summary>
 		private new void ExecuteLoadSave()
 		{
 			SavedGameVM currentSelectedSave = CurrentSelectedSave;
@@ -148,15 +206,26 @@ namespace Missions.Services.Arena.View
 			if (_loadAction != null) _loadAction.Invoke(currentSelectedSave.Save);
 		}
 
+		/// <summary>
+		/// Done is pressed, load screen is popped
+		/// </summary>
 		private new void ExecuteDone()
 		{
 			ScreenManager.PopScreen();
 		}
 
+		/// <summary>
+		/// Callback for when the load is cancelled
+		/// </summary>
 		private void OnCancelLoadSave()
 		{
 		}
 
+
+		/// <summary>
+		/// Callback for game deletion. Disallow it for now.
+		/// </summary>
+		/// <param name="savedGame">Copy of the save game view</param>
 		private void OnDeleteSavedGame(SavedGameVM savedGame)
 		{
 			InformationManager.DisplayMessage(new InformationMessage("Cannot delete saves from here"));
@@ -166,26 +235,40 @@ namespace Missions.Services.Arena.View
 
 	}
 
-	class ArenaLoadScreen : SaveLoadScreen
+	/// <summary>
+	/// Mission load screen; extends SaveLoadScreen
+	/// </summary>
+	class MissionLoadScreen : SaveLoadScreen
 	{
-		public ArenaLoadScreen() : base(false)
+		public MissionLoadScreen() : base(false)
 		{
 		}
 	}
 
-	[OverrideView(typeof(ArenaLoadScreen))]
-	public class ArenaLoadGameGauntletScreen : ScreenBase
+	/// <summary>
+	/// MissionLoadGameGautletScreen extends the ScrenBase to contain the necessary information
+	/// Type is overrided to MissionLoadScreen base so it can be loaded as a scren
+	/// </summary>
+	[OverrideView(typeof(MissionLoadScreen))]
+	public class MissionLoadGameGauntletScreen : ScreenBase
 	{
-		public ArenaLoadGameGauntletScreen(Action<SaveGameFileInfo> loadAction) {
+		/// <summary>
+		/// Constructor with callback when the game is selected to be loaded
+		/// </summary>
+		/// <param name="loadAction">Callback when the game is selected to be loaded</param>
+		public MissionLoadGameGauntletScreen(Action<SaveGameFileInfo> loadAction) {
 			_loadAction = loadAction;
 		}
 
+		/// <summary>
+		/// Initialize the game load selection page as done by TW
+		/// </summary>
 		protected override void OnInitialize()
 		{
 			base.OnInitialize();
 
 			
-			_datasource = new ArenaLoadUI(_loadAction);
+			_datasource = new MissionLoadVM(_loadAction);
 			_gauntletLayer = new GauntletLayer(1, "GauntletLayer");
 			_gauntletLayer.LoadMovie("SaveLoadScreen", _datasource);
 			_gauntletLayer.InputRestrictions.SetInputRestrictions(true, InputUsageMask.All);
@@ -200,6 +283,9 @@ namespace Missions.Services.Arena.View
 			AddLayer(_gauntletLayer);
 		}
 
+		/// <summary>
+		/// Dereference all the initialized instances and remove layer
+		/// </summary>
 		protected override void OnFinalize()
 		{
 			base.OnFinalize();
@@ -207,14 +293,28 @@ namespace Missions.Services.Arena.View
 			_gauntletLayer = null;
 			_datasource = null;
 			_spriteCategory = null;
+			_loadAction = null;
 		}
 
+		/// <summary>
+		/// Class instance for the gautlet layer
+		/// </summary>
 		private GauntletLayer _gauntletLayer;
 
+		/// <summary>
+		/// Class instance for the SaveLoadVM
+		/// </summary>
 		private SaveLoadVM _datasource;
 
+		/// <summary>
+		/// Class instance for the sprite category used in the selection screen
+		/// </summary>
 		private SpriteCategory _spriteCategory;
 
+
+		/// <summary>
+		/// Class instance for the callback when a save is selected for loading
+		/// </summary>
 		private Action<SaveGameFileInfo> _loadAction;
 	}
 
