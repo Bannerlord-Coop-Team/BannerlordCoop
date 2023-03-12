fdc854adc0d09df586d92178062e579ebc3f6d51
Garrett Luskey <garrett.luskey@gmail.com>
2023-03-12 13:51:03 -0500

    Config fix along with registry framework

---
+++ source/Common.Tests/Registry/RegistryBaseTest.cs
@@ -0,0 +1,62 @@
+﻿namespace Common.Tests.Registry
+{
+    public class RegistryBaseTest
+    {
+        [Fact]
+        public void Register()
+        {
+            var registry = new RegistryBase<TestClass>();
+
+            Assert.True(registry.Register(new TestClass()));
+            Assert.Equal(1, registry.Count);
+        }
+
+        [Fact]
+        public void Register_ExistingObj()
+        {
+            var testClass = new TestClass();
+
+            var registry = new RegistryBase<TestClass>();
+
+            Assert.True(registry.Register(testClass));
+            Assert.False(registry.Register(testClass));
+        }
+
+        [Fact]
+        public void Remove_ByObj()
+        {
+            var testClass = new TestClass();
+
+            var registry = new RegistryBase<TestClass>();
+
+            Assert.True(registry.Register(testClass));
+            Assert.Equal(1, registry.Count);
+
+            Assert.True(registry.Remove(testClass));
+
+            Assert.Equal(0, registry.Count);
+        }
+
+        [Fact]
+        public void Remove_ById()
+        {
+            var testClass = new TestClass();
+
+            var registry = new RegistryBase<TestClass>();
+
+            Assert.True(registry.Register(testClass));
+            Assert.Equal(1, registry.Count);
+
+            Assert.True(registry.TryGetValue(testClass, out var id));
+
+            Assert.True(registry.Remove(id));
+
+            Assert.Equal(0, registry.Count);
+        }
+    }
+
+    internal class TestClass
+    {
+
+    }
+}
