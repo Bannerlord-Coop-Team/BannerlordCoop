c50df1c2465821bd2b870d30cbc15cbb0f1b5015
Garrett Luskey <garrett.luskey@gmail.com>
2023-04-17 10:40:32 -0500

    Update PeerReady.cs

--- source/Missions/Services/Network/Messages/PeerReady.cs
+++ source/Missions/Services/Network/Messages/PeerReady.cs
@@ -3,6 +3,9 @@ using LiteNetLib;
 
 namespace Missions.Services.Network.Messages
 {
+    /// <summary>
+    /// Event for when a peer events are able to be processed
+    /// </summary>
     public readonly struct PeerReady : IEvent
     {
         public NetPeer Peer { get; }
