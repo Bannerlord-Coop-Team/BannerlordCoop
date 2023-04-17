81f5d801cb7d12b407546a781acca75f2c3c57ae
EgardA <veryhasted@gmail.com>
2023-04-17 16:11:48 +0200

    AiAgentData class
    Introduces an AiAgentData class to simplify the Ai agent transfer

---
+++ source/Missions/Services/Network/Data/AiAgentData.cs
@@ -0,0 +1,29 @@
+﻿using ProtoBuf;
+using System;
+using TaleWorlds.Library;
+
+namespace Missions.Services.Network.Data
+{
+    /// <summary>
+    /// Data Class for AiAgent
+    /// </summary>
+    public class AiAgentData
+    {
+        public readonly Guid UnitId;
+        public readonly Vec3 UnitPosition;
+        public readonly string UnitIdString;
+        public readonly float UnitHealth;
+
+        public AiAgentData()
+        {
+        }
+
+        public AiAgentData(Guid unitId, Vec3 unitPosition, string unitIdString, float unitHealth)
+        {
+            UnitId = unitId;
+            UnitPosition = unitPosition;
+            UnitIdString = unitIdString;
+            UnitHealth = unitHealth;
+        }
+    }
+}
