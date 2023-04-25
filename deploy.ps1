62b4fd3bdfa16f0d0d8cad50cca3cc5d1f4728b8
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2023-04-25 08:04:04 +0200

    Finalize .NET Standard refactor

--- deploy.ps1
+++ deploy.ps1
@@ -14,9 +14,10 @@ $DeployDir      = "${BaseDir}deploy\"
 $ConfigPath     = "${BaseDir}config.json"
 $TemplateDir    = "${BaseDir}template"
 $UIMovieDir     = "${BaseDir}UIMovies"
+$MBBinDir       = "${BaseDir}\mb2\bin\Win64_Shipping_Client"
 
 # create output directory structure
-$DeployBinDir = "$DeployDir\mb2"
+$DeployBinDir = "${DeployDir}\mb2"
 
 Remove-Item ${DeployBinDir} -Recurse -ErrorAction Ignore
 
@@ -34,11 +35,14 @@ $subModuleContent = $subModuleContent.replace('${version}', $config.version)
 $subModuleContent = $subModuleContent.replace('${game_version}', $config.game_version)
 $subModuleContent | Out-File -Encoding utf8 -FilePath $DeployDir\SubModule.xml
 
-# copy all dlls
-Copy-Item mb2 -Force -Filter "TaleWorlds*.dll" -Destination $DeployDir -Recurse
-Copy-Item mb2 -Force -Filter "SandBox*.dll" -Destination $DeployDir -Recurse
+# copy mod dll
+Copy-Item -Force *.dll -Destination $DeployBinDir
 
-# remove empty folders
+# copy game dlls for deployment
+Copy-Item ${BaseDir}mb2 -Force -Filter "TaleWorlds*.dll" -Destination $DeployDir -Recurse
+Copy-Item ${BaseDir}mb2 -Force -Filter "SandBox*.dll" -Destination $DeployDir -Recurse
+
+# remove empty folders from deployment
 Get-ChildItem $DeployBinDir -Recurse -Force -Directory | 
     Sort-Object -Property FullName -Descending |
     Where-Object { $($_ | Get-ChildItem -Force | Select-Object -First 1).Count -eq 0 } |
@@ -48,10 +52,15 @@ Get-ChildItem $DeployBinDir -Recurse -Force -Directory |
 if(Test-Path (${BaseDir} + $config.modsDir))
 {
     $ModDir = ${BaseDir} + $config.modsDir + "\" + $config.name
-    Remove-Item ${ModDir} -Recurse -ErrorAction Ignore
+    Remove-Item "${ModDir}\*" -Recurse -Force -ErrorAction Ignore
     New-Item -Force -ItemType Directory -Path ${ModDir} | Out-Null
-    Copy-item -Force -Recurse $DeployDir\* -Destination $ModDir\
-    
+    New-Item -Force -ItemType Directory -Path "${ModDir}\bin" | Out-Null
+    New-Item -Force -ItemType Directory -Path "${ModDir}\bin\Win64_Shipping_Client" | Out-Null
+    $ModSourceDir = ${SolutionDir} + "\" + $config.name
+    Get-ChildItem -Path "${ModSourceDir}" -Filter "*.dll" -Recurse -ErrorAction Ignore | Where { $_.PSIsContainer -eq $false } | Copy-Item -Destination "$ModDir\bin\Win64_Shipping_Client"
+    $BindingRedirectFile = $config.name + ".dll.config"
+    Get-ChildItem -Path "${ModSourceDir}" -Filter $BindingRedirectFile -ErrorAction Ignore -Recurse | Copy-Item -Destination "${MBBinDir}\Bannerlord.exe.config"
+    Copy-Item -Force $DeployDir\SubModule.xml -Destination $ModDir\
 }
 
 # write Movie Prefabs
