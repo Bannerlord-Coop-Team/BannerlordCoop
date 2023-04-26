909ad9f46e7d4b7ab5b962aa6e4aa140cb9dec43
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2023-04-26 05:50:23 +0200

    Fix build errors

--- source/MissionTestMod/Properties/AssemblyInfo.cs
+++ source/MissionTestMod/Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MissionTestMod")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MissionTestMod")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,5 +26,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
