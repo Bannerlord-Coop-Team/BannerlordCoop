8ddce05a16deaf967458ceed4fe6aaa24b22d41a
Garrett Luskey <garrett.luskey@gmail.com>
2023-05-23 19:36:07 -0500

    Added examples to proto contract

--- source/Coop.Core/Templates/ServiceTemplate/Messages/NetworkMessageTemplate.cs
+++ source/Coop.Core/Templates/ServiceTemplate/Messages/NetworkMessageTemplate.cs
@@ -6,5 +6,9 @@ namespace Coop.Core.Templates.ServiceTemplate.Messages
     [ProtoContract]
     public record NetworkMessageTemplate : IEvent
     {
+        [ProtoMember(1)]
+        public string SomeData { get; } // TODO Rename/remove example data
+        [ProtoMember(2)]
+        public int SomeOtherData { get; } // TODO Rename/remove example data
     }
 }
