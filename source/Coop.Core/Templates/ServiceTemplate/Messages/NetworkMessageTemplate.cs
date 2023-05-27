0543d1a4881133dea2e64de8bf3fd9112b2a6382
Garrett Luskey <garrett.luskey@gmail.com>
2023-05-27 17:28:16 -0500

    Service Template (#620)
    * initial service template
    
    * removed old guid code
    
    * Added examples to proto contract

---
+++ source/Coop.Core/Templates/ServiceTemplate/Messages/NetworkMessageTemplate.cs
@@ -0,0 +1,14 @@
+﻿using Common.Messaging;
+using ProtoBuf;
+
+namespace Coop.Core.Templates.ServiceTemplate.Messages
+{
+    [ProtoContract]
+    public record NetworkMessageTemplate : IEvent
+    {
+        [ProtoMember(1)]
+        public string SomeData { get; } // TODO Rename/remove example data
+        [ProtoMember(2)]
+        public int SomeOtherData { get; } // TODO Rename/remove example data
+    }
+}
