df406895378a04355d09779d5fdadf725acb8d9b
7H30D0R <marcusanholt@gmail.com>
2023-05-29 13:05:34 +0200

    Add module mismatch popup skip

---
+++ source/GameInterface/Services/GameDebug/Patches/ModuleMismatchInquiryPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using SandBox;
+using System.Collections;
+
+#if DEBUG
+namespace GameInterface.Services.GameDebug.Patches
+{
+    /// <summary>
+    /// Skip module mismatch popup when loading save (debug only)
+    /// </summary>
+    [HarmonyPatch(typeof(SandBoxSaveHelper))]
+    internal class ModuleMismatchInquiryPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("CheckModules")]
+        private static void CheckModulePostfix(ref IList __result)
+        {
+            // Result contains list of module changes, clear this to emulate compatibility.
+            __result.Clear();
+        }
+    }
+}
+#endif
