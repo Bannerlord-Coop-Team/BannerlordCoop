9a2ce19f0060f158cbcb08e94ca70d554efbe225
Garrett Luskey <garrett.luskey@gmail.com>
2023-06-03 11:21:26 -0500

    Update TestEnvironmentModule.cs

--- source/Coop.IntegrationTests/Environment/TestEnvironmentModule.cs
+++ source/Coop.IntegrationTests/Environment/TestEnvironmentModule.cs
@@ -1,35 +1,35 @@
-﻿using Autofac;
-using GameInterface.Serialization;
-using GameInterface.Services.ObjectManager;
-using GameInterface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Autofac;
+//using GameInterface.Serialization;
+//using GameInterface.Services.ObjectManager;
+//using GameInterface;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Coop.IntegrationTests.Environment;
+//namespace Coop.IntegrationTests.Environment;
 
-internal class TestEnvironmentModule : Module
-{
-    protected override void Load(ContainerBuilder builder)
-    {
-        base.Load(builder);
-        builder.RegisterType<GameInterface>().As<IGameInterface>().SingleInstance().AutoActivate();
-        builder.RegisterType<MBObjectManagerAdapter>().As<IObjectManager>().InstancePerLifetimeScope();
-        builder.RegisterType<BinaryPackageFactory>().As<IBinaryPackageFactory>().InstancePerLifetimeScope();
-        builder.RegisterModule<ServiceModule>();
+//internal class TestEnvironmentModule : Module
+//{
+//    protected override void Load(ContainerBuilder builder)
+//    {
+//        base.Load(builder);
+//        builder.RegisterType<GameInterface>().As<IGameInterface>().SingleInstance().AutoActivate();
+//        builder.RegisterType<MBObjectManagerAdapter>().As<IObjectManager>().InstancePerLifetimeScope();
+//        builder.RegisterType<BinaryPackageFactory>().As<IBinaryPackageFactory>().InstancePerLifetimeScope();
+//        builder.RegisterModule<ServiceModule>();
 
-        builder.RegisterType<MobilePartyRegistry>()
-               .As<IMobilePartyRegistry>()
-               .InstancePerLifetimeScope();
+//        builder.RegisterType<MobilePartyRegistry>()
+//               .As<IMobilePartyRegistry>()
+//               .InstancePerLifetimeScope();
 
-        builder.RegisterType<HeroRegistry>()
-               .As<IHeroRegistry>()
-               .InstancePerLifetimeScope();
+//        builder.RegisterType<HeroRegistry>()
+//               .As<IHeroRegistry>()
+//               .InstancePerLifetimeScope();
 
-        builder.RegisterType<ControlledEntityRegistry>()
-               .As<IControlledEntityRegistry>()
-               .InstancePerLifetimeScope();
-    }
-}
+//        builder.RegisterType<ControlledEntityRegistry>()
+//               .As<IControlledEntityRegistry>()
+//               .InstancePerLifetimeScope();
+//    }
+//}
