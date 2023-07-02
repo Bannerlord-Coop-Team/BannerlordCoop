ef77b42604374fb6e6271b5777ec571eb325db2b
Garrett Luskey <garrett.luskey@gmail.com>
2023-07-02 18:06:38 -0500

    Added allowed instance from battles

---
+++ source/Common/Util/AllowedInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Common.Util;
+
+/// <summary>
+/// Class allowing for atomic operations on the given object
+/// Primarily used for overriding values during a Harmony patch
+/// </summary>
+/// <typeparam name="T">Type to allow state changing</typeparam>
+public class AllowedInstance<T> : IDisposable where T : class
+{
+    private readonly static SemaphoreSlim _sem = new SemaphoreSlim(1);
+    public T Instance { get; private set; }
+    public AllowedInstance(T instance)
+    {
+        _sem.Wait();
+        Instance = instance;
+    }
+
+    ~AllowedInstance() => Dispose();
+
+    public void Dispose()
+    {
+        _sem.Release();
+        Instance = null;
+    }
+}
