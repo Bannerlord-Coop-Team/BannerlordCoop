c38b9d219f6ed094049abb5b33ffd2c6f2df94c6
EgardA <veryhasted@gmail.com>
2023-07-10 23:05:47 +0200

    Some UI says it changes but other not

---
+++ source/GameInterface/Services/Settlements/Handlers/SettlementHandler.cs
@@ -0,0 +1,59 @@
+﻿using Common.Logging;
+using Common.Messaging;
+using GameInterface.Services.ObjectManager;
+using GameInterface.Services.Settlements.Messages;
+using GameInterface.Services.Settlements.Patches;
+using Serilog;
+using System.Runtime.Serialization;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace GameInterface.Services.Settlements.Handlers
+{
+    /// <summary>
+    /// GameInterface Settlement handler
+    /// </summary>
+    public class SettlementHandler : IHandler
+    {
+        private readonly IMessageBroker messageBroker;
+        private readonly IObjectManager objectManager;
+
+        public SettlementHandler(IMessageBroker messageBroker, IObjectManager objectManager)
+        {
+            this.messageBroker = messageBroker;
+            this.objectManager = objectManager;
+            messageBroker.Subscribe<ChangeSettlementOwnership>(Handle);
+        }
+
+        public void Dispose()
+        {
+            messageBroker.Unsubscribe<ChangeSettlementOwnership>(Handle);
+        }
+
+        private void Handle(MessagePayload<ChangeSettlementOwnership> obj)
+        {
+            var payload = obj.What;
+
+            if (objectManager.TryGetObject(payload.SettlementId, out Settlement settlement) == false)
+            {
+                return;
+            }
+
+            if (objectManager.TryGetObject(payload.OwnerId, out Hero owner) == false)
+            {
+                return;
+            }
+
+            if (objectManager.TryGetObject(payload.CapturerId, out Hero capturer) == false)
+            {
+                return;
+            }
+
+            ChangeOwnerOfSettlementPatch.RunOriginalApplyInternal(settlement, owner, capturer, 
+                (ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail)payload.Detail);
+
+        }
+    }
+}
---
+++ source/GameInterface/Services/Settlements/Patches/ChangeOwnerOfSettlementPatch.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using Common.Messaging;
+using Common.Util;
+using GameInterface.Services.Settlements.Messages;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Library.NewsManager;
+using static TaleWorlds.CampaignSystem.Actions.ChangeOwnerOfSettlementAction;
+
+namespace GameInterface.Services.Settlements.Patches
+{
+    /// <summary>
+    /// Patches ownership changes of settlements
+    /// </summary>
+    [HarmonyPatch(typeof(ChangeOwnerOfSettlementAction), "ApplyInternal")]
+    public class ChangeOwnerOfSettlementPatch
+    {
+        private static readonly AllowedInstance<Settlement> AllowedInstance = new AllowedInstance<Settlement>();
+        private static readonly MethodInfo _applyInternal = typeof(ChangeOwnerOfSettlementAction).GetMethod("ApplyInternal", BindingFlags.NonPublic | BindingFlags.Static);
+    
+        public static bool Prefix(Settlement settlement, Hero newOwner, Hero capturerHero, ChangeOwnerOfSettlementDetail detail)
+        {
+            if (AllowedInstance.IsAllowed(settlement)) return true;
+
+            MessageBroker.Instance.Publish(settlement, 
+                new LocalSettlementOwnershipChange(settlement.StringId, newOwner.StringId, capturerHero.StringId, Convert.ToInt32(detail)));
+
+            return false;
+        }
+    
+        public static void RunOriginalApplyInternal(Settlement settlement, Hero newOwner, Hero capturerHero, ChangeOwnerOfSettlementDetail detail)
+        {
+            using (AllowedInstance)
+            {
+                AllowedInstance.Instance = settlement;
+
+                GameLoopRunner.RunOnMainThread(() =>
+                {
+                    _applyInternal.Invoke(null, new object[] { settlement, newOwner, capturerHero, detail });
+                }, true);
+            }
+        }
+    
+    }
+}
