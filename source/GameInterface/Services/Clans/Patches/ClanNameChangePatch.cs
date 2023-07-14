71185a5e8af1a4723ca9fa2af68a3a4d3ba4c242
EgardA <veryhasted@gmail.com>
2023-07-14 14:11:29 +0200

    Update ClanNameChangePatch.cs

--- source/GameInterface/Services/Clans/Patches/ClanNameChangePatch.cs
+++ source/GameInterface/Services/Clans/Patches/ClanNameChangePatch.cs
@@ -4,13 +4,9 @@ using Common.Messaging;
 using Common.Util;
 using GameInterface.Services.Clans.Messages;
 using HarmonyLib;
-using SandBox.CampaignBehaviors;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Localization;
 
 namespace GameInterface.Services.Clans.Patches
@@ -18,11 +14,11 @@ namespace GameInterface.Services.Clans.Patches
     [HarmonyPatch(typeof(Clan), nameof(Clan.ChangeClanName))]
     public class ClanNameChangePatch
     {
-        private static MethodInfo Clan_Name => typeof(Clan).GetProperty(nameof(Clan.Name)).GetSetMethod();
+        private static MethodInfo Clan_Name = typeof(Clan).GetProperty(nameof(Clan.Name)).GetSetMethod(true);
         private static readonly Action<Clan, TextObject> Clan_Name_Setter = Clan_Name.BuildDelegate<Action<Clan, TextObject>>();
 
         static bool Prefix(ref Clan __instance, TextObject name, TextObject informalName)
-        {            
+        {
             MessageBroker.Instance.Publish(__instance, new ClanNameChange(__instance, name.ToString(), informalName.ToString()));
 
             return false;
