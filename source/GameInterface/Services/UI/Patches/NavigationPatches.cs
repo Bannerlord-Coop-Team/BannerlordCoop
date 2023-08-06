86dea0ab40561592fee309d82d3196bc6f3d0e27
EgardA <veryhasted@gmail.com>
2023-08-06 10:41:38 +0200

    Create NavigationPatches.cs

---
+++ source/GameInterface/Services/UI/Patches/NavigationPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.GameState;
+using TaleWorlds.Core;
+
+namespace GameInterface.Services.UI.Patches
+{
+    [HarmonyPatch(typeof(GameStateManager))]
+    internal class NavigationPatches
+    {
+        [HarmonyPatch("PushState")]
+        [HarmonyPrefix]
+        public static bool PushStatePatch(TaleWorlds.Core.GameState gameState)
+        {
+            return gameState switch
+            {
+                ClanState => false,
+                KingdomState => false,
+                QuestsState => false,
+                CharacterDeveloperState => false,
+                PartyState => false,
+                InventoryState => false,
+                _ => true,
+            };
+        }
+    }
+}
