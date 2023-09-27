bf958450f3ceab8add4865b2fd529a2a83dd3f6c
Alexander Egard <veryhasted@gmail.com>
2023-09-27 10:23:07 +0200

    Nameplates & Visibility Fix

---
+++ source/GameInterface/Services/MobileParties/Patches/PartyVisibleOnServerPatch.cs
@@ -0,0 +1,28 @@
+﻿using Common.Messaging;
+using GameInterface.Services.MobileParties.Messages.Control;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem;
+
+namespace GameInterface.Services.MobileParties.Patches
+{
+    /// <summary>
+    /// 
+    /// Parties are always visible on server
+    /// 
+    /// </summary>
+    [HarmonyPatch(typeof(MobileParty), nameof(MobileParty.IsVisible), MethodType.Getter)]
+    internal class PartyVisibleOnServerPatch
+    {
+        private static void Postfix(ref bool __result)
+        {
+            if(ModInformation.IsServer)
+            {
+                __result = true;
+            }
+        }
+    }
+}
