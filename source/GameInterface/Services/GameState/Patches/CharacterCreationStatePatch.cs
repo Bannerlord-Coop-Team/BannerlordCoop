b70281cc380ef7f39ac1961209fa59247123ac08
Martin Prejsa <martinprejsa@duck.com>
2023-11-29 17:06:48 -0800

    Skip Character Creation Options page
    Move CharacterCreationFinishedPatch.cs -> CharacterCreationStatePatch.cs, renamed functions and objects inside, created NextStagePostfix to handle option page skip

---
+++ source/GameInterface/Services/GameState/Patches/CharacterCreationStatePatch.cs
@@ -0,0 +1,27 @@
+﻿using Common.Messaging;
+using GameInterface.Services.CharacterCreation.Messages;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
+
+namespace GameInterface.Services.GameState.Patches;
+
+[HarmonyPatch(typeof(CharacterCreationState))]
+internal class CharacterCreationStatePatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(CharacterCreationState.FinalizeCharacterCreation))]
+    private static void FinalizeCharacterCreationPostfix(ref CharacterCreationState __instance)
+    {
+        MessageBroker.Instance.Publish(__instance, new CharacterCreationFinished());
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(CharacterCreationState.NextStage))]
+    private static void NextStagePostfix(ref CharacterCreationState __instance)
+    {
+        if (__instance.CurrentStage is CharacterCreationOptionsStage)
+        {
+            __instance.FinalizeCharacterCreation();
+        }
+    }
+}
