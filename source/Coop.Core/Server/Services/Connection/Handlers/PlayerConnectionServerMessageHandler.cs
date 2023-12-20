1b0c573ea267d533ff988a6c1db6174c4d84b441
Alexander Egard <veryhasted@gmail.com>
2023-12-20 22:37:47 +0100

    Move from Common to Server

--- source/Coop.Core/Server/Services/Connection/Handlers/PlayerConnectionServerMessageHandler.cs
+++ source/Coop.Core/Server/Services/Connection/Handlers/PlayerConnectionServerMessageHandler.cs
@@ -6,19 +6,22 @@ using Coop.Core.Server.Connections;
 using Coop.Core.Server.Connections.Messages;
 using Coop.Core.Server.Services.Time.Messages;
 using GameInterface.Services.GameDebug.Messages;
+using GameInterface.Services.Heroes.Messages;
 using LiteNetLib;
 using Serilog;
 using System.Linq;
 
-namespace Coop.Core.Common.Services.Connection.Handlers
+namespace Coop.Core.Server.Services.Connection.Handlers
 {
-    public class PlayerConnectionServerMessageHandler
+    public class PlayerConnectionServerMessageHandler : IHandler
     {
         private static readonly ILogger Logger = LogManager.GetLogger<PlayerConnectionServerMessageHandler>();
 
         private readonly IMessageBroker messageBroker;
         private readonly IClientRegistry clientRegistry;
         private readonly INetwork network;
+
+        private string unpauseReadyMessage = "All players connected, game can now be un-paused";
 
         public PlayerConnectionServerMessageHandler(IMessageBroker messageBroker, IClientRegistry clientRegistry, INetwork network)
         {
@@ -29,10 +32,18 @@ namespace Coop.Core.Common.Services.Connection.Handlers
             messageBroker.Subscribe<NetworkPlayerCampaignEntered>(PlayerCampaignEnteredHandler);
             messageBroker.Subscribe<PlayerConnected>(PlayerConnectedHandler);
             messageBroker.Subscribe<NetworkRequestTimeSpeedChange>(NetworkRequestTimeSpeedChange);
-            messageBroker.Subscribe<NetworkConnected>(Handle_NetworkConnected);
+            messageBroker.Subscribe<PackageGameSaveData>(Handle_NetworkConnected);
         }
 
-        internal void Handle_NetworkConnected(MessagePayload<NetworkConnected> obj)
+        public void Dispose()
+        {
+            messageBroker.Unsubscribe<NetworkPlayerCampaignEntered>(PlayerCampaignEnteredHandler);
+            messageBroker.Unsubscribe<PlayerConnected>(PlayerConnectedHandler);
+            messageBroker.Unsubscribe<NetworkRequestTimeSpeedChange>(NetworkRequestTimeSpeedChange);
+            messageBroker.Unsubscribe<PackageGameSaveData>(Handle_NetworkConnected);
+        }
+
+        internal void Handle_NetworkConnected(MessagePayload<PackageGameSaveData> obj)
         {
             messageBroker.Publish(this, new SendInformationMessage("A new player is joining the game, pausing"));
         }
@@ -43,8 +54,8 @@ namespace Coop.Core.Common.Services.Connection.Handlers
 
             if (!clientRegistry.PlayersLoading)
             {
-                messageBroker.Publish(this, new SendInformationMessage("All players connected, game can now be un-paused"));
-                network.SendAllBut(playerId, new SendInformationMessage("All players connected, game can now be un-paused"));
+                messageBroker.Publish(this, new SendInformationMessage(unpauseReadyMessage));
+                network.SendAllBut(playerId, new SendInformationMessage(unpauseReadyMessage));
             }
         }
         private void PlayerConnectedHandler(MessagePayload<PlayerConnected> obj)
@@ -56,8 +67,11 @@ namespace Coop.Core.Common.Services.Connection.Handlers
             if (AnyLoaders())
             {
                 int loadingPeers = clientRegistry.LoadingPeers.Count;
-                messageBroker.Publish(this, new SendInformationMessage("Pausing disabled, " + loadingPeers + " player(s) are currently joining the game"));
-                network.SendAll(new SendInformationMessage("Pausing disabled, " + loadingPeers + " player(s) are currently joining the game"));
+
+                string loadingMessage = "Pausing disabled, " + loadingPeers + " player(s) are currently joining the game";
+
+                messageBroker.Publish(this, new SendInformationMessage(loadingMessage));
+                network.SendAll(new SendInformationMessage(loadingMessage));
             }
         }
 
