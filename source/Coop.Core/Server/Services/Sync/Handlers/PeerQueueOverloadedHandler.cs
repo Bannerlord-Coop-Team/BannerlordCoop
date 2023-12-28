0985ce3901c063047c689138c60f116ecb2224c1
Martin Prejsa <martinprejsa@duck.com>
2023-12-28 11:14:11 +0100

    Code revisions according to PR comments

--- source/Coop.Core/Server/Services/Sync/Handlers/PeerQueueOverloadedHandler.cs
+++ source/Coop.Core/Server/Services/Sync/Handlers/PeerQueueOverloadedHandler.cs
@@ -5,25 +5,24 @@ using Coop.Core.Server.Services.Time.Handlers;
 using Coop.Core.Server.Services.Time.Messages;
 using GameInterface.Services.GameDebug.Messages;
 using GameInterface.Services.Heroes.Enum;
-using GameInterface.Services.Heroes.Messages;
 using LiteNetLib;
 using System;
 using System.Collections.Generic;
-using System.Threading;
-
 namespace Coop.Core.Server.Services.Sync.Handlers
 {
     internal class PeerQueueOverloadedHandler : IHandler
     {
+        private static readonly long POLL_INTERVAL = 100;
+
         private readonly IMessageBroker messageBroker;
         private readonly INetwork network;
 
         private readonly Poller poller;
         private readonly HashSet<NetPeer> overloadedPeers;
 
-        private TimeControlEnum originalSpeed;
-
         private readonly TimeHandler timeHandler;
+
+        private TimeControlEnum originalSpeed;
 
         public PeerQueueOverloadedHandler(
             IMessageBroker messageBroker, 
@@ -36,30 +35,30 @@ namespace Coop.Core.Server.Services.Sync.Handlers
             this.timeHandler = timeHandler;
 
             overloadedPeers = new();
-            poller = new(Poll, TimeSpan.FromMilliseconds(100));
+            poller = new(Poll, TimeSpan.FromMilliseconds(POLL_INTERVAL));
 
             messageBroker.Subscribe<PeerQueueOverloaded>(Handle);
 
             poller.Start();
         }
 
-        private void Handle(MessagePayload<PeerQueueOverloaded> payload) 
+        private void Handle(MessagePayload<PeerQueueOverloaded> payload)
         {
-            Monitor.Enter(overloadedPeers);
-
-            if (overloadedPeers.Add(payload.What.NetPeer))
+            lock (overloadedPeers)
             {
-                Monitor.Exit(overloadedPeers);
-                //TODO: set originalSpeed
-                originalSpeed = TimeControlEnum.Play_1x;
+                if (overloadedPeers.Add(payload.What.NetPeer))
+                {
+                    //TODO: set originalSpeed
+                    originalSpeed = TimeControlEnum.Play_1x;
 
-                //TODO: check for loaders OR replace with helper
-                messageBroker.Publish(this, new SetTimeControlMode(originalSpeed));
-                network.SendAll(new NetworkTimeSpeedChanged(originalSpeed));
+                    timeHandler.SetTimeMode(TimeControlEnum.Pause);
 
-                var msg = new SendInformationMessage($"{overloadedPeers} clients are catching up, pausing");
-                messageBroker.Publish(this, msg);
-                network.SendAll(msg);
+                    var msg = new SendInformationMessage($"{overloadedPeers} clients are catching up, pausing");
+                    messageBroker.Publish(this, msg);
+                    network.SendAll(msg);
+
+                    poller.Start();
+                }
             }
         }
 
@@ -75,34 +74,30 @@ namespace Coop.Core.Server.Services.Sync.Handlers
                 }
             }
 
-            //TODO: maybe concurrent hashset?
-            Monitor.Enter(overloadedPeers);
             if (toRemove.Count == 0)
             {
-                Monitor.Exit(overloadedPeers);
                 return;
             }
 
-            foreach (var item in toRemove)
+            lock (overloadedPeers)
             {
-                overloadedPeers.Remove(item);
+                foreach (var item in toRemove)
+                {
+                    overloadedPeers.Remove(item);
+                }
+
+                if (overloadedPeers.Count == 0)
+                {
+                    timeHandler.SetTimeMode(originalSpeed);
+
+                    var msg = new SendInformationMessage("All clients synchronized, resuming");
+                    messageBroker.Publish(this, msg);
+                    network.SendAll(msg);
+
+                    poller.Stop();
+                    return;
+                }
             }
-
-            if (overloadedPeers.Count == 0)
-            {
-                Monitor.Exit(overloadedPeers);
-
-                //TODO: check for loaders OR replace with helper
-                messageBroker.Publish(this, new SetTimeControlMode(originalSpeed));
-                network.SendAll(new NetworkTimeSpeedChanged(originalSpeed));
-
-                var msg = new SendInformationMessage("All clients synchronized, resuming");
-                messageBroker.Publish(this, msg);
-                network.SendAll(msg);
-
-                return;
-            }
-            Monitor.Exit(overloadedPeers);
         }
 
         public void Dispose()
