841227f413771b01611da73e308b80de080e6aa7
Alexander Egard <veryhasted@gmail.com>
2023-12-30 13:15:22 +0100

    Enable bandit conversations

--- source/GameInterface/Services/Bandits/Patches/DisableBanditsCampaignBehavior.cs
+++ source/GameInterface/Services/Bandits/Patches/DisableBanditsCampaignBehavior.cs
@@ -6,6 +6,10 @@ namespace GameInterface.Services.Bandits.Patches;
 [HarmonyPatch(typeof(BanditsCampaignBehavior))]
 internal class DisableBanditsCampaignBehavior
 {
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.RegisterEvents))]
+    [HarmonyPatch(nameof(BanditsCampaignBehavior.OnSettlementEntered))]
+    [HarmonyPatch(nameof(BanditsCampaignBehavior.WeeklyTick))]
+    [HarmonyPatch(nameof(BanditsCampaignBehavior.DailyTick))]
+    [HarmonyPatch(nameof(BanditsCampaignBehavior.HourlyTick))]
+    [HarmonyPatch("OnNewGameCreatedPartialFollowUp")]
     static bool Prefix() => false;
 }
