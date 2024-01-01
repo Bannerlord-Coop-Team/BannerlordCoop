bc1b5cb8e5f930665d473b6cc68f4021db6bed8d
7H30D0R <marcusanholt@gmail.com>
2024-01-01 17:16:22 +0100

    Enable game ticks with menus open

--- source/GameInterface/Services/Time/Patches/DisableGameMenuPausePatches.cs
+++ source/GameInterface/Services/Time/Patches/DisableGameMenuPausePatches.cs
@@ -1,20 +1,21 @@
 ﻿using GameInterface.Services.Heroes.Patches;
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Reflection;
-using System.Text;
 using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.CampaignSystem;
-using System.Xml.Serialization;
+using TaleWorlds.CampaignSystem.GameState;
+using TaleWorlds.Core;
 
 namespace GameInterface.Services.Time.Patches;
 
 [HarmonyPatch(typeof(GameMenu))]
-internal static class DisabeGameMenuPausePatches
+internal static class DisableGameMenuPausePatches
 {
+    private static readonly MethodInfo MapState_OnTick = typeof(MapState).GetMethod("OnTick", BindingFlags.NonPublic | BindingFlags.Instance);
+
     [HarmonyPatch(nameof(GameMenu.ActivateGameMenu))]
     [HarmonyTranspiler]
     private static IEnumerable<CodeInstruction> ActivateGameMenuPatch(IEnumerable<CodeInstruction> instructions) => ReplaceTimeControlMode(instructions);
@@ -49,6 +50,53 @@ internal static class DisabeGameMenuPausePatches
             {
                 instr.opcode = OpCodes.Call;
                 instr.operand = deferFunction;
+            }
+        }
+
+        return instrs;
+    }
+
+    [HarmonyPatch(typeof(GameStateManager), nameof(GameStateManager.RegisterActiveStateDisableRequest))]
+    [HarmonyPrefix]
+    static bool RegisterActiveStateDisableRequestPatch()
+    {
+        return false;
+    }
+
+
+    [HarmonyPatch(typeof(GameStateManager), nameof(GameStateManager.OnTick))]
+    [HarmonyPrefix]
+    static void OnTickPatch(ref GameStateManager __instance, float dt)
+    {
+        if (!(__instance.ActiveState is MapState))
+        {
+            MapState mapState = __instance.LastOrDefault<MapState>();
+            if (mapState == null) return;
+
+            MapState_OnTick.Invoke(mapState, new object[] { dt });
+        }
+    }
+
+    // Remove "base.GameStateManager.ActiveState == this" condition from if statement
+    [HarmonyPatch(typeof(MapState), "OnMapModeTick")]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> OnMapModeTickPatch(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> instrs = instructions.ToList();
+
+        MethodInfo activeStateGetter = typeof(GameStateManager).GetProperty(nameof(GameStateManager.ActiveState)).GetGetMethod();
+
+        for (int i = 0; i < instrs.Count(); i++)
+        {
+            var instr = instrs[i];
+
+            if (instr.opcode == OpCodes.Callvirt &&
+               instr.operand as MethodInfo == activeStateGetter &&
+               instrs[i - 2].opcode == OpCodes.Ldarg_0 &&
+               instrs[i + 2].opcode == OpCodes.Bne_Un_S)
+            {
+                instrs.RemoveRange(i - 2, 5);
+                break;
             }
         }
 
