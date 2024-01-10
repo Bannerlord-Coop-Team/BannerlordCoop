8ffae349fc11c20a72bfae433a3b6a3c0d20988d
Martin Prejsa <martinprejsa@duck.com>
2024-01-10 19:56:14 +0100

    Update source/GameInterface/Services/ItemRosters/Handlers/ItemRosterUpdateHandler.cs
    Co-authored-by: Garrett Luskey <garrett.luskey@gmail.com>

--- source/GameInterface/Services/ItemRosters/Handlers/ItemRosterUpdateHandler.cs
+++ source/GameInterface/Services/ItemRosters/Handlers/ItemRosterUpdateHandler.cs
@@ -32,37 +32,40 @@ namespace GameInterface.Services.ItemRosters.Handlers
 
         public void Handle(MessagePayload<ItemRosterUpdate> payload)
         {
-            if (ModInformation.IsServer)
-                return;
-
-            ItemRoster roster = null;
-
-            if (objectManager.TryGetObject(payload.What.ItemID, out ItemObject item))
+            var msg = payload.What;
+            if (objectManager.TryGetObject(msg.ItemID, out ItemObject item) == false)
             {
-                ItemModifier modifier = null;
-                if (payload.What.ItemModifierID != null)
-                {
-                    if(!objectManager.TryGetObject(payload.What.ItemModifierID, out modifier))
-                    {
-                        logger.Error("Failed to update item roster, ItemModifier '{0}' not found", payload.What.ItemModifierID);
-                    }
-                }
-
-                if (objectManager.TryGetObject(payload.What.PartyBaseID, out Settlement s))
-                {
-                    roster = s.ItemRoster;
-                }
-                else if (objectManager.TryGetObject(payload.What.PartyBaseID, out MobileParty p))
-                {
-                    roster = p.ItemRoster;
-                }
-                else
-                {
-                    logger.Error("Failed to update item roster, no Settlement nor Party with ID '{0}' was found", payload.What.PartyBaseID);
-                }
-
-                ItemRosterPatch.AddToCountsOverride(roster, new EquipmentElement(item, modifier), payload.What.Amount);
+                Logger.Error("Unable to find item with id: {itemId}", msg.ItemID);
+                return;
             }
+
+            
+            ItemModifier modifier = null;
+            if (msg.ItemModifierID != null)
+            {
+                if(objectManager.TryGetObject(msg.ItemModifierID, out modifier) == false)
+                {
+                    Logger.Error("Failed to update item roster, ItemModifier '{itemModifierId}' not found", msg.ItemModifierID);
+                    return;
+                }
+            }
+
+            ItemRoster roster;
+            if (objectManager.TryGetObject(msg.PartyBaseID, out Settlement s))
+            {
+                roster = s.ItemRoster;
+            }
+            else if (objectManager.TryGetObject(msg.PartyBaseID, out MobileParty p))
+            {
+                roster = p.ItemRoster;
+            }
+            else
+            {
+                Logger.Error("Failed to update item roster, no Settlement nor Party with ID '{partyBaseId}' was found", msg.PartyBaseID);
+                return;
+            }
+
+            ItemRosterPatch.AddToCountsOverride(roster, new EquipmentElement(item, modifier), msg.Amount);
         }
 
         public void Dispose()
