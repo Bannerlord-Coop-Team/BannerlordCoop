fba9318448354c0c02980e0b8ee9802f39c7f6a1
Samuel Zurowski <samuelzurowski@gmail.com>
2024-01-13 21:46:11 -0500

    Patched and tested GameExited Event

---
+++ source/GameInterface.Tests/Bootstrap/Patches/GameExitedPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SandBox.View.Map;
+using Common.Messaging;
+using GameInterface.Services.GameState.Messages;
+
+namespace GameInterface.Tests.Bootstrap.Patches;
+
+[HarmonyPatch("OnExit")]
+internal class GameExitedPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("OnExit")]
+    static bool OnExit(ref MapScreen __instance)
+    {
+        return false;
+    }
+}
---
+++ source/GameInterface/Services/GameState/Patches/GameExitPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Messaging;
+using GameInterface.Services.GameState.Messages;
+using HarmonyLib;
+using SandBox.View.Map;
+
+namespace GameInterface.Services.GameState.Patches;
+
+
+[HarmonyPatch(typeof(MapScreen))]
+internal class GameExitPatch
+{
+    // Assume that the server is going to want to know that a player is exiting.
+    // Thus this seems more like a prefix. May want to change depending on networking model. 
+
+    [HarmonyPostfix]
+    [HarmonyPatch("OnExit")]
+    static void OnExit(ref MapScreen __instance)
+    {
+        MessageBroker.Instance.Publish(__instance, new GameExited());
+    }
+
+}
