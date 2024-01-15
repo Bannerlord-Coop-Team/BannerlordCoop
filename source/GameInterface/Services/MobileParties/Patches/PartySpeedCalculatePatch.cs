0016f4d05422b64bf1223bcc06af98ff182c50b8
Samuel Zurowski <samuelzurowski@gmail.com>
2024-01-14 20:36:35 -0500

    Found a way to hook players speed

---
+++ source/GameInterface/Services/MobileParties/Patches/PartySpeedCalculatePatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameInterface.Services.MobileParties.Extensions;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace GameInterface.Services.MobileParties.Patches
+{
+    [HarmonyPatch(typeof(MobileParty))]
+    internal class PartySpeedCalculatePatch
+    {
+
+        [HarmonyPrefix]
+        [HarmonyPatch("CalculateSpeed")]
+        static void CalculateSpeed(ref MobileParty __instance/*, ref float __result*/)
+        {
+            if(__instance.IsPartyControlled() && __instance.ToString().Contains("CoopParty"))
+            {
+                String s = __instance.ToString();
+                int i = 0;
+            }
+            //__result = 0.0f;
+
+        }
+
+    }
+}
