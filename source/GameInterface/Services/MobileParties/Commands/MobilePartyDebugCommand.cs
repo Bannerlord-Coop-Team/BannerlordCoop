216d16ccb7fd447c983e83ecde6a37dbe75f3eca
Samuel Zurowski <samuelzurowski@gmail.com>
2024-01-15 17:06:51 -0500

    Fixed typos in code and used Postfix patch

--- source/GameInterface/Services/MobileParties/Commands/MobilePartyDebugCommand.cs
+++ source/GameInterface/Services/MobileParties/Commands/MobilePartyDebugCommand.cs
@@ -28,29 +28,19 @@ namespace GameInterface.Services.MobileParties.Commands
             {
                 return string.Format("ID: '{0}' not found", args[0]);
             }
-            /*
-             * Here are some classes/methods to 
-             * give you a head start MobileParty.CalculateSpeedForPartyUnified, 
-             * DefaultPartySpeedCalculatingModel, and DefaultInventoryCapacityModel.
-             * My guess is that the player party does not have the same inventory or skills on the client/server
-             */
+  
             FieldInfo typeMobileParty = typeof(MobileParty).GetField("_lastCalculatedSpeed", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
             var _lastCalculatedSpeed = typeMobileParty.GetValue(mobileParty);
-
-
-
-            // FieldInfo typeHero = typeof(Hero).GetField("_characterAttributes", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            // var _characterAttributes = typeHero.GetValue(mobileParty.Owner);  
 
             String skillsStr = "";
             foreach(SkillObject skill in Skills.All)
             {
                 int skillValue = owner.GetSkillValue(skill);
-                skillsStr += String.Format("'{0}': {1}\n", skill.StringId, skillValue);
+                skillsStr += String.Format("\n'{0}': {1}", skill.StringId, skillValue);
             }
             //owner.GetTraitLevel(TraitObject.)
             String explanations = mobileParty.SpeedExplained.GetExplanations();
-            return String.Format("MobileParty info for: '{0}':\nStringID: {1}\nSpeed: {2}\nDefaultInventoryCapacityModel: {3}\nWeight Carried: {4}\nLastCaulated Speed: {5}\nBaseSpeed: {6}\nPlayer Skills\n Explanations:\n{7}",
+            return String.Format("MobileParty info for: '{0}':\nStringID: {1}\nSpeed: {2}\nDefaultInventoryCapacityModel: {3}\nWeight Carried: {4}\nLastCalculated Speed: {5}\nBaseSpeed: {6}\nPlayer Skills:\nExplanations:\n{7}\n",
                owner, mobileParty.StringId, mobileParty.Speed, mobileParty.InventoryCapacity, mobileParty.TotalWeightCarried, _lastCalculatedSpeed, skillsStr, explanations);
         }
     }
--- source/GameInterface/Services/MobileParties/Patches/CalculateBaseSpeedPatch.cs
+++ source/GameInterface/Services/MobileParties/Patches/CalculateBaseSpeedPatch.cs
@@ -1,11 +1,7 @@
 ﻿using GameInterface.Services.MobileParties.Extensions;
 using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using System.Text;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Party;
@@ -13,41 +9,21 @@ using TaleWorlds.Core;
 
 namespace GameInterface.Services.MobileParties.Patches;
 
-[HarmonyDebug]
 [HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel))]
-[HarmonyPatch(nameof(DefaultPartySpeedCalculatingModel.CalculateBaseSpeed))]
 internal class CalculateBaseSpeedPatch 
 {
-    //static FieldInfo f_someField = AccessTools.Field(typeof(SomeType), nameof(SomeType.someField));
-    //static MethodInfo m_MyExtraMethod = SymbolExtensions.GetMethodInfo(() => Tools.MyExtraMethod());
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        var instr = instructions.ToList();
-
-
-        instr[405].opcode = OpCodes.Nop;
-        instr[406].opcode = OpCodes.Nop;
-        instr[407].opcode = OpCodes.Nop;
-        instr[408].opcode = OpCodes.Nop;
-        instr[409].opcode = OpCodes.Nop;
-        instr[410].opcode = OpCodes.Nop;
-
-        return instr.AsEnumerable();
-        //yield return new CodeInstruction(OpCodes.Call, )
-    }
-
+    [HarmonyPatch(nameof(DefaultPartySpeedCalculatingModel.CalculateBaseSpeed))]
+    [HarmonyPostfix]
     private static void CalculateBaseSpeed(ref MobileParty mobileParty, ref ExplainedNumber __result)
     {
-        /*
         if(ModInformation.IsServer && mobileParty.IsPartyControlled() == false)
         {
             float playerMapMovementSpeedBonusMultiplier = Campaign.Current.Models.DifficultyModel.GetPlayerMapMovementSpeedBonusMultiplier();
             if (playerMapMovementSpeedBonusMultiplier > 0f)
             {
-                __result.AddFactor(playerMapMovementSpeedBonusMultiplier, new TaleWorlds.Localization.TextObject(0f));
+                __result.AddFactor(playerMapMovementSpeedBonusMultiplier, GameTexts.FindText("str_game_difficulty"));
             }
         }
-        */
     }
 }
 
