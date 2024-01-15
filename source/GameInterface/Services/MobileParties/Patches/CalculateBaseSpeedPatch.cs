7b4d3483f775810a0cc32498d7cf4592a784b5c4
Garrett Luskey <garrett.luskey@gmail.com>
2024-01-15 15:48:37 -0600

    GameTexts fix

--- source/GameInterface/Services/MobileParties/Patches/CalculateBaseSpeedPatch.cs
+++ source/GameInterface/Services/MobileParties/Patches/CalculateBaseSpeedPatch.cs
@@ -11,43 +11,62 @@ using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
 
-namespace GameInterface.Services.MobileParties.Patches
+namespace GameInterface.Services.MobileParties.Patches;
+
+[HarmonyDebug]
+[HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel))]
+[HarmonyPatch(nameof(DefaultPartySpeedCalculatingModel.CalculateBaseSpeed))]
+internal class CalculateBaseSpeedPatch 
 {
-    [HarmonyDebug]
-    [HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel))]
-    [HarmonyPatch(nameof(DefaultPartySpeedCalculatingModel.CalculateBaseSpeed))]
-    internal class CalculateBaseSpeedPatch 
+    //static FieldInfo f_someField = AccessTools.Field(typeof(SomeType), nameof(SomeType.someField));
+    //static MethodInfo m_MyExtraMethod = SymbolExtensions.GetMethodInfo(() => Tools.MyExtraMethod());
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        //static FieldInfo f_someField = AccessTools.Field(typeof(SomeType), nameof(SomeType.someField));
-        //static MethodInfo m_MyExtraMethod = SymbolExtensions.GetMethodInfo(() => Tools.MyExtraMethod());
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        var instr = instructions.ToList();
+
+
+        instr[405].opcode = OpCodes.Nop;
+        instr[406].opcode = OpCodes.Nop;
+        instr[407].opcode = OpCodes.Nop;
+        instr[408].opcode = OpCodes.Nop;
+        instr[409].opcode = OpCodes.Nop;
+        instr[410].opcode = OpCodes.Nop;
+
+        return instr.AsEnumerable();
+        //yield return new CodeInstruction(OpCodes.Call, )
+    }
+
+    private static void CalculateBaseSpeed(ref MobileParty mobileParty, ref ExplainedNumber __result)
+    {
+        /*
+        if(ModInformation.IsServer && mobileParty.IsPartyControlled() == false)
         {
-            var instr = instructions.ToList();
-
-
-            instr[405].opcode = OpCodes.Nop;
-            instr[406].opcode = OpCodes.Nop;
-            instr[407].opcode = OpCodes.Nop;
-            instr[408].opcode = OpCodes.Nop;
-            instr[409].opcode = OpCodes.Nop;
-            instr[410].opcode = OpCodes.Nop;
-
-            return instr.AsEnumerable();
-            //yield return new CodeInstruction(OpCodes.Call, )
-        }
-
-        private static void CalculateBaseSpeed(ref MobileParty mobileParty, ref ExplainedNumber __result)
-        {
-            /*
-            if(ModInformation.IsServer && mobileParty.IsPartyControlled() == false)
+            float playerMapMovementSpeedBonusMultiplier = Campaign.Current.Models.DifficultyModel.GetPlayerMapMovementSpeedBonusMultiplier();
+            if (playerMapMovementSpeedBonusMultiplier > 0f)
             {
-                float playerMapMovementSpeedBonusMultiplier = Campaign.Current.Models.DifficultyModel.GetPlayerMapMovementSpeedBonusMultiplier();
-                if (playerMapMovementSpeedBonusMultiplier > 0f)
-                {
-                    __result.AddFactor(playerMapMovementSpeedBonusMultiplier, new TaleWorlds.Localization.TextObject(0f));
-                }
+                __result.AddFactor(playerMapMovementSpeedBonusMultiplier, new TaleWorlds.Localization.TextObject(0f));
             }
-            */
+        }
+        */
+    }
+}
+
+[HarmonyPatchCategory(GameInterface.HARMONY_STATIC_FIXES_CATEGORY)]
+[HarmonyPatch(typeof(GameTexts))]
+internal class GameTextsPatches
+{
+    private static readonly FieldInfo get_GameTextManager = typeof(GameTexts)
+        .GetField("_gameTextManager", BindingFlags.Static | BindingFlags.NonPublic);
+
+    [HarmonyPatch(nameof(GameTexts.FindText))]
+    [HarmonyPrefix]
+    private static void FindTextPrefix()
+    {
+        if (get_GameTextManager.GetValue(null) == null)
+        {
+            var gameTextManager = new GameTextManager();
+            gameTextManager.LoadGameTexts();
+            GameTexts.Initialize(gameTextManager);
         }
     }
 }
