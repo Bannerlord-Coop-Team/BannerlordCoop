3e015f1f85accd82aea9b2639aa9ca5ae627b2e2
Samuel Zurowski <samuelzurowski@gmail.com>
2024-01-16 10:00:13 -0500

    Setup Settlements Registry need to do more fixes

--- source/GameInterface/Services/Settlements/SettlementRegistry.cs
+++ source/GameInterface/Services/Settlements/SettlementRegistry.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using Common;
+using GameInterface.Services.Registry;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TaleWorlds.CampaignSystem.Settlements;
 
-namespace GameInterface.Services.Settlements
+namespace GameInterface.Services.Settlements;
+
+/// <summary>
+/// 
+/// </summary>
+internal interface ISettlementRegistry : IRegistry<Settlement>
 {
-    internal class SettlementRegistry
+    void AddSettlement(Settlement settlement);
+    void ContainsSettlement(Settlement settlement);
+    void ContainsVillage(Village village);
+    void RegisterAllSettlements();
+
+}
+
+internal class SettlementRegistry : RegistryBase<Settlement>, ISettlementRegistry
+{
+    public void AddSettlement(Settlement settlement)
     {
+        throw new NotImplementedException();
+    }
+
+    public void ContainsSettlement(Settlement settlement)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ContainsVillage(Village village)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetSettlement(Settlement settlement)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RegisterAllSettlements()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool RegisterNewObject(Settlement obj, out string id)
+    {
+        throw new NotImplementedException();
     }
 }
