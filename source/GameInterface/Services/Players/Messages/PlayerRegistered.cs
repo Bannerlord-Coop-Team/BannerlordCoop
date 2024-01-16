a91ac61c581bb425a0cdbdffaafcf39ae32ee3ba
Samuel Zurowski <samuelzurowski@gmail.com>
2024-01-15 23:47:19 -0500

    added playeregistered comment

--- source/GameInterface/Services/Players/Messages/PlayerRegistered.cs
+++ source/GameInterface/Services/Players/Messages/PlayerRegistered.cs
@@ -3,6 +3,10 @@ using GameInterface.Services.Players.Data;
 
 namespace GameInterface.Services.Players.Messages;
 
+/// <summary>
+/// When the client recieves a NetworkPlayerRegister this event goes to the
+/// GameInterface to notify the PlayerRegistry.
+/// </summary>
 public record PlayerRegistered : IEvent
 {
     public PlayerRegistered(Player player)
