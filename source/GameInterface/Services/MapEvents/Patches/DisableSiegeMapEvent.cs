ec8094d7d1b03524cdfc90de16649b3a38dd5861
Alexander Egard <veryhasted@gmail.com>
2024-01-17 13:20:27 +0100

    Create DisableSiegeMapEvent.cs

---
+++ source/GameInterface/Services/MapEvents/Patches/DisableSiegeMapEvent.cs
@@ -0,0 +1,20 @@
+﻿using Common.Logging;
+using HarmonyLib;
+using Serilog;
+using TaleWorlds.CampaignSystem.MapEvents;
+
+namespace GameInterface.Services.MapEvents.Patches
+{
+    [HarmonyPatch(typeof(MapEventManager))]
+    public class DisableSiegeMapEvent
+    {
+        private static readonly ILogger Logger = LogManager.GetLogger<DisableSiegeMapEvent>();
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(MapEventManager.StartSiegeMapEvent))]
+        static bool Prefix() //TODO Enable Siege
+        {
+            return false;
+        }
+    }
+}
