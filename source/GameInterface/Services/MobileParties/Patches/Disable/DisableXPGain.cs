1381e9e6b65f8096cb6036eb649af9fef6eceec0
Alexander Egard <veryhasted@gmail.com>
2024-01-20 01:33:13 +0100

    DisabledXPGain

---
+++ source/GameInterface/Services/MobileParties/Patches/Disable/DisableXPGain.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using SandBox.Issues;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem.MapEvents;
+
+namespace GameInterface.Services.MobileParties.Patches.Disable
+{
+    [HarmonyPatch(typeof(MapEventParty))]
+    internal class DisableXPGain
+    {
+        [HarmonyPatch(nameof(MapEventParty.CommitXpGain))]
+        static bool Prefix() => false;
+    }
+}
