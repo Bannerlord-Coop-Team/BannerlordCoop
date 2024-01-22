78aceae94bd047aef0fef42fdc088f70d97e796b
Alexander Egard <veryhasted@gmail.com>
2024-01-22 15:36:54 +0100

    Revert "Split Troop Recruitment into separate branch"
    This reverts commit 5f3eddc8b056f23669d9214dfb8f440dfc436260.

---
+++ source/GameInterface/Services/MobileParties/Messages/AddNewTroop.cs
@@ -0,0 +1,25 @@
+﻿using Common.Messaging;
+
+namespace GameInterface.Services.MobileParties.Messages
+{
+    /// <summary>
+    /// Event to tell GameInterface to handle unit being recruited
+    /// </summary>
+    public record AddNewTroop : IEvent
+    {
+        public string CharacterId { get; }
+        public string PartyId { get; }
+        public bool IsPrisonRoster { get; }
+        public bool InsertAtFront { get; }
+        public int InsertionIndex { get; }
+
+        public AddNewTroop(string characterId, string partyId, bool isPrisonRoster, bool insertAtFront, int insertionIndex)
+        {
+            CharacterId = characterId;
+            PartyId = partyId;
+            IsPrisonRoster = isPrisonRoster;
+            InsertAtFront = insertAtFront;
+            InsertionIndex = insertionIndex;
+        }
+    }
+}
---
+++ source/GameInterface/Services/MobileParties/Patches/RecruitmentCampaignBehavior.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace GameInterface.Services.MobileParties.Patches;
+
+[HarmonyPatch(typeof(RecruitmentCampaignBehavior))]
+internal class RecruitmentCampaignBehaviorPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("CheckRecruiting")]
+    private static bool CheckRecruitingPrefix(ref MobileParty mobileParty, ref Settlement settlement)
+    {
+        // TODO only allow for server and broadcast when it happens
+        return true;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch("HourlyTickParty")]
+    private static bool HourlyTickPartyPrefix(ref MobileParty mobileParty)
+    {
+        // TODO disable for player parties
+        return false;
+    }
+}
