069f1613034bd1f095de0ff639ef2cc162c7b76e
Thomas Soutif <th.soutif@gmail.com>
2024-02-02 23:51:35 +0100

    Command to list kingdom

---
+++ source/GameInterface/Services/Kingdoms/Commands/KingdomDebugCommand.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using GameInterface.Services.ObjectManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static TaleWorlds.Library.CommandLineFunctionality;
+using TaleWorlds.CampaignSystem;
+using System.Linq;
+
+namespace GameInterface.Services.Kingdoms.Commands
+{
+    public class KingdomDebugCommand
+    {
+
+        /// <summary>
+        /// Attempts to get the ObjectManager
+        /// </summary>
+        /// <param name="objectManager">Resolved ObjectManager, will be null if unable to resolve</param>
+        /// <returns>True if ObjectManager was resolved, otherwise False</returns>
+        private static bool TryGetObjectManager(out IObjectManager objectManager)
+        {
+            objectManager = null;
+            if (ContainerProvider.TryGetContainer(out var container) == false) return false;
+
+            return container.TryResolve(out objectManager);
+        }
+
+
+
+        // coop.debug.kingdom.list
+        /// <summary>
+        /// Lists all the current Kingdoms
+        /// </summary>
+        /// <param name="args">actually none are being used..</param>
+        /// <returns>strings of all the kingdoms</returns>
+        [CommandLineArgumentFunction("list", "coop.debug.kingdom")]
+        public static string ListKingdoms(List<string> args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            List<Kingdom> kingdoms = Campaign.Current.CampaignObjectManager.Kingdoms.ToList();
+            kingdoms.ForEach((kingdom) =>
+            {
+                stringBuilder.Append(string.Format("Name: '{0}'\n Id : '{1}'\n", kingdom.Name, kingdom.StringId));
+            });
+            return stringBuilder.ToString();
+        }
+    }
+}
