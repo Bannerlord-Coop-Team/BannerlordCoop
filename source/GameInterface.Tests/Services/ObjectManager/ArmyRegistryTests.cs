381fda88d6bd19ed7100eae2d2367795b8300595
Garrett Luskey <garrett.luskey@gmail.com>
2024-02-03 12:41:05 -0600

    Register army test

---
+++ source/GameInterface.Tests/Services/ObjectManager/ArmyRegistryTests.cs
@@ -0,0 +1,21 @@
+﻿using Common.Util;
+using GameInterface.Services.Armies;
+using GameInterface.Services.Armies.Extensions;
+using TaleWorlds.CampaignSystem;
+using Xunit;
+
+namespace GameInterface.Tests.Services.ObjectManager;
+public class ArmyRegistryTests
+{
+    [Fact]
+    public void RegisterArmy()
+    {
+        var armyRegistry = new ArmyRegistry();
+
+        var army = ObjectHelper.SkipConstructor<Army>();
+
+        armyRegistry.RegisterNewObject(army, out var newId);
+
+        Assert.Equal(army.GetStringId(), newId);
+    }
+}
