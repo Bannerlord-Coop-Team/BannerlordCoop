e169bb718a59ba50654c9d6d8a8c5387ccdd61da
Thomas Soutif <th.soutif@gmail.com>
2024-02-03 11:11:48 +0100

    Add the army in the registry
    When a Kingdom create an army, this one is register to be use later.

---
+++ source/GameInterface/Services/Kingdoms/Patches/OnArmyCreatedPatch.cs
@@ -0,0 +1,35 @@
+﻿using Common.Util;
+using GameInterface.Policies;
+using GameInterface.Services.Armies;
+using GameInterface.Services.Armies.Extensions;
+using GameInterface.Services.ObjectManager;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem;
+namespace GameInterface.Services.Kingdoms.Patches
+{
+
+    
+    [HarmonyPatch(typeof(CampaignEventReceiver))] 
+    internal class OnArmyCreatedPatch
+    {
+
+        [HarmonyPatch("OnArmyCreated")]
+        [HarmonyPrefix]
+        public static bool OnArmyCreatedPrefix(ref Army army)
+        {
+            //This method is called when an army is created. It is used to register the army in the registry.
+
+            if(AllowedThread.IsThisThreadAllowed()) { return true; }
+            if (PolicyProvider.AllowOriginalCalls) { return true; }
+            if (ModInformation.IsClient) { return false; }
+
+            IArmyRegistry armyRegistry = new ArmyRegistry();
+            armyRegistry.RegisterNewObject(army, out var newId);
+           
+            return true;
+        }
+    }
+}
