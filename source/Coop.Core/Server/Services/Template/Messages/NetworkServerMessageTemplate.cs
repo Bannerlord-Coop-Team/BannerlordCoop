dd318404077b803275a5d563e88e6af7a8db335c
Samuel Zurowski <samuelzurowski@gmail.com>
2024-02-03 14:43:37 -0500

    no code generation i guess

--- source/Coop.Core/Server/Services/Template/Messages/NetworkServerMessageTemplate.cs
+++ source/Coop.Core/Server/Services/Template/Messages/NetworkServerMessageTemplate.cs
@@ -1,21 +1,20 @@
 ﻿using Common.Messaging;
 using ProtoBuf;
 
-namespace Coop.Core.Server.Services.Template.Messages
-{
-    /// <summary>
-    /// TODO describe what message does
-    /// </summary>
-    [ProtoContract(SkipConstructor = true)]
-    // Name of the message should start with "Network"
-    public record NetworkServerMessageTemplate : IEvent
-    {
-        [ProtoMember(1)]
-        public string SomeData { get;  }
+namespace Coop.Core.Server.Services.Template.Messages;
 
-        public NetworkServerMessageTemplate(string someData)
-        {
-            SomeData = someData;
-        }
-    }
-}
+	/// <summary>
+	/// TODO describe what message does
+	/// </summary>
+	[ProtoContract(SkipConstructor = true)]
+	// Name of the message should start with "Network"
+	public record NetworkServerMessageTemplate : IEvent
+	{
+		[ProtoMember(1)]
+		public string SomeData { get; }
+
+		public NetworkServerMessageTemplate(string someData)
+		{
+			SomeData = someData;
+		}
+	}
