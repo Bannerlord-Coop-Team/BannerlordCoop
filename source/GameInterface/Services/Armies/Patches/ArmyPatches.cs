989d73f52706e31ab9d8a144ab3ce18686c3bed6
Thomas Soutif <th.soutif@gmail.com>
2024-02-04 23:39:54 +0100

    army patch update for party

--- source/GameInterface/Services/Armies/Patches/ArmyPatches.cs
+++ source/GameInterface/Services/Armies/Patches/ArmyPatches.cs
@@ -4,10 +4,12 @@ using Common.Util;
 using GameInterface.Policies;
 using GameInterface.Services.Armies.Extensions;
 using GameInterface.Services.Armies.Messages;
+using GameInterface.Services.ObjectManager;
 using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
-
+using Common.Logging;
+using Serilog;
 namespace GameInterface.Services.Armies.Patches;
 
 /// <summary>
@@ -16,6 +18,7 @@ namespace GameInterface.Services.Armies.Patches;
 [HarmonyPatch(typeof(Army))]
 public class ArmyPatches
 {
+    private static ILogger Logger = LogManager.GetLogger<Kingdom>();
 
     [HarmonyPatch(typeof(Army), "OnAddPartyInternal")]
     [HarmonyPrefix]
@@ -26,12 +29,33 @@ public class ArmyPatches
 
         if (ModInformation.IsClient) return false;
 
-
-        var message = new MobilePartyInArmyAdded(mobileParty.StringId, __instance.LeaderParty.StringId);
-        MessageBroker.Instance.Publish(mobileParty, message);
-
         return true;
     }
+
+    [HarmonyPatch(typeof(Army), "OnAddPartyInternal")]
+    [HarmonyPostfix]
+    static void OnAddPartyInternalPostfix(MobileParty mobileParty)
+    {
+        if (ModInformation.IsClient) return;
+
+        if (ContainerProvider.TryResolve<IObjectManager>(out var objectManager) == false)
+        {
+            Logger.Error("Unable to resolve {objectManager}", typeof(IObjectManager));
+            return;
+        }
+        string armyId = mobileParty.Army.GetStringId();
+        if (armyId == null)
+        {
+            Logger.Error("{army} was not properly registered", mobileParty.Army.Name);
+            return;
+        }
+
+        var message = new MobilePartyInArmyAdded(mobileParty.StringId, armyId);
+        MessageBroker.Instance.Publish(mobileParty, message);
+
+        return;
+    }
+
 
     [HarmonyPatch(typeof(Army), "OnRemovePartyInternal")]
     [HarmonyPrefix]
