63be7d6a3f3c8bc7273de661159d3a3321d30400
Thomas Soutif <th.soutif@gmail.com>
2024-02-04 12:33:17 +0100

    fix enumeration for create / delete army
    The value was not pass correctly throw the network

--- source/GameInterface/Services/Armies/Handlers/ArmyHandler.cs
+++ source/GameInterface/Services/Armies/Handlers/ArmyHandler.cs
@@ -4,6 +4,7 @@ using GameInterface.Services.Armies.Messages;
 using GameInterface.Services.Armies.Patches;
 using GameInterface.Services.ObjectManager;
 using Serilog;
+using System;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
@@ -42,7 +43,7 @@ public class ArmyHandler : IHandler
             Logger.Error("Unable to find Army ({armyId})", data.ArmyId);
             return;
         }
-        Army.ArmyDispersionReason armyReason = (Army.ArmyDispersionReason)data.Reason;
+        Army.ArmyDispersionReason armyReason = (Army.ArmyDispersionReason)Enum.Parse(typeof(Army.ArmyDispersionReason), data.Reason);
         ArmyDeletionPatch.DisbandArmy(army, armyReason);
     }
 
@@ -68,8 +69,10 @@ public class ArmyHandler : IHandler
             return;
         }
 
-        Army.ArmyTypes armyType = (Army.ArmyTypes)data.SelectedArmyType;
+        //Army.ArmyTypes armyType = (Army.ArmyTypes)data.SelectedArmyType;
 
+
+        Army.ArmyTypes armyType = (Army.ArmyTypes)Enum.Parse(typeof(Army.ArmyTypes), data.SelectedArmyType);
         ArmyCreationPatch.CreateArmyInKingdom(kingdom, armyLeader, targetSettlement, armyType, data.ArmyStringId);
     }
 
