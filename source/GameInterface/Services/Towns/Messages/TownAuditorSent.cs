111e91a13c6080321c2d865315ee2be2a4bd09a8
Soutif Thomas <th.soutif@gmail.com>
2024-02-13 16:19:46 +0100

    summary

--- source/GameInterface/Services/Towns/Messages/TownAuditorSent.cs
+++ source/GameInterface/Services/Towns/Messages/TownAuditorSent.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 namespace GameInterface.Services.Towns.Messages
 {
+    /// <summary>
+    /// Used by the town auditor debug command to send all sync value from client to server only
+    /// </summary>
     public record class TownAuditorSent : ICommand
     {
 
