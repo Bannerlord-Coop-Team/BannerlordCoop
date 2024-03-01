13f219ebb4ec458dc0d760324ae10a3c00dfde4e
Samuel Zurowski <samuelzurowski@gmail.com>
2024-02-29 20:53:44 -0500

    Readded important mobileparty cache functions

--- source/GameInterface/Services/Settlements/Patches/HeroWithoutPartyPatch.cs
+++ source/GameInterface/Services/Settlements/Patches/HeroWithoutPartyPatch.cs
@@ -1,4 +1,12 @@
-﻿using HarmonyLib;
+﻿using Common;
+using Common.Logging;
+using Common.Messaging;
+using Common.Util;
+using GameInterface.Extentions;
+using GameInterface.Policies;
+using GameInterface.Services.Settlements.Messages;
+using HarmonyLib;
+using Serilog;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Settlements;
 
@@ -11,15 +19,93 @@ namespace GameInterface.Services.Settlements.Patches;
 [HarmonyPatch(typeof(Settlement))]
 public class HeroWithoutPartyPatch
 {
-    // TODO: discuss and test if needed, not sure...
-    // only server needs to know about this..
-    // may not be needed but for now if it does the code is here.
+    private static readonly ILogger Logger = LogManager.GetLogger<HeroWithoutPartyPatch>();
+
     [HarmonyPatch("AddHeroWithoutParty")]
     [HarmonyPrefix]
-    private static bool AddHeroWithoutPartyPrefix(ref Settlement __instance, Hero individual) => ModInformation.IsServer;
+    private static bool AddHeroWithoutPartyPrefix(ref Settlement __instance, Hero individual)
+    {
+        if (CallOriginalPolicy.IsOriginalAllowed()) return true;
+        if (ModInformation.IsClient) return false;
+
+        var heroCache = __instance.GetHeroesWithoutPartyCache();
+
+        if(!heroCache.Contains(individual))
+        {
+
+            var message = new SettlementChangedAddHeroWithoutParty(__instance.StringId, individual.StringId);
+
+            MessageBroker.Instance.Publish(__instance, message);
+
+        }
+        return true;
+    }
+
+    internal static void RunAddHeroWithoutParty(Settlement settlement, Hero individual)
+    {
+
+        // does this even need a allowedthread?
+        var heroList = settlement.GetHeroesWithoutPartyCache();
+
+        if (!heroList.Contains(individual))
+        {
+            heroList.Add(individual);
+        } 
+        else
+        {
+            Logger.Error("Attempted to add Hero {HeroId} that was already in list", individual.StringId);
+        }
+        GameLoopRunner.RunOnMainThread(() =>
+        {
+            using (new AllowedThread())
+            {
+                settlement.SetHeroesWithoutPartyCache(heroList);
+            }
+        });
+    }
 
 
     [HarmonyPatch("RemoveHeroWithoutParty")]
     [HarmonyPrefix]
-    private static bool RemoveHeroWithoutPartyPrefix(ref Settlement __instance, Hero individual) => ModInformation.IsServer;
+    private static bool RemoveHeroWithoutPartyPrefix(ref Settlement __instance, Hero individual)
+    {
+        if (CallOriginalPolicy.IsOriginalAllowed()) return true;
+        if (ModInformation.IsClient) return false;
+
+        var heroCache = __instance.GetHeroesWithoutPartyCache();
+
+        if (heroCache.Contains(individual))
+        {
+
+            var message = new SettlementChangedRemoveHeroWithoutParty(__instance.StringId, individual.StringId);
+
+            MessageBroker.Instance.Publish(__instance, message);
+        }
+
+        return true;
+    }
+    
+    internal static void RunRemoveHeroWithoutParty(Settlement settlement, Hero individual)
+    {
+
+        var heroList = settlement.GetHeroesWithoutPartyCache();
+
+        if (heroList.Contains(individual))
+        {
+            heroList.Remove(individual);
+        }
+        else
+        {
+            Logger.Error("Attempted to Remove Hero {HeroId} that is not in list", individual.StringId);
+        }
+
+        GameLoopRunner.RunOnMainThread(() =>
+        {
+            using (new AllowedThread())
+            {
+                settlement.SetHeroesWithoutPartyCache(heroList);
+            }
+        });
+    }
+  
 }
