b0cc808cb1a88e21be0e199bbdcb0dde36514a27
MaxDorob <maxdor81@gmail.com>
2024-03-02 20:54:10 +0600

    Client handler for settlement comp

---
+++ source/Coop.Core/Client/Services/Settlements/Handlers/ClientSettlementComponetHandler.cs
@@ -0,0 +1,53 @@
+﻿using Common.Messaging;
+using Common.Network;
+using GameInterface.Services.Settlements.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coop.Core.Client.Services.Settlements.Handlers
+{
+    public class ClientSettlementComponetHandler : IHandler
+    {
+        private readonly IMessageBroker messageBroker;
+        private readonly INetwork network;
+
+        public ClientSettlementComponetHandler(IMessageBroker messageBroker, INetwork network) 
+        {
+            this.messageBroker = messageBroker;
+            this.network = network;
+            messageBroker.Subscribe<NetworkSettlementComponentChangedGold>(HandleChangedGold);
+            messageBroker.Subscribe<NetworkSettlementComponentChangedIsOwnerUnassigned>(HandleChangedIsOwnerUnassigned);
+            messageBroker.Subscribe<NetworkSettlementComponentChangedOwner>(HandleChangedOwner);
+        }
+
+        private void HandleChangedOwner(MessagePayload<NetworkSettlementComponentChangedOwner> payload)
+        {
+            var obj = payload.What;
+            var message = new ChangeSettlementComponentOwner(obj.SettlementComponentId, obj.OwnerId);
+            messageBroker.Publish(this, message);
+        }
+
+        private void HandleChangedIsOwnerUnassigned(MessagePayload<NetworkSettlementComponentChangedIsOwnerUnassigned> payload)
+        {
+            var obj = payload.What;
+            var message = new ChangeSettlementComponentIsOwnerUnassigned(obj.SettlementComponentId, obj.IsOwnerUnassigned);
+            messageBroker.Publish(this, message);
+        }
+
+        private void HandleChangedGold(MessagePayload<NetworkSettlementComponentChangedGold> payload)
+        {
+            var obj = payload.What;
+            var message = new ChangeSettlementComponentGold(obj.SettlementComponentId, obj.Gold);
+            messageBroker.Publish(this, message);
+        }
+
+        public void Dispose()
+        {
+            messageBroker.Unsubscribe<NetworkSettlementComponentChangedGold>(HandleChangedGold);
+            messageBroker.Unsubscribe<NetworkSettlementComponentChangedIsOwnerUnassigned>(HandleChangedIsOwnerUnassigned);
+            messageBroker.Unsubscribe<NetworkSettlementComponentChangedOwner>(HandleChangedOwner);
+        }
+
+    }
+}
