ab2b5526ed6dceb9a639b1fad8b3247b5104dc4e
Alexander Egard <veryhasted@gmail.com>
2024-03-08 00:26:17 +0100

    Added more BatchLogging

--- source/GameInterface/Services/Settlements/Messages/SettlementComponentGoldChanged.cs
+++ source/GameInterface/Services/Settlements/Messages/SettlementComponentGoldChanged.cs
@@ -1,4 +1,5 @@
-﻿using Common.Messaging;
+﻿using Common.Logging.Attributes;
+using Common.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,7 @@ namespace GameInterface.Services.Settlements.Messages
     /// <summary>
     /// Notify <see cref="TaleWorlds.CampaignSystem.Settlements.SettlementComponent.Gold"/> changed
     /// </summary>
+    [BatchLogMessage]
     public record SettlementComponentGoldChanged : IEvent
     {
         public string SettlementComponentId { get; }
