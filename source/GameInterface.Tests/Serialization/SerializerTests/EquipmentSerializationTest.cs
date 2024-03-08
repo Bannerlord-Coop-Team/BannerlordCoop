ae50fa69f789cb21b6869e0a1d8acf06bb6781ce
MaxDorob <maxdor81@gmail.com>
2024-03-09 01:56:03 +0600

    EquipmentSerializationTest removed reflection

--- source/GameInterface.Tests/Serialization/SerializerTests/EquipmentSerializationTest.cs
+++ source/GameInterface.Tests/Serialization/SerializerTests/EquipmentSerializationTest.cs
@@ -37,12 +37,11 @@ namespace GameInterface.Tests.Serialization.SerializerTests
             Assert.NotEmpty(bytes);
         }
 
-        private static readonly FieldInfo _equipmentType = typeof(Equipment).GetField("_equipmentType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
         [Fact]
         public void Equipment_Full_Serialization()
         {
             Equipment equipment = new Equipment();
-            _equipmentType.SetValue(equipment, EquipmentType.Battle);
+            equipment._equipmentType = EquipmentType.Battle;
 
             for (int i = 0; i < 12; i++)
             {
@@ -68,7 +67,7 @@ namespace GameInterface.Tests.Serialization.SerializerTests
 
             Assert.Equal(equipment.IsCivilian, newEquipment.IsCivilian);
             Assert.Equal(equipment.IsValid, newEquipment.IsValid);
-            Assert.Equal((EquipmentType)_equipmentType.GetValue(equipment), (EquipmentType)_equipmentType.GetValue(newEquipment));
+            Assert.Equal(equipment._equipmentType, newEquipment._equipmentType);
             for (int i = 0; i < 12; i++)
             {
                 Assert.True(equipment[i].IsEqualTo(newEquipment[i]));
