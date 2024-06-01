0379da262826ec318049f3148836b47eceb90f8e
Dennis Post <dennis.post@venisly.de>
2024-06-01 15:36:50 +0200

    Implemented module version validation

--- source/Coop.Core/Server/Connections/States/ResolveCharacterState.cs
+++ source/Coop.Core/Server/Connections/States/ResolveCharacterState.cs
@@ -1,10 +1,14 @@
-﻿using Common.Logging;
+﻿using System.Linq;
+using Common.Logging;
 using Common.Messaging;
 using Common.Network;
 using Coop.Core.Server.Connections.Messages;
 using GameInterface.Services.Heroes.Messages;
+using GameInterface.Services.Modules;
+using GameInterface.Services.Modules.Validators;
 using LiteNetLib;
 using Serilog;
+using TaleWorlds.Library;
 
 namespace Coop.Core.Server.Connections.States;
 
@@ -18,17 +22,24 @@ public class ResolveCharacterState : ConnectionStateBase
 
     private readonly IMessageBroker messageBroker;
     private readonly INetwork network;
+    private readonly IModuleValidator moduleValidator;
+    private readonly IModuleInfoProvider moduleInfoProvider;
     public ResolveCharacterState(IConnectionLogic connectionLogic,
         IMessageBroker messageBroker,
-        INetwork network) 
+        INetwork network,
+        IModuleValidator moduleValidator,
+        IModuleInfoProvider moduleInfoProvider) 
         : base(connectionLogic)
     {
         this.messageBroker = messageBroker;
         this.network = network;
+        this.moduleValidator = moduleValidator;
+        this.moduleInfoProvider = moduleInfoProvider;
 
         messageBroker.Subscribe<NetworkClientValidate>(ClientValidateHandler);
         messageBroker.Subscribe<HeroResolved>(ResolveHeroHandler);
         messageBroker.Subscribe<ResolveHeroNotFound>(HeroNotFoundHandler);
+        messageBroker.Subscribe<NetworkModuleVersionsValidate>(ModuleVersionsValidateHandler);
     }
 
     public override void Dispose()
@@ -36,6 +47,19 @@ public class ResolveCharacterState : ConnectionStateBase
         messageBroker.Unsubscribe<NetworkClientValidate>(ClientValidateHandler);
         messageBroker.Unsubscribe<HeroResolved>(ResolveHeroHandler);
         messageBroker.Unsubscribe<ResolveHeroNotFound>(HeroNotFoundHandler);
+        messageBroker.Unsubscribe<NetworkModuleVersionsValidate>(ModuleVersionsValidateHandler);
+    }
+
+    internal void ModuleVersionsValidateHandler(MessagePayload<NetworkModuleVersionsValidate> obj)
+    {
+        var clientModules = obj.What.Modules;
+        var serverModules = moduleInfoProvider.GetModuleInfos();
+
+        var result = moduleValidator.Validate(serverModules, clientModules.Select(ConvertToModuleInfo).ToList());
+
+        var validateMessage = new NetworkModuleVersionsValidated(result == null, result);
+        var playerPeer = ConnectionLogic.Peer;
+        network.Send(playerPeer, validateMessage);
     }
 
     internal void ClientValidateHandler(MessagePayload<NetworkClientValidate> obj)
@@ -82,5 +106,15 @@ public class ResolveCharacterState : ConnectionStateBase
 
     public override void EnterMission()
     {
+    }
+
+    private static ModuleInfo ConvertToModuleInfo(NetworkModuleInfo networkModuleInfo)
+    {
+        return new ModuleInfo()
+        {
+            Id = networkModuleInfo.Id,
+            IsOfficial = networkModuleInfo.IsOfficial,
+            Version = new ApplicationVersion((ApplicationVersionType)networkModuleInfo.Version.ApplicationVersionType, networkModuleInfo.Version.Major, networkModuleInfo.Version.Minor, networkModuleInfo.Version.Revision, networkModuleInfo.Version.ChangeSet)
+        };
     }
 }
