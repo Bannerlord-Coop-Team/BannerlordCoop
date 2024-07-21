570ebcbe238002b98126516853a3eae5f968561c
Garrett Luskey <garrett.luskey@gmail.com>
2024-07-21 12:48:09 -0500

    re-enabled test

--- source/E2E.Tests/Services/MapEvents/MapEventCollectionTests.cs
+++ source/E2E.Tests/Services/MapEvents/MapEventCollectionTests.cs
@@ -27,38 +27,36 @@ public class MapEventCollectionTests : IDisposable
     [Fact]
     public void Server_MapEvent_Sides_SyncAllClients()
     {
-        // TODO get working in docker image
+        // Arrange
+        var server = TestEnvironment.Server;
 
-        //// Arrange
-        //var server = TestEnvironment.Server;
+        // Act
+        string? attackerSideId = null;
+        string? defenderSideId = null;
+        server.Call(() =>
+        {
+            var mapEvent = GameObjectCreator.CreateInitializedObject<MapEvent>();
+            var attackerParty = GameObjectCreator.CreateInitializedObject<MobileParty>();
+            var defenderParty = GameObjectCreator.CreateInitializedObject<MobileParty>();
 
-        //// Act
-        //string? attackerSideId = null;
-        //string? defenderSideId = null;
-        //server.Call(() =>
-        //{
-        //    var mapEvent = GameObjectCreator.CreateInitializedObject<MapEvent>();
-        //    var attackerParty = GameObjectCreator.CreateInitializedObject<MobileParty>();
-        //    var defenderParty = GameObjectCreator.CreateInitializedObject<MobileParty>();
+            // TODO find better way
+            mapEvent.MapEventVisual = ObjectHelper.SkipConstructor<GauntletMapEventVisual>();
 
-        //    // TODO find better way
-        //    mapEvent.MapEventVisual = ObjectHelper.SkipConstructor<GauntletMapEventVisual>();
+            mapEvent.Initialize(attackerParty.Party, defenderParty.Party);
 
-        //    mapEvent.Initialize(attackerParty.Party, defenderParty.Party);
+            Assert.True(server.ObjectManager.TryGetId(attackerParty, out attackerSideId));
+            Assert.True(server.ObjectManager.TryGetId(defenderParty, out defenderSideId));
+        }, new MethodBase[] { AccessTools.Method(typeof(GauntletMapEventVisual), nameof(GauntletMapEventVisual.Initialize)) });
 
-        //    Assert.True(server.ObjectManager.TryGetId(attackerParty, out attackerSideId));
-        //    Assert.True(server.ObjectManager.TryGetId(defenderParty, out defenderSideId));
-        //}, new MethodBase[] { AccessTools.Method(typeof(GauntletMapEventVisual), nameof(GauntletMapEventVisual.Initialize)) });
+        // Assert
+        Assert.NotNull(attackerSideId);
+        Assert.NotNull(defenderSideId);
 
-        //// Assert
-        //Assert.NotNull(attackerSideId);
-        //Assert.NotNull(defenderSideId);
-
-        //foreach (var client in TestEnvironment.Clients)
-        //{
-        //    Assert.True(client.ObjectManager.TryGetObject<MapEventSide>(attackerSideId, out var _));
-        //    Assert.True(client.ObjectManager.TryGetObject<MapEventSide>(defenderSideId, out var _));
-        //}
+        foreach (var client in TestEnvironment.Clients)
+        {
+            Assert.True(client.ObjectManager.TryGetObject<MapEventSide>(attackerSideId, out var _));
+            Assert.True(client.ObjectManager.TryGetObject<MapEventSide>(defenderSideId, out var _));
+        }
     }
 
     [Fact]
