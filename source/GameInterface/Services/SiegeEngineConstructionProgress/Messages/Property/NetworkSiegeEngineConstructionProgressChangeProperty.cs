eb737cabaa995e8d5c9731130dcd8bc0839022f3
brodrigz <57366583+brodrigz@users.noreply.github.com>
2024-09-22 21:12:26 -0300

    Sync SiegeEngineConstructionProgress

---
+++ source/GameInterface/Services/SiegeEngineConstructionProgress/Messages/Property/NetworkSiegeEngineConstructionProgressChangeProperty.cs
@@ -0,0 +1,33 @@
+﻿using Common.Messaging;
+using GameInterface.Services.SiegeEngineConstructionProgresss.Patches;
+using ProtoBuf;
+
+namespace GameInterface.Services.SiegeEngineConstructionProgressss.Messages
+{
+    [ProtoContract(SkipConstructor = true)]
+    public record NetworkSiegeEngineConstructionProgressChangeProperty : ICommand
+    {
+        [ProtoMember(1)]
+        public string propertyName;
+        [ProtoMember(2)]
+        public string siegeEngineConstructionProgressId;
+        [ProtoMember(3)]
+        public string objectId;
+        [ProtoMember(4)]
+        public byte[] serializedValue;
+
+        public NetworkSiegeEngineConstructionProgressChangeProperty(string propertyName, string besiegerCampId, string objectId)
+        {
+            this.propertyName = propertyName;
+            this.siegeEngineConstructionProgressId = besiegerCampId;
+            this.objectId = objectId;
+        }
+
+        public NetworkSiegeEngineConstructionProgressChangeProperty(string propertyName, string besiegerCampId, byte[] serializedValue)
+        {
+            this.propertyName = propertyName;
+            this.siegeEngineConstructionProgressId = besiegerCampId;
+            this.serializedValue = serializedValue;
+        }
+    }
+}
---
+++ source/GameInterface/Services/SiegeEngineConstructionProgress/Patches/SiegeEngineConstructionProgressCreationPatches.cs
@@ -0,0 +1,40 @@
+﻿using Common.Logging;
+using Common.Messaging;
+using GameInterface.Policies;
+using GameInterface.Services.SiegeEngineConstructionProgresss.Messages;
+using HarmonyLib;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TaleWorlds.CampaignSystem.Siege;
+using static TaleWorlds.CampaignSystem.Siege.SiegeEvent;
+
+namespace GameInterface.Services.SiegeEngineConstructionProgresss.Patches;
+
+[HarmonyPatch]
+internal class SiegeEngineConstructionProgressCreationPatches
+{
+    static readonly ILogger Logger = LogManager.GetLogger<SiegeEngineConstructionProgressCreationPatches>();
+
+    static IEnumerable<MethodBase> TargetMethods() => AccessTools.GetDeclaredConstructors(typeof(SiegeEngineConstructionProgress));
+
+    static bool Prefix(ref SiegeEngineConstructionProgress __instance)
+    {
+        // Call original if we call this function
+        if (CallOriginalPolicy.IsOriginalAllowed()) return true;
+
+        if (ModInformation.IsClient)
+        {
+            Logger.Error("Client created unmanaged {name}\n"
+                + "Callstack: {callstack}", typeof(SiegeEngineConstructionProgress), Environment.StackTrace);
+            return true;
+        }
+
+        var message = new SiegeEngineConstructionProgressCreated(__instance);
+
+        MessageBroker.Instance.Publish(__instance, message);
+
+        return true;
+    }
+}
