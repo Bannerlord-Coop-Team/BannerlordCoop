8cf2e24986512adbec8ff5f8e68b7dde7b12a1ba
brodrigz <bruno_har@hotmail.com>
2024-09-25 19:36:58 -0300

    Field autosync (not working)

---
+++ source/GameInterface/Services/BesiegerCamps/BesiegerCampSync.cs
@@ -0,0 +1,18 @@
+﻿using GameInterface.AutoSync;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.Siege;
+
+namespace GameInterface.Services.BesiegerCamps
+{
+    class BesiegerCampSync : IAutoSync
+    {
+        public BesiegerCampSync(IAutoSyncBuilder autoSyncBuilder)
+        {
+            autoSyncBuilder.AddField(AccessTools.Field(typeof(BesiegerCamp), nameof(BesiegerCamp._leaderParty)));
+        }
+    }
+}
