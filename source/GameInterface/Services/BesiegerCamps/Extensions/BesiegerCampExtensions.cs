9faa4066b83a69cb65ac45fb098ba93a56f951fb
brodrigz <bruno_har@hotmail.com>
2024-10-11 23:04:16 -0300

    Remove deadcode

--- source/GameInterface/Services/BesiegerCamps/Extensions/BesiegerCampExtensions.cs
+++ source/GameInterface/Services/BesiegerCamps/Extensions/BesiegerCampExtensions.cs
@@ -40,19 +40,5 @@ namespace GameInterface.Services.BesiegerCamps.Extensions
 
             return true;
         }
-
-        //// quick and dirty way to pass the type as refference isntead of type arg
-        //public static bool TryGetObject(this IObjectManager src, string id, Type type, out object obj)
-        //{
-        //    MethodInfo method = src.GetType().GetMethod(nameof(IObjectManager.TryGetObject));
-        //    MethodInfo genericMethod = method.MakeGenericMethod(type);
-
-        //    object[] parameters = new object[] { id, null };
-        //    bool result = (bool)genericMethod.Invoke(src, parameters);
-
-        //    obj = parameters[1];
-
-        //    return result;
-        //}
     }
 }
