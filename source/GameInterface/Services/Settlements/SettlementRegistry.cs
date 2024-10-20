2e20fa5dee59e86073423e25eae369adc83df233
Alexander Egard <veryhasted@gmail.com>
2024-10-20 18:16:59 +0200

    Update SettlementRegistry.cs

--- source/GameInterface/Services/Settlements/SettlementRegistry.cs
+++ source/GameInterface/Services/Settlements/SettlementRegistry.cs
@@ -8,74 +8,51 @@ using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.ObjectSystem;
 
 namespace GameInterface.Services.Settlements;
-
 internal class SettlementRegistry : RegistryBase<Settlement>
 {
-    private const string SeigeEventPrefix = $"Coop{nameof(Settlement)}";
-    private static int InstanceCounter = 0;
+    public static readonly string SettlementStringIdPrefix = "CoopSettlement";
+    private static int SettlementCounter = 0;
 
     public SettlementRegistry(IRegistryCollection collection) : base(collection) { }
 
     public override void RegisterAll()
     {
-        foreach (var Settlement in Campaign.Current.Settlements)
+        var campaignObjectManager = Campaign.Current?.CampaignObjectManager;
+
+        if (campaignObjectManager == null)
         {
-            RegisterNewObject(Settlement, out _);
+            Logger.Error("Unable to register objects when CampaignObjectManager is null");
+            return;
+        }
+
+        foreach (var settlement in campaignObjectManager.Settlements)
+        {
+            base.RegisterExistingObject(settlement.StringId, settlement);
         }
     }
 
-    protected override string GetNewId(Settlement party)
+    public override bool RegisterExistingObject(string id, object obj)
     {
-        return $"{SeigeEventPrefix}_{Interlocked.Increment(ref InstanceCounter)}";
+        var result = base.RegisterExistingObject(id, obj);
+
+        AddToCampaignObjectManager(obj);
+
+        return result;
+    }
+
+    protected override string GetNewId(Settlement settlement)
+    {
+        return $"{SettlementStringIdPrefix}_{Interlocked.Increment(ref SettlementCounter)}";
+    }
+
+    private void AddToCampaignObjectManager(object obj)
+    {
+        if (TryCast(obj, out var _) == false) return;
+
+        var objectManager = Campaign.Current?.CampaignObjectManager;
+
+        if (objectManager == null) return;
+
+        objectManager.Settlements = MBObjectManager.Instance.GetObjectTypeList<Settlement>();
     }
 }
-
-
-//internal class SettlementRegistry : RegistryBase<Settlement>
-//{
-//    public static readonly string SettlementStringIdPrefix = "CoopSettlement";
-
-//    public SettlementRegistry(IRegistryCollection collection) : base(collection) { }
-
-//    public override void RegisterAll()
-//    {
-//        var campaignObjectManager = Campaign.Current?.CampaignObjectManager;
-
-//        if (campaignObjectManager == null)
-//        {
-//            Logger.Error("Unable to register objects when CampaignObjectManager is null");
-//            return;
-//        }
-
-//        foreach (var settlement in campaignObjectManager.Settlements)
-//        {
-//            base.RegisterExistingObject(settlement.StringId, settlement);
-//        }
-//    }
-
-//    public override bool RegisterExistingObject(string id, object obj)
-//    {
-//        var result = base.RegisterExistingObject(id, obj);
-
-//        AddToCampaignObjectManager(obj);
-
-//        return result;
-//    }
-
-//    protected override string GetNewId(Settlement settlement)
-//    {
-//        settlement.StringId = Campaign.Current.CampaignObjectManager.FindNextUniqueStringId<Settlement>(SettlementStringIdPrefix);
-//        return settlement.StringId;
-//    }
-
-//    private void AddToCampaignObjectManager(object obj)
-//    {
-//        if (TryCast(obj, out var _) == false) return;
-
-//        var objectManager = Campaign.Current?.CampaignObjectManager;
-
-//        if (objectManager == null) return;
-
-//        objectManager.Settlements = MBObjectManager.Instance.GetObjectTypeList<Settlement>();
-//    }
-//}
