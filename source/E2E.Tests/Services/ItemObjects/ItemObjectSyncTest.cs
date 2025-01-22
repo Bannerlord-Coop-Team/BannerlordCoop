a05ce9a501c4b5929ef68f5c70d531dbf1c9804d
Jackson Martin <jackamartin17@gmail.com>
2025-01-22 15:04:00 -0500

    ItemObject Field Sync
    ItemObject Field Sync, Resolves #995

---
+++ source/E2E.Tests/Services/ItemObjects/ItemObjectSyncTest.cs
@@ -0,0 +1,70 @@
+﻿using Common.Util;
+using E2E.Tests.Environment;
+using HarmonyLib;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements.Buildings;
+using TaleWorlds.CampaignSystem.Siege;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using Xunit.Abstractions;
+
+namespace E2E.Tests.Services.CraftingService
+{
+    public class ItemObjectSyncTest : IDisposable
+    {
+        E2ETestEnvironment TestEnvironment { get; }
+        public ItemObjectSyncTest(ITestOutputHelper output)
+        {
+            TestEnvironment = new E2ETestEnvironment(output);
+        }
+
+        public void Dispose()
+        {
+            TestEnvironment.Dispose();
+        }
+
+        [Fact]
+        public void ServerCreateItemObject_SyncAllClients()
+        {
+            // Arrange
+            var server = TestEnvironment.Server;
+
+            // Act
+            string? itemObjectId = null;
+
+            var typeField = AccessTools.Field(typeof(ItemObject), nameof(ItemObject.Type));
+
+            var typeIntercept = TestEnvironment.GetIntercept(typeField);
+            
+
+            server.Call(() =>
+            {
+                ItemObject itemObject = new ItemObject();
+                Assert.True(server.ObjectManager.TryGetId(itemObject, out itemObjectId));
+            });
+
+            // Assert
+            Assert.True(server.ObjectManager.TryGetObject(itemObjectId, out ItemObject itemObject));
+
+            server.Call(() =>
+            {
+                //NEED TO ADD typeField TEST
+                typeIntercept.Invoke(null, new object[] { itemObject, 69 });
+            });
+
+            server.ObjectManager.TryGetId(itemObject, out string serverObjectId);
+
+            foreach (var client in TestEnvironment.Clients)
+            {
+                Assert.True(client.ObjectManager.TryGetObject(serverObjectId, out ItemObject clientItemObject));
+                Assert.Equal(itemObject.Type, clientItemObject.Type);
+
+                client.ObjectManager.TryGetId(clientItemObject.Type, out string clientObjectId);
+
+                Assert.Equal(serverObjectId, clientObjectId.Type);
+            }
+        }
+    }
+}
