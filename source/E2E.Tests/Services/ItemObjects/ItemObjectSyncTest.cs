b52bf403e4ee36d979280016ccb1f10e468f62c9
Jackson Martin <jackamartin17@gmail.com>
2025-01-23 11:12:52 -0500

    ItemObject Properties Sync
    Synced ItemObject Properties Listed in #994. Vec3 will need to be Serialized

--- source/E2E.Tests/Services/ItemObjects/ItemObjectSyncTest.cs
+++ source/E2E.Tests/Services/ItemObjects/ItemObjectSyncTest.cs
@@ -7,8 +7,14 @@ using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Settlements.Buildings;
 using TaleWorlds.CampaignSystem.Siege;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using Xunit.Abstractions;
+using GameInterface.Tests.Bootstrap.Modules;
+using System.ComponentModel;
+using Autofac;
+using NuGet.Frameworks;
+using Newtonsoft.Json.Serialization;
 
 namespace E2E.Tests.Services.ItemObjectService
 {
@@ -35,14 +41,34 @@ namespace E2E.Tests.Services.ItemObjectService
             string? itemObjectId = null;
 
             var typeField = AccessTools.Field(typeof(ItemObject), nameof(ItemObject.Type));
-
             var typeIntercept = TestEnvironment.GetIntercept(typeField);
-            
+
+            var culture = new BasicCultureObject() { Name = new TextObject("Battania") };
+            var weaponDesign = ObjectHelper.SkipConstructor<WeaponDesign>();
+            var holsterPositionShift = new Vec3(1, 2, 3, 4);
 
             server.Call(() =>
             {
                 ItemObject itemObject = new ItemObject();
                 Assert.True(server.ObjectManager.TryGetId(itemObject, out itemObjectId));
+
+                itemObject.Weight = 3.5f;
+                itemObject.Name = new TextObject("Name");
+                itemObject.MultiMeshName = "MultiMeshName";
+                itemObject.HolsterMeshName = "HolsterMeshName";
+                itemObject.HolsterWithWeaponMeshName = "WithWeaponMeshName";
+                //itemObject.HolsterPositionShift = holsterPositionShift;
+                itemObject.FlyingMeshName = "FlyingMeshName";
+                itemObject.BodyName = "BodyName";
+                itemObject.HolsterBodyName = "HolsterBodyName";
+                itemObject.CollisionBodyName = "CollisionBodyName";
+                itemObject.RecalculateBody = false;
+                itemObject.Culture = culture;
+                itemObject.Difficulty = 1;
+                itemObject.ScaleFactor = 3.5f;
+                itemObject.ItemFlags = ItemFlags.Civilian;
+                itemObject.Appearance = 3.5f;
+                itemObject.WeaponDesign = weaponDesign;
             });
 
             // Assert
@@ -54,6 +80,8 @@ namespace E2E.Tests.Services.ItemObjectService
             });
 
             server.ObjectManager.TryGetId(itemObject, out string serverObjectId);
+            server.ObjectManager.TryGetId(itemObject.Culture, out string serverCultureId);
+            server.ObjectManager.TryGetId(itemObject.WeaponDesign, out string serverWeaponDesignId);
 
             foreach (var client in TestEnvironment.Clients)
             {
@@ -61,8 +89,27 @@ namespace E2E.Tests.Services.ItemObjectService
                 Assert.Equal(itemObject.Type, clientItemObject.Type);
 
                 client.ObjectManager.TryGetId(clientItemObject, out string clientObjectId);
-
+                client.ObjectManager.TryGetId(clientItemObject.Culture, out string clientCultureId);
+                client.ObjectManager.TryGetId(clientItemObject.WeaponDesign, out string clientWeaponDesignId);
                 Assert.Equal(serverObjectId, clientObjectId);
+
+                Assert.Equal(3.5f, clientItemObject.Weight);
+                Assert.Equal("Name", clientItemObject.Name.ToString());
+                Assert.Equal("MultiMeshName", clientItemObject.MultiMeshName);
+                Assert.Equal("HolsterMeshName", clientItemObject.HolsterMeshName);
+                Assert.Equal("WithWeaponMeshName", clientItemObject.HolsterWithWeaponMeshName);
+                //Assert.Equal(holsterPositionShift, clientItemObject.HolsterPositionShift);
+                Assert.Equal("FlyingMeshName", clientItemObject.FlyingMeshName);
+                Assert.Equal("BodyName", clientItemObject.BodyName);
+                Assert.Equal("HolsterBodyName", clientItemObject.HolsterBodyName);
+                Assert.Equal("CollisionBodyName", clientItemObject.CollisionBodyName);
+                Assert.False(clientItemObject.RecalculateBody);
+                Assert.Equal(clientCultureId, serverCultureId);
+                Assert.Equal(1, clientItemObject.Difficulty);
+                Assert.Equal(3.5f, clientItemObject.ScaleFactor);
+                Assert.Equal(ItemFlags.Civilian, clientItemObject.ItemFlags);
+                Assert.Equal(3.5f, clientItemObject.Appearance);
+                Assert.Equal(clientWeaponDesignId, serverWeaponDesignId);
             }
         }
     }
--- source/GameInterface/Services/ItemObjects/ItemObjectSync.cs
+++ source/GameInterface/Services/ItemObjects/ItemObjectSync.cs
@@ -7,9 +7,32 @@ namespace GameInterface.Services.ItemObjects
 {
     public class ItemObjectSync : IAutoSync
     {
+        /**
+        * HolsterPositionShift (Vec3) does not like to be serialized, not sure how to approach this one
+        */
         public ItemObjectSync(IAutoSyncBuilder autoSyncBuilder)
         {
+            //Fields
             autoSyncBuilder.AddField(AccessTools.Field(typeof(ItemObject), nameof(ItemObject.Type)));
+
+            //Properties
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.Weight)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.Name)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.MultiMeshName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.HolsterMeshName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.HolsterWithWeaponMeshName)));
+            //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.HolsterPositionShift)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.FlyingMeshName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.BodyName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.HolsterBodyName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.CollisionBodyName)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.RecalculateBody)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.Culture)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.Difficulty)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.ScaleFactor)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.ItemFlags)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.Appearance)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(ItemObject), nameof(ItemObject.WeaponDesign)));
         }
     }
 }
