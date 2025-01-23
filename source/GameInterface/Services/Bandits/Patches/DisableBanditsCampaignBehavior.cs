d3a35807a723f19db5972840df1ce08a346a8593
Alexander Egard <veryhasted@gmail.com>
2025-01-23 23:41:17 +0100

    Removes a big and fixes StrengthOfSides

--- source/GameInterface/Services/Bandits/Patches/DisableBanditsCampaignBehavior.cs
+++ source/GameInterface/Services/Bandits/Patches/DisableBanditsCampaignBehavior.cs
@@ -6,26 +6,30 @@ namespace GameInterface.Services.Bandits.Patches;
 [HarmonyPatch(typeof(BanditsCampaignBehavior))]
 internal class DisableBanditsCampaignBehavior
 {
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.OnSettlementEntered))]
+    [HarmonyPatch(nameof(BanditsCampaignBehavior.RegisterEvents))]
     [HarmonyPrefix]
-    static bool OnSettlementEnteredPrefix() => false;
+    static bool RegisterEventsPrefix() => false;
 
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.WeeklyTick))]
-    [HarmonyPrefix]
-    static bool WeeklyTickPrefix() => ModInformation.IsServer;
+    //[HarmonyPatch(nameof(BanditsCampaignBehavior.OnSettlementEntered))]
+    //[HarmonyPrefix]
+    //static bool OnSettlementEnteredPrefix() => false;
 
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.DailyTick))]
-    [HarmonyPrefix]
-    static bool DailyTickPrefix() => ModInformation.IsServer;
+    //[HarmonyPatch(nameof(BanditsCampaignBehavior.WeeklyTick))]
+    //[HarmonyPrefix]
+    //static bool WeeklyTickPrefix() => ModInformation.IsServer;
 
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.HourlyTick))]
-    [HarmonyPrefix]
-    static bool HourlyTickPrefix() => false;
+    //[HarmonyPatch(nameof(BanditsCampaignBehavior.DailyTick))]
+    //[HarmonyPrefix]
+    //static bool DailyTickPrefix() => ModInformation.IsServer;
 
-    [HarmonyPatch("OnNewGameCreatedPartialFollowUp")]
-    static bool Prefix() => false;
+    //[HarmonyPatch(nameof(BanditsCampaignBehavior.HourlyTick))]
+    //[HarmonyPrefix]
+    //static bool HourlyTickPrefix() => false;
 
-    [HarmonyPatch(nameof(BanditsCampaignBehavior.OnNewGameCreated))]
-    [HarmonyPrefix]
-    static bool OnNewGameCreatedPrefix() => ModInformation.IsServer;
+    //[HarmonyPatch("OnNewGameCreatedPartialFollowUp")]
+    //static bool Prefix() => false;
+
+    //[HarmonyPatch(nameof(BanditsCampaignBehavior.OnNewGameCreated))]
+    //[HarmonyPrefix]
+    //static bool OnNewGameCreatedPrefix() => ModInformation.IsServer;
 }
