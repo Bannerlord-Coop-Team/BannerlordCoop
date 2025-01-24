20b039276f1dd564435278737a233c618d32fd1e
Alexander Egard <veryhasted@gmail.com>
2025-01-24 17:23:47 +0100

    Start

---
+++ source/GameInterface/Services/MapEvents/MapEventSync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameInterface.AutoSync;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.MapEvents;
+
+namespace GameInterface.Services.MapEvents
+{
+    internal class MapEventSync : IAutoSync
+    {
+        public MapEventSync(IAutoSyncBuilder autoSyncBuilder)
+        {
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.AttackersRanAway)));
+            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.BattleObserver)));
+        }
+    }
+}
