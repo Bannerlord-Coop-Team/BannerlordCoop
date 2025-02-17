152b917c1e0a8acacf0c57d37bb3368f16fac2f7
Seras <langM1@gmx.net>
2025-02-17 21:20:36 +0100

    Added field sync

--- source/GameInterface/Services/Towns/TownSync.cs
+++ source/GameInterface/Services/Towns/TownSync.cs
@@ -1,8 +1,5 @@
 ﻿using GameInterface.AutoSync;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using TaleWorlds.CampaignSystem.Settlements;
 
 namespace GameInterface.Services.Towns;
@@ -10,6 +7,21 @@ internal class TownSync : IAutoSync
 {
     public TownSync(IAutoSyncBuilder autoSyncBuilder)
     {
+        // Already synced manually
         autoSyncBuilder.AddProperty(AccessTools.Property(typeof(Town), nameof(Town.Governor)));
+
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._prosperity)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._wallLevel)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._isCastle)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._ownerClan)));
+        // Not synced fields
+        // autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._security)));
+        // autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._loyalty)));
+        // Remove Prop or auto sync
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town.BoostBuildingProcess)));
+        // Remove Prop or auto sync
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._tradeTax)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town.InRebelliousState)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(Town), nameof(Town._governor)));
     }
 }
