3e378034a9eb0345d0432da613826d571c034cc0
Seras <langM1@gmx.net>
2025-02-20 22:16:24 +0100

    Extended E2ETestEnvironment with assertion methods

---
+++ source/E2E.Tests/Util/SyncTestBase.cs
@@ -0,0 +1,28 @@
+﻿using E2E.Tests.Environment;
+using HarmonyLib;
+using Xunit.Abstractions;
+using E2E.Tests.Environment.Instance;
+using TaleWorlds.Localization;
+using Newtonsoft.Json.Linq;
+
+namespace E2E.Tests.Util
+{
+    public abstract class SyncTestBase : IDisposable
+    {
+        internal E2ETestEnvironment TestEnvironment { get; }
+
+        internal EnvironmentInstance Server => TestEnvironment.Server;
+
+        internal IEnumerable<EnvironmentInstance> Clients => TestEnvironment.Clients;
+
+        public SyncTestBase(ITestOutputHelper output)
+        {
+            TestEnvironment = new E2ETestEnvironment(output);
+        }
+
+        public void Dispose()
+        {
+            TestEnvironment.Dispose();
+        }
+    }
+}
