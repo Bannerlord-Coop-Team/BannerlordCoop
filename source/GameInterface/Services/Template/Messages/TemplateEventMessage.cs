6f10b862885dc3cdb4f5e9b924a890809efd11ae
Garrett Luskey <garrett.luskey@gmail.com>
2025-03-18 20:59:42 -0500

    added back tests and updated tempates

---
+++ source/GameInterface/Services/Template/Messages/TemplateEventMessage.cs
@@ -0,0 +1,26 @@
+﻿using Common.Messaging;
+using TaleWorlds.CampaignSystem;
+
+namespace GameInterface.Services.Template.Messages;
+
+/// <summary>
+/// TODO update summary
+/// A event is a reaction to something
+/// Normally used in patches
+/// </summary>
+public record TemplateEventMessage : IEvent
+{
+    // This data can be anything you need
+    public TemplateEventMessage(Campaign instance, float value)
+    {
+        Instance = instance;
+        Value = value;
+    }
+
+    // Example instance
+    public Campaign Instance { get; }
+
+    // Example value
+    public float Value { get; }
+}
+
---
+++ source/GameInterface/Services/Template/Messages/TemplateNetworkMessage.cs
@@ -0,0 +1,26 @@
+﻿using Common.Messaging;
+using ProtoBuf;
+
+namespace GameInterface.Services.Template.Messages;
+/// <summary>
+/// TODO update summary
+/// A command changes the state of something
+/// </summary>
+// We use Protobuf for our serializer for network transferable classes
+[ProtoContract]
+public record TemplateNetworkMessage : ICommand
+{
+
+    // This data needs to be natively serializable or have a registered protobuf surrogate
+    [ProtoMember(1)]
+    public string NetworkId { get; }
+
+    [ProtoMember(2)]
+    public float Value { get; }
+
+    public TemplateNetworkMessage(string networkId, float value)
+    {
+        NetworkId = networkId;
+        Value = value;
+    }
+}
