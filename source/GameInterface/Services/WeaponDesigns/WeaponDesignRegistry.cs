19f58f27157a6543dfd32c549087564fdce4e13c
Alexander Egard <veryhasted@gmail.com>
2025-04-09 22:12:15 +0200

    Weapon Design

--- source/GameInterface/Services/WeaponDesigns/WeaponDesignRegistry.cs
+++ source/GameInterface/Services/WeaponDesigns/WeaponDesignRegistry.cs
@@ -1,36 +1,67 @@
-﻿using GameInterface.Registry;
+﻿using Common;
+using GameInterface.Registry;
+using GameInterface.Registry.Auto;
+using HarmonyLib;
+using Serilog;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
+using TaleWorlds.Localization;
 using static TaleWorlds.CampaignSystem.CampaignBehaviors.CraftingCampaignBehavior;
 
-namespace GameInterface.Services.ItemObjects
+namespace GameInterface.Services.WeaponDesigns
 {
-    internal class WeaponDesignRegistry : RegistryBase<WeaponDesign>
+    internal class WeaponDesignRegistry : IAutoRegistry<WeaponDesign>
     {
-        private const string ItemObjectIdPrefix = "CoopWeaponDesign";
-        private int InstanceCounter = 0;
+        ILogger Logger { get; }
 
-        public WeaponDesignRegistry(IRegistryCollection collection) : base(collection) { }
+        public IEnumerable<MethodBase> Constructors => new MethodBase[] {
+        AccessTools.Constructor(typeof(WeaponDesign), new Type[] { typeof(CraftingTemplate), typeof(TextObject), typeof(WeaponDesignElement[]) })
+    };
 
-        public override void RegisterAll()
+        public IEnumerable<MethodBase> DestroyMethods => Array.Empty<MethodBase>();
+
+        public WeaponDesignRegistry(ILogger logger, IAutoRegistryFactory autoRegistryFactory)
+        {
+            Logger = logger;
+
+            autoRegistryFactory.RegisterType(this);
+        }
+
+        public void RegisterAllObjects(IRegistry<WeaponDesign> registry)
         {
             var dict = Campaign.Current.GetCampaignBehavior<CraftingCampaignBehavior>()._craftedItemDictionary;
             foreach (KeyValuePair<ItemObject, CraftedItemInitializationData> craft in dict)
             {
                 var networkId = $"{nameof(WeaponDesign)}_{craft.Key.StringId}";
-                if (RegisterNewObject(craft.Value.CraftedData, out var _) == false)
-                {
+                if (registry.RegisterExistingObject(networkId, craft.Value.CraftedData) == false)
                     Logger.Error($"Unable to register {craft.Value.CraftedData}");
-                }
             }
         }
 
-        protected override string GetNewId(WeaponDesign obj)
+        public void OnClientCreated(WeaponDesign obj, string id)
         {
-            return $"{ItemObjectIdPrefix}_{Interlocked.Increment(ref InstanceCounter)}";
+
+        }
+
+        public void OnClientDestroyed(WeaponDesign obj, string id)
+        {
+
+        }
+
+        public void OnServerCreated(WeaponDesign obj, string id)
+        {
+
+        }
+
+        public void OnServerDestroyed(WeaponDesign obj, string id)
+        {
+
         }
     }
 }
