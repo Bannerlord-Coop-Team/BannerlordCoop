46d1845c39b292ff300f690e3f1dedd95a5644b0
Seras <langM1@gmx.net>
2025-06-09 21:57:37 +0200

    Test for Besiegercamps fails at SiegeEventCreation

---
+++ source/E2E.Tests/Services/BesiegerCamps/BesiegerCampSyncTests.cs
@@ -0,0 +1,34 @@
+﻿using E2E.Tests.Util;
+using TaleWorlds.CampaignSystem.Siege;
+using Xunit.Abstractions;
+using static TaleWorlds.CampaignSystem.Siege.SiegeEvent;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace E2E.Tests.Services.BesiegerCamps
+{
+    public class BesiegerCampSyncTests : SyncTestBase
+    {
+        public BesiegerCampSyncTests(ITestOutputHelper output) : base(output)
+        {
+            TestEnvironment.CreateRegisteredObject<SiegeEvent>();
+            TestEnvironment.CreateRegisteredObject<BesiegerCamp>();
+            TestEnvironment.CreateRegisteredObject<MobileParty>();
+        }
+
+        [Fact]
+        public void Server_BesiegerCamp_Fields()
+        {
+            TestEnvironment.AssertReferenceField<BesiegerCamp, MobileParty>(nameof(BesiegerCamp._leaderParty));
+        }
+
+        [Fact]
+        public void Server_BesiegerCamp_Properties()
+        {
+            // Properties
+            TestEnvironment.AssertProperty<BesiegerCamp, int>(nameof(BesiegerCamp.NumberOfTroopsKilledOnSide), 200);
+            TestEnvironment.AssertReferenceProperty<BesiegerCamp, SiegeEvent>(nameof(BesiegerCamp.SiegeEvent));
+            TestEnvironment.AssertReferenceProperty<BesiegerCamp, SiegeEnginesContainer>(nameof(BesiegerCamp.SiegeEngines));
+            TestEnvironment.AssertReferenceProperty < BesiegerCamp, SiegeStrategy>(nameof(BesiegerCamp.SiegeStrategy));
+        }
+    }
+}
--- source/GameInterface/Services/BesiegerCamps/BesiegerCampSync.cs
+++ source/GameInterface/Services/BesiegerCamps/BesiegerCampSync.cs
@@ -1,33 +1,23 @@
-﻿using Common.Logging;
-using GameInterface.AutoSync;
-using GameInterface.Registry.Auto;
+﻿using GameInterface.DynamicSync;
 using HarmonyLib;
-using Serilog;
-using System.Linq;
-using System.Reflection;
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Party;
-using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Siege;
-using TaleWorlds.Library;
-using TaleWorlds.Localization;
 
 namespace GameInterface.Services.BesiegerCamps
 {
-    internal class BesiegerCampSync : IAutoSync
+    internal class BesiegerCampSync : IDynamicSync
     {
-        readonly ILogger Logger = LogManager.GetLogger<BesiegerCampSync>();
-
-        public BesiegerCampSync(IAutoSyncBuilder autoSyncBuilder)
+        public BesiegerCampSync(DynamicSyncRegistry dynamicSyncRegistry)
         {
             // Fields
-            autoSyncBuilder.AddField(AccessTools.Field(typeof(BesiegerCamp), nameof(BesiegerCamp._leaderParty)));
+            dynamicSyncRegistry.AddField(AccessTools.Field(typeof(BesiegerCamp), nameof(BesiegerCamp._leaderParty)));  
 
-            // Props
-            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.NumberOfTroopsKilledOnSide)));
-            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeEvent)));
-            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeEngines)));
-            autoSyncBuilder.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeStrategy)));
+            // Properties
+            dynamicSyncRegistry.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.NumberOfTroopsKilledOnSide)));
+            dynamicSyncRegistry.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeEvent)));
+            dynamicSyncRegistry.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeEngines)));
+            dynamicSyncRegistry.AddProperty(AccessTools.Property(typeof(BesiegerCamp), nameof(BesiegerCamp.SiegeStrategy)));
+
+            // Targetmethods
         }
     }
 }
