93757b5fadbec973411275ff7920119407ebff03
Garrett Luskey <garrett.luskey@gmail.com>
2026-01-24 11:22:22 -0600

    PropertyOwner working

---
+++ source/GameInterface/Serialization/External/PropertyOwner_PerkObject_BinaryPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Core;
+
+namespace GameInterface.Serialization.External;
+
+[Serializable]
+public class PropertyOwner_PerkObject_BinaryPackage : BinaryPackageBase<PropertyOwner<PerkObject>>
+{
+    public Dictionary<string, int> SerializedAttributes;
+
+    public PropertyOwner_PerkObject_BinaryPackage(PropertyOwner<PerkObject> obj, IBinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+    {
+    }
+    protected override void PackInternal()
+    {
+        SerializedAttributes = Object._attributes.ToDictionary(x => ResolveId(x.Key), x => x.Value);
+    }
+    protected override void UnpackInternal()
+    {
+        Object = new PropertyOwner<PerkObject>();
+
+        foreach (var x in SerializedAttributes)
+        {
+            Object.SetPropertyValue(ResolveObject<PerkObject>(x.Key), x.Value);
+        }
+    }
+}
---
+++ source/GameInterface/Serialization/External/PropertyOwner_SkillObject_BinaryPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Core;
+
+namespace GameInterface.Serialization.External;
+
+[Serializable]
+public class PropertyOwner_SkillObject_BinaryPackage : BinaryPackageBase<PropertyOwner<SkillObject>>
+{
+    public Dictionary<string, int> SerializedAttributes;
+
+    public PropertyOwner_SkillObject_BinaryPackage(PropertyOwner<SkillObject> obj, IBinaryPackageFactory binaryPackageFactory) : base(obj, binaryPackageFactory)
+    {
+    }
+    protected override void PackInternal()
+    {
+        SerializedAttributes = Object._attributes.ToDictionary(x => ResolveId(x.Key), x => x.Value);
+    }
+    protected override void UnpackInternal()
+    {
+        Object = new PropertyOwner<SkillObject>();
+
+        foreach (var x in SerializedAttributes)
+        {
+            Object.SetPropertyValue(ResolveObject<SkillObject>(x.Key), x.Value);
+        }
+    }
+}
