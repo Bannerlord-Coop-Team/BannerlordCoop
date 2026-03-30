60eb627dd53d3544130b84a4a38be25d528aa60f
Alexander Egard <veryhasted@gmail.com>
2026-03-30 23:48:56 +0200

    reduced ai disabling

--- source/GameInterface/Services/MapEvents/Handlers/MapEventHandler.cs
+++ source/GameInterface/Services/MapEvents/Handlers/MapEventHandler.cs
@@ -1,143 +1,142 @@
-﻿//using Common;
-//using Common.Logging;
-//using Common.Messaging;
-//using Common.Network;
-//using Common.Util;
-//using GameInterface.Services.MapEvents.Messages;
-//using GameInterface.Services.ObjectManager;
-//using Serilog;
-//using System;
-//using TaleWorlds.CampaignSystem;
-//using TaleWorlds.CampaignSystem.MapEvents;
+﻿using Common;
+using Common.Logging;
+using Common.Messaging;
+using Common.Network;
+using Common.Util;
+using GameInterface.Services.MapEvents.Messages;
+using GameInterface.Services.ObjectManager;
+using Serilog;
+using TaleWorlds.CampaignSystem.MapEvents;
 
-//namespace GameInterface.Services.MapEvents.Handlers;
-//internal class MapEventHandler : IHandler
-//{
-//    private static readonly ILogger Logger = LogManager.GetLogger<MapEventHandler>();
+namespace GameInterface.Services.MapEvents.Handlers;
 
-//    private readonly IMessageBroker messageBroker;
-//    private readonly INetwork network;
-//    private readonly IObjectManager objectManager;
+internal class MapEventHandler : IHandler
+{
+    private static readonly ILogger Logger = LogManager.GetLogger<MapEventHandler>();
 
-//    public MapEventHandler(IMessageBroker messageBroker, INetwork network, IObjectManager objectManager)
-//    {
-//        this.messageBroker = messageBroker;
-//        this.network = network;
-//        this.objectManager = objectManager;
-//        messageBroker.Subscribe<MapEventCreated>(Handle);
-//        messageBroker.Subscribe<NetworkCreateMapEvent>(Handle);
+    private readonly IMessageBroker messageBroker;
+    private readonly INetwork network;
+    private readonly IObjectManager objectManager;
 
-//        messageBroker.Subscribe<MapEventSidesArrayUpdated>(Handle);
-//        messageBroker.Subscribe<NetworkUpdateMapSidesArray>(Handle);
-        
+    public MapEventHandler(IMessageBroker messageBroker, INetwork network, IObjectManager objectManager)
+    {
+        this.messageBroker = messageBroker;
+        this.network = network;
+        this.objectManager = objectManager;
+        messageBroker.Subscribe<MapEventCreated>(Handle);
+        messageBroker.Subscribe<NetworkCreateMapEvent>(Handle);
 
-//        messageBroker.Subscribe<MapEventDestroyed>(Handle);
-//        messageBroker.Subscribe<NetworkDestroyMapEvent>(Handle);
-//    }
+        messageBroker.Subscribe<MapEventSidesArrayUpdated>(Handle);
+        messageBroker.Subscribe<NetworkUpdateMapSidesArray>(Handle);
 
 
-
-//    public void Dispose()
-//    {
-//        messageBroker.Unsubscribe<MapEventCreated>(Handle);
-//        messageBroker.Unsubscribe<NetworkCreateMapEvent>(Handle);
-
-//        messageBroker.Unsubscribe<MapEventSidesArrayUpdated>(Handle);
-//        messageBroker.Unsubscribe<NetworkUpdateMapSidesArray>(Handle);
-
-//        messageBroker.Unsubscribe<MapEventDestroyed>(Handle);
-//        messageBroker.Unsubscribe<NetworkDestroyMapEvent>(Handle);
-//    }
-
-//    private void Handle(MessagePayload<MapEventCreated> payload)
-//    {
-//        objectManager.AddNewObject(payload.What.Instance, out var id);
-
-//        network.SendAll(new NetworkCreateMapEvent(id));
-//    }
+        messageBroker.Subscribe<MapEventDestroyed>(Handle);
+        messageBroker.Subscribe<NetworkDestroyMapEvent>(Handle);
+    }
 
 
-//    private void Handle(MessagePayload<NetworkCreateMapEvent> payload)
-//    {
-//        var newMapEvent = ObjectHelper.SkipConstructor<MapEvent>();
 
-//        // TODO find better way of doing this
-//        newMapEvent._sides = new MapEventSide[2];
+    public void Dispose()
+    {
+        messageBroker.Unsubscribe<MapEventCreated>(Handle);
+        messageBroker.Unsubscribe<NetworkCreateMapEvent>(Handle);
 
-//        objectManager.AddExisting(payload.What.MapEventId, newMapEvent);
-//    }
+        messageBroker.Unsubscribe<MapEventSidesArrayUpdated>(Handle);
+        messageBroker.Unsubscribe<NetworkUpdateMapSidesArray>(Handle);
 
-//    private void Handle(MessagePayload<MapEventDestroyed> payload)
-//    {
-//        var mapEvent = payload.What.Instance;
-//        if (objectManager.TryGetId(mapEvent, out var mapEventId) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), mapEventId);
-//            return;
-//        }
+        messageBroker.Unsubscribe<MapEventDestroyed>(Handle);
+        messageBroker.Unsubscribe<NetworkDestroyMapEvent>(Handle);
+    }
 
-//        objectManager.Remove(payload.What.Instance);
+    private void Handle(MessagePayload<MapEventCreated> payload)
+    {
+        objectManager.AddNewObject(payload.What.Instance, out var id);
 
-//        network.SendAll(new NetworkDestroyMapEvent(mapEventId));
-//    }
-
-//    private void Handle(MessagePayload<NetworkDestroyMapEvent> payload)
-//    {
-//        if (objectManager.TryGetObject<MapEvent>(payload.What.MapEventId, out var mapEvent) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), payload.What.MapEventId);
-//            return;
-//        }
-
-//        objectManager.Remove(mapEvent);
-
-//        GameLoopRunner.RunOnMainThread(() =>
-//        {
-//            using (new AllowedThread())
-//            {
-//                mapEvent.FinalizeEvent();
-//            }
-//        });
-//    }
-
-//    private void Handle(MessagePayload<MapEventSidesArrayUpdated> payload)
-//    {
-//        var mapEvent = payload.What.Instance;
-//        if (objectManager.TryGetId(mapEvent, out var instanceId) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), mapEvent);
-//            return;
-//        }
-
-//        var value = payload.What.Value;
-//        if (objectManager.TryGetId(value, out var valueId) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEventSide), value);
-//            return;
-//        }
+        network.SendAll(new NetworkCreateMapEvent(id));
+    }
 
 
-//        network.SendAll(new NetworkUpdateMapSidesArray(instanceId, valueId, payload.What.Index));
-//    }
+    private void Handle(MessagePayload<NetworkCreateMapEvent> payload)
+    {
+        var newMapEvent = ObjectHelper.SkipConstructor<MapEvent>();
 
-//    private void Handle(MessagePayload<NetworkUpdateMapSidesArray> payload)
-//    {
-//        var instanceId = payload.What.InstanceId;
-//        var valueId = payload.What.ValueId;
-//        var index = payload.What.Index;
+        // TODO find better way of doing this
+        newMapEvent._sides = new MapEventSide[2];
 
-//        if (objectManager.TryGetObject<MapEvent>(instanceId, out var mapEvent) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), instanceId);
-//            return;
-//        }
+        objectManager.AddExisting(payload.What.MapEventId, newMapEvent);
+    }
 
-//        if (objectManager.TryGetObject<MapEventSide>(valueId, out var mapEventSide) == false)
-//        {
-//            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEventSide), valueId);
-//            return;
-//        }
+    private void Handle(MessagePayload<MapEventDestroyed> payload)
+    {
+        var mapEvent = payload.What.Instance;
+        if (objectManager.TryGetId(mapEvent, out var mapEventId) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), mapEventId);
+            return;
+        }
 
-//        mapEvent._sides[index] = mapEventSide;
-//    }
-//}
+        objectManager.Remove(payload.What.Instance);
+
+        network.SendAll(new NetworkDestroyMapEvent(mapEventId));
+    }
+
+    private void Handle(MessagePayload<NetworkDestroyMapEvent> payload)
+    {
+        if (objectManager.TryGetObject<MapEvent>(payload.What.MapEventId, out var mapEvent) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), payload.What.MapEventId);
+            return;
+        }
+
+        objectManager.Remove(mapEvent);
+
+        GameLoopRunner.RunOnMainThread(() =>
+        {
+            using (new AllowedThread())
+            {
+                mapEvent.FinalizeEvent();
+            }
+        });
+    }
+
+    private void Handle(MessagePayload<MapEventSidesArrayUpdated> payload)
+    {
+        var mapEvent = payload.What.Instance;
+        if (objectManager.TryGetId(mapEvent, out var instanceId) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), mapEvent);
+            return;
+        }
+
+        var value = payload.What.Value;
+        if (objectManager.TryGetId(value, out var valueId) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEventSide), value);
+            return;
+        }
+
+
+        network.SendAll(new NetworkUpdateMapSidesArray(instanceId, valueId, payload.What.Index));
+    }
+
+    private void Handle(MessagePayload<NetworkUpdateMapSidesArray> payload)
+    {
+        var instanceId = payload.What.InstanceId;
+        var valueId = payload.What.ValueId;
+        var index = payload.What.Index;
+
+        if (objectManager.TryGetObject<MapEvent>(instanceId, out var mapEvent) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEvent), instanceId);
+            return;
+        }
+
+        if (objectManager.TryGetObject<MapEventSide>(valueId, out var mapEventSide) == false)
+        {
+            Logger.Error("Unable to get {type} if from {obj}", nameof(MapEventSide), valueId);
+            return;
+        }
+
+        mapEvent._sides[index] = mapEventSide;
+    }
+}
--- source/GameInterface/Services/MapEvents/MapEventSync.cs
+++ source/GameInterface/Services/MapEvents/MapEventSync.cs
@@ -9,26 +9,26 @@ internal class MapEventSync : IDynamicSync
 {
     public MapEventSync(DynamicSyncRegistry autoSyncBuilder)
     {
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.RetreatingSide)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.BattleState)));
-        ////autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.Component)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsInvulnerable)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsPlayerSimulation)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsVisible)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.MapEventSettlement)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.Position)));
-        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.State)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.RetreatingSide)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.BattleState)));
+        //autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.Component)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsInvulnerable)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsPlayerSimulation)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.IsVisible)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.MapEventSettlement)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.Position)));
+        autoSyncBuilder.AddProperty(AccessTools.Property(typeof(MapEvent), nameof(MapEvent.State)));
 
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent.DiplomaticallyFinished)));
-        ////autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._battleResultExplainers)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventResultsCalculated)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._battleState)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._eventTerrainType)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._isFinishCalled)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._isVisible)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._keepSiegeEvent)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventStartTime)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventType)));
-        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventResultsApplied)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent.DiplomaticallyFinished)));
+        //autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._battleResultExplainers)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventResultsCalculated)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._battleState)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._eventTerrainType)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._isFinishCalled)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._isVisible)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._keepSiegeEvent)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventStartTime)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventType)));
+        autoSyncBuilder.AddField(AccessTools.Field(typeof(MapEvent), nameof(MapEvent._mapEventResultsApplied)));
     }
 }
