7a72a0bc92fefaf5743d6e3ce21376563d251116
Garrett Luskey <garrett.luskey@gmail.com>
2026-05-02 14:25:13 -0500

    fixed ai

--- source/Coop.Core/Client/Services/MobileParties/Messages/NetworkRequestMobilePartyControl.cs
+++ source/Coop.Core/Client/Services/MobileParties/Messages/NetworkRequestMobilePartyControl.cs
@@ -7,12 +7,12 @@ namespace Coop.Core.Client.Services.MobileParties.Messages;
 /// Request control of a mobile party entity.
 /// </summary>
 [ProtoContract(SkipConstructor = true)]
-public record NetworkRequestMobilePartyControl : ICommand
+public readonly struct NetworkRequestMobilePartyControl : ICommand
 {
     [ProtoMember(1)]
-    public string ControllerId;
+    public readonly string ControllerId;
     [ProtoMember(2)]
-    public string PartyId;
+    public readonly string PartyId;
 
     public NetworkRequestMobilePartyControl(string controllerId, string partyId)
     {
