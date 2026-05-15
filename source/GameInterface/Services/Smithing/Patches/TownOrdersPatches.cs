46856803e4a10ce35c7e2f3339757f6d7d5aa75d
jordanbrymora <jordanbrymora@gmail.com>
2026-05-15 22:44:52 +1000

    Moved CampaignBehaviorManager patch to its own service

--- source/GameInterface/Services/Smithing/Patches/TownOrdersPatches.cs
+++ source/GameInterface/Services/Smithing/Patches/TownOrdersPatches.cs
@@ -108,26 +108,4 @@ namespace GameInterface.Services.Smithing.Patches
             return false;
         }
     }
-
-    [HarmonyPatch(typeof(CampaignBehaviorManager))]
-    internal class LoadBehaviorDataPatch
-    {
-        private static readonly ILogger Logger = LogManager.GetLogger<CampaignBehaviorManager>();
-
-        [HarmonyPatch("LoadBehaviorData")]
-        [HarmonyPrefix]
-        public static bool LoadBehaviorData(ref CampaignBehaviorManager __instance)
-        {
-            if (ModInformation.IsClient) return true;
-
-            foreach (CampaignBehaviorBase campaignBehavior in __instance._campaignBehaviors)
-            {
-                __instance._campaignBehaviorDataStore.LoadBehaviorData(campaignBehavior);
-            }
-            //_instance._campaignBehaviorDataStore.ClearBehaviorData();
-
-            // Skip original on server
-            return false;
-        }
-    }
 }
