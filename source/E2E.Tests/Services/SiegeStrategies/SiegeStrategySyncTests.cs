71e512d69f1e473c0b0867975547a04f396bad73
NatteTosti69 <100526621+NatteTosti69@users.noreply.github.com>
2026-05-19 21:38:17 +0200

    added property sync

---
+++ source/E2E.Tests/Services/SiegeStrategies/SiegeStrategySyncTests.cs
@@ -0,0 +1,30 @@
+﻿using E2E.Tests.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.Siege;
+using Xunit.Abstractions;
+
+namespace E2E.Tests.Services.SiegeStrategies
+{
+    public class SiegeStrategySyncTests : SyncTestBase
+    {
+        private string siegeStrategyId;
+
+        public SiegeStrategySyncTests(ITestOutputHelper output) : base(output)
+        {
+            siegeStrategyId = TestEnvironment.CreateRegisteredObject<SiegeStrategy>();
+        }
+        [Fact]
+        public void Server_SiegeStrategy_Properties()
+        {
+            // Arrange
+            Assert.True(Server.ObjectManager.TryGetObject(siegeStrategyId, out SiegeStrategy siegeStrategy));
+
+            // Assert
+            TestEnvironment.AssertProperty<SiegeStrategy, string>(nameof(SiegeStrategy.Name), "testStrategy");
+            TestEnvironment.AssertProperty<SiegeStrategy, string>(nameof(SiegeStrategy.Description), "testStrategy_description");
+        }
+    }
+}
