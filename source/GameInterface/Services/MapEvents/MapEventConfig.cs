cd2bf56b6204803249487276a5aa6f33650bffaf
Garrett Luskey <garrett.luskey@gmail.com>
2026-05-19 18:22:46 -0500

    Updated disable

---
+++ source/GameInterface/Services/MapEvents/MapEventConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameInterface.Services.MapEvents;
+
+internal static class MapEventConfig
+{
+    public const bool Enabled = true;
+}
