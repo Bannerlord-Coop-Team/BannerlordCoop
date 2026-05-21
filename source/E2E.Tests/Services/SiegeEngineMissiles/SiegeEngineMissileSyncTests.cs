ec2f8f898b2ea66a22e00cc76cf34ba8b7fb06bd
NatteTosti69 <100526621+NatteTosti69@users.noreply.github.com>
2026-05-21 18:15:52 +0200

    Added tests for fields
    Tests dont work because SiegeEngineMissile is a nested class inside SiegeEvent

---
+++ source/E2E.Tests/Services/SiegeEngineMissiles/SiegeEngineMissileSyncTests.cs
@@ -0,0 +1,33 @@
+﻿using E2E.Tests.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.Siege;
+using TaleWorlds.Core;
+using Xunit.Abstractions;
+
+namespace E2E.Tests.Services.SiegeEngineMissiles
+{
+    public class SiegeEngineMissileSyncTests : SyncTestBase
+    {
+        string siegeEngineMissileId;
+        public SiegeEngineMissileSyncTests(ITestOutputHelper output) : base(output)
+        {
+            siegeEngineMissileId = TestEnvironment.CreateRegisteredObject<SiegeEvent.SiegeEngineMissile>();
+        }
+        [Fact]
+        public void Server_Settlement_Fields()
+        {
+            Server.ObjectManager.TryGetObject(siegeEngineMissileId, out SiegeEvent.SiegeEngineMissile siegeEngineMissile);
+
+
+            //TestEnvironment.AssertField<SiegeEvent.SiegeEngineMissile,int>(nameof(SiegeEvent.SiegeEngineMissile.ShooterSlotIndex), 2);
+            //TestEnvironment.AssertReferenceField<SiegeEvent.SiegeEngineMissile, SiegeEngineType>(nameof(SiegeEvent.SiegeEngineMissile.ShooterSiegeEngineType));
+
+        }
+
+    }
+}
