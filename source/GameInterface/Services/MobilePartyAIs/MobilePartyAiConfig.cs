88861a381f5a3d62b8ac655324158416f88250a7
Garrett Luskey <garrett.luskey@gmail.com>
2026-05-31 20:24:48 -0500

    Enabled ai

--- source/GameInterface/Services/MobilePartyAIs/MobilePartyAiConfig.cs
+++ source/GameInterface/Services/MobilePartyAIs/MobilePartyAiConfig.cs
@@ -6,6 +6,6 @@ namespace GameInterface.Services.MobilePartyAIs;
 
 internal class MobilePartyAiConfig
 {
+    public const bool ENABLED = true;
     public const bool DEBUG = false;
-    public const bool DISABLE_AI = true;
 }
