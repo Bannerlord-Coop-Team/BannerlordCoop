074a387b69cb944abfc3daf8c16ea178e47928fb
jordanbrymora <jordanbrymora@gmail.com>
2026-06-07 17:18:02 +1000

    Initial PartyHealingSync

--- source/GameInterface/Services/MobileParties/Patches/Disable/DisablePartyHealCampaignBehavior.cs
+++ source/GameInterface/Services/MobileParties/Patches/Disable/DisablePartyHealCampaignBehavior.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using Common;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
 
 namespace GameInterface.Services.MobileParties.Patches;
@@ -7,5 +8,5 @@ namespace GameInterface.Services.MobileParties.Patches;
 internal class DisablePartyHealCampaignBehavior
 {
     [HarmonyPatch(nameof(PartyHealCampaignBehavior.RegisterEvents))]
-    static bool Prefix() => false;
+    static bool Prefix() => ModInformation.IsServer;
 }
