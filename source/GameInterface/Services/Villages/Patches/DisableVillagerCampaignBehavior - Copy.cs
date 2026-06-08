eadecf10efd126bc858fad668104bf6fa171e605
Garrett Luskey <garrett.luskey@gmail.com>
2026-06-07 20:09:52 -0500

    Fix for not being able to get into map events

--- source/GameInterface/Services/Villages/Patches/DisableVillagerCampaignBehavior - Copy.cs
+++
@@ -1,9 +0,0 @@
-﻿using HarmonyLib;
-
-namespace GameInterface.Services.Villages.Patches;
-
-[HarmonyPatch("VillageTradeBoundCampaignBehavior", "RegisterEvents")]
-internal class DisableVillageTradeBoundCampaignBehavior
-{
-    static bool Prefix() => false;
-}
