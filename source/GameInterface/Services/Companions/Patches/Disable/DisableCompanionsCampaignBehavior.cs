e651b38b6d2b624515961bb49c70c4923d803260
jordanbrymora <jordanbrymora@gmail.com>
2026-06-10 00:50:58 +1000

    Remove unused debugging patch

--- source/GameInterface/Services/Companions/Patches/Disable/DisableCompanionsCampaignBehavior.cs
+++ source/GameInterface/Services/Companions/Patches/Disable/DisableCompanionsCampaignBehavior.cs
@@ -1,7 +1,6 @@
 ﻿using Common;
 using HarmonyLib;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
-using TaleWorlds.CampaignSystem.Settlements;
 
 namespace GameInterface.Services.Companions.Patches.Disable
 {
@@ -10,12 +9,5 @@ namespace GameInterface.Services.Companions.Patches.Disable
     {
         [HarmonyPatch(nameof(CompanionsCampaignBehavior.RegisterEvents))]
         static bool Prefix() => ModInformation.IsServer;
-
-        [HarmonyPatch(nameof(CompanionsCampaignBehavior.CreateCompanionAndAddToSettlement))]
-        [HarmonyPostfix]
-        public static void CreateCompanionAndAddToSettlementPostfix(Settlement settlement)
-        {
-            return;
-        }
     }
 }
