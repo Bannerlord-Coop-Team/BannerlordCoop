8d070b805fa7a3d8598d34f84882add8e6b1cc1d
jordanbrymora <jordanbrymora@gmail.com>
2026-06-11 11:56:40 +1000

    Disabled OnHeroRelationChanged on clients
    OnCompanionRemoved shouldn't need to be synced. Companions can't be dismissed in settlements and so the current location complex doesn't need to be updated.

--- source/GameInterface/Services/Companions/Patches/Disable/DisableCompanionRolesCampaignBehavior.cs
+++ source/GameInterface/Services/Companions/Patches/Disable/DisableCompanionRolesCampaignBehavior.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using Common;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
 
 namespace GameInterface.Services.Companions.Patches.Disable
@@ -8,6 +11,17 @@ namespace GameInterface.Services.Companions.Patches.Disable
     internal class DisableCompanionRolesCampaignBehavior
     {
         [HarmonyPatch(nameof(CompanionRolesCampaignBehavior.RegisterEvents))]
-        static bool Prefix() => true;
+        static bool RegisterEventsPrefix() => true;
+
+        // Disable these methods on the client
+        private static IEnumerable<MethodBase> TargetMethods() => new MethodBase[]
+        {
+            AccessTools.Method(typeof(CompanionRolesCampaignBehavior), nameof(CompanionRolesCampaignBehavior.OnHeroRelationChanged))
+        };
+
+        static bool Prefix()
+        {
+            return ModInformation.IsServer;
+        }
     }
 }
