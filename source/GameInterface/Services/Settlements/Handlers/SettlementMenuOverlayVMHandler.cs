64786e91257f0a56a27a85b5379e5d6de621e5cd
jordanbrymora <jordanbrymora@gmail.com>
2026-06-13 01:25:17 +1000

    Sync assigning clan members to settlement

---
+++ source/GameInterface/Services/Settlements/Handlers/SettlementMenuOverlayVMHandler.cs
@@ -0,0 +1,75 @@
+﻿using Common.Logging;
+using Common.Messaging;
+using Common.Network;
+using GameInterface.Services.ObjectManager;
+using GameInterface.Services.Settlements.Messages;
+using Serilog;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace GameInterface.Services.Settlements.Handlers;
+
+internal class SettlementMenuOverlayVMHandler : IHandler
+{
+    private static readonly ILogger Logger = LogManager.GetLogger<SettlementMenuOverlayVMHandler>();
+
+    private readonly IMessageBroker messageBroker;
+    private readonly IObjectManager objectManager;
+    private readonly INetwork network;
+
+    public SettlementMenuOverlayVMHandler(
+        IMessageBroker messageBroker,
+        IObjectManager objectManager,
+        INetwork network)
+    {
+        this.messageBroker = messageBroker;
+        this.objectManager = objectManager;
+        this.network = network;
+
+        messageBroker.Subscribe<ClanMembersAssignedToSettlement>(Handle_ClanMembersAssignedToSettlement);
+        messageBroker.Subscribe<AssignClanMembersToSettlement>(Handle_AssignClanMembersToSettlement);
+    }
+
+    public void Dispose()
+    {
+        messageBroker.Unsubscribe<ClanMembersAssignedToSettlement>(Handle_ClanMembersAssignedToSettlement);
+        messageBroker.Unsubscribe<AssignClanMembersToSettlement>(Handle_AssignClanMembersToSettlement);
+    }
+
+    private void Handle_ClanMembersAssignedToSettlement(MessagePayload<ClanMembersAssignedToSettlement> obj)
+    {
+        if (!objectManager.TryGetIdWithLogging(obj.What.Settlement, out var settlementId)) return;
+        if (!objectManager.TryGetIdWithLogging(obj.What.MainParty, out var mainPartyId)) return;
+
+        List<string> heroIds = new();
+        foreach (var hero in obj.What.LeftHeroes)
+        {
+            if (!objectManager.TryGetIdWithLogging(hero, out var heroId)) continue;
+
+            heroIds.Add(heroId);
+        }
+
+        var message = new AssignClanMembersToSettlement(settlementId, mainPartyId, heroIds);
+        network.SendAll(message);
+    }
+
+    private void Handle_AssignClanMembersToSettlement(MessagePayload<AssignClanMembersToSettlement> obj)
+    {
+        if (!objectManager.TryGetObjectWithLogging<Settlement>(obj.What.SettlementId, out var settlement)) return;
+        if (!objectManager.TryGetObjectWithLogging<MobileParty>(obj.What.MainPartyId, out var mainParty)) return;
+
+        foreach (var heroId in obj.What.HeroIds)
+        {
+            if (!objectManager.TryGetObjectWithLogging<Hero>(heroId, out var hero)) continue;
+
+            mainParty.MemberRoster.RemoveTroop(hero.CharacterObject, 1, default, 0);
+            if (!settlement.HeroesWithoutParty.Contains(hero))
+            {
+                EnterSettlementAction.ApplyForCharacterOnly(hero, settlement);
+            }
+        }
+    }
+}
---
+++ source/GameInterface/Services/Settlements/Messages/AssignClanMembersToSettlement.cs
@@ -0,0 +1,25 @@
+﻿using Common.Messaging;
+using ProtoBuf;
+using System.Collections.Generic;
+
+namespace GameInterface.Services.Settlements.Messages;
+
+[ProtoContract(SkipConstructor = true)]
+internal readonly struct AssignClanMembersToSettlement : ICommand
+{
+    [ProtoMember(1)]
+    public readonly string SettlementId;
+
+    [ProtoMember(2)]
+    public readonly string MainPartyId;
+
+    [ProtoMember(3)]
+    public readonly List<string> HeroIds;
+
+    public AssignClanMembersToSettlement(string settlementId, string mainPartyId, List<string> heroIds)
+    {
+        SettlementId = settlementId;
+        MainPartyId = mainPartyId;
+        HeroIds = heroIds;
+    }
+}
