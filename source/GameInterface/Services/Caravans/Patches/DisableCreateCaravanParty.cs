e287ccf56696f39923e6e83e4c61b3f0359606a2
jordanbrymora <jordanbrymora@gmail.com>
2026-06-16 10:50:04 +1000

    Disable caravan spawning

---
+++ source/GameInterface/Services/Caravans/Patches/DisableCreateCaravanParty.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.Party.PartyComponents;
+
+namespace GameInterface.Services.Caravans.Patches;
+
+[HarmonyPatch(typeof(CaravanPartyComponent))]
+internal class DisableCreateCaravanParty
+{
+    [HarmonyPatch(nameof(CaravanPartyComponent.CreateCaravanParty))]
+    static bool Prefix() => false;
+}
