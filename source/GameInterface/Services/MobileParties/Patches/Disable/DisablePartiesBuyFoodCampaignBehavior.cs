7dd78e15100b1f655c7169a2f986760e27fb20fa
jordanbrymora <jordanbrymora@gmail.com>
2026-07-08 12:08:40 +1000

    Variety of campaign behaviors affecting settlement gold enabled

--- source/GameInterface/Services/MobileParties/Patches/Disable/DisablePartiesBuyFoodCampaignBehavior.cs
+++ source/GameInterface/Services/MobileParties/Patches/Disable/DisablePartiesBuyFoodCampaignBehavior.cs
@@ -1,11 +1,27 @@
-﻿using HarmonyLib;
+﻿using Common;
+using GameInterface.Services.MobileParties.Extensions;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
 
-namespace GameInterface.Services.MobileParties.Patches;
+namespace GameInterface.Services.MobileParties.Patches.Disable;
 
 [HarmonyPatch(typeof(PartiesBuyFoodCampaignBehavior))]
 internal class DisablePartiesBuyFoodCampaignBehavior
 {
+    // BuyFoodInternal & BuyFoodForArmy use IsMainParty which is handled by IsMainPartyPatch
     [HarmonyPatch(nameof(PartiesBuyFoodCampaignBehavior.RegisterEvents))]
-    static bool Prefix() => false;
+    static bool Prefix() => ModInformation.IsServer;
+}
+
+[HarmonyPatch(typeof(PartiesBuyFoodCampaignBehavior))]
+internal class PartiesBuyFoodCampaignBehaviorPatches
+{
+    [HarmonyPatch(nameof(PartiesBuyFoodCampaignBehavior.BuyFoodInternal))]
+    [HarmonyPrefix]
+    public static bool BuyFoodInternalPrefix(PartiesBuyFoodCampaignBehavior __instance, MobileParty mobileParty, Settlement settlement, int numberOfFoodItemsNeededToBuy)
+    {
+        return mobileParty != null && !mobileParty.IsPlayerParty();
+    }
 }
