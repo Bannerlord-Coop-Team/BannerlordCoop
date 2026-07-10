99128028353a0a6cb1246a61e94c3983c3f90e4d
Andrew Orlowski <57453461+uniqueuser-repo@users.noreply.github.com>
2026-07-10 11:59:55 -0500

    Fix missing using after development merge

--- source/GameInterface/Services/MapEvents/Handlers/BattleFinalizeHandler.cs
+++ source/GameInterface/Services/MapEvents/Handlers/BattleFinalizeHandler.cs
@@ -17,6 +17,7 @@ using GameInterface.Services.SiegeEvents.Patches;
 using LiteNetLib;
 using Serilog;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using TaleWorlds.CampaignSystem;
