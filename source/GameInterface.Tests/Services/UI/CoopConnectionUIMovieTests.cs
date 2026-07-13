184e8bce799b6eb5cb427d2b8532c3d9181b0518
Andrew Orlowski <57453461+uniqueuser-repo@users.noreply.github.com>
2026-07-13 10:53:59 -0500

    Inset incompatible lobby status

--- source/GameInterface.Tests/Services/UI/CoopConnectionUIMovieTests.cs
+++ source/GameInterface.Tests/Services/UI/CoopConnectionUIMovieTests.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Xml.Linq;
 using Xunit;
@@ -12,19 +11,20 @@ public class CoopConnectionUIMovieTests
     public void IncompatibleStatusHint_KeepsRowBindingsOutsideHintDataSource()
     {
         var document = XDocument.Load(FindMoviePath());
-        var hint = Assert.Single(document.Descendants("HintWidget")
-            .Where(element => (string)element.Attribute("DataSource") == "{StatusHint}"));
+        var hint = Assert.Single(document.Descendants("HintWidget"),
+            element => element.Attribute("DataSource")?.Value == "{StatusHint}");
 
         Assert.Null(hint.Attribute("IsVisible"));
 
         var container = Assert.IsType<XElement>(hint.Parent?.Parent);
         Assert.Equal("Widget", container.Name.LocalName);
-        Assert.Equal("@IsStatusHintVisible", (string)container.Attribute("IsVisible"));
+        Assert.Equal("@IsStatusHintVisible", container.Attribute("IsVisible")?.Value);
+        Assert.Equal("28", container.Attribute("MarginRight")?.Value);
         Assert.Null(container.Attribute("DataSource"));
 
         var label = Assert.Single(hint.Descendants("TextWidget"));
-        Assert.Equal("Incompatible (i)", (string)label.Attribute("Text"));
-        Assert.Equal("#FF8080FF", (string)label.Attribute("Brush.FontColor"));
+        Assert.Equal("Incompatible (i)", label.Attribute("Text")?.Value);
+        Assert.Equal("#FF8080FF", label.Attribute("Brush.FontColor")?.Value);
     }
 
     private static string FindMoviePath([CallerFilePath] string sourceFile = "")
