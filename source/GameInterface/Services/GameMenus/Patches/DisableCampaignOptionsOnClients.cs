29e5a332a600190ef638b846708a45cfc966ca81
jordanbrymora <jordanbrymora@gmail.com>
2026-07-14 19:57:39 +1000

    Sync campaign options + disable buttons on clients

---
+++ source/GameInterface/Services/GameMenus/Patches/DisableCampaignOptionsOnClients.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+
+namespace GameInterface.Services.GameMenus.Patches;
+
+/// <summary>
+/// Disable changing campaign options on clients.
+/// This solution still lets clients see the current configuration while preventing any changes.
+/// </summary>
+[HarmonyPatch(typeof(CampaignOptionData))]
+internal class DisableManagingCampaignOptionsOnClients
+{
+    [HarmonyPatch(nameof(CampaignOptionData.GetIsDisabledWithReason))]
+    [HarmonyPrefix]
+    public static bool GetIsDisabledWithReasonPrefix(CampaignOptionData __instance, ref CampaignOptionDisableStatus __result)
+    {
+        if (ModInformation.IsClient)
+        {
+            __result = new(true, "Managing campaign options is disabled on clients; the host does this.", -1f);
+            return false;
+        }
+
+        return true;
+    }
+}
