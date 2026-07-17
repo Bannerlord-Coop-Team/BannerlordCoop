0ba5275e56d0f0e5f247072fbd2e8821ebb58888
Andrew Orlowski <57453461+uniqueuser-repo@users.noreply.github.com>
2026-07-16 21:44:27 -0500

    Test packet profiler client bypass

---
+++ source/Common.Tests/Logging/PacketProfilerRoleTests.cs
@@ -0,0 +1,31 @@
+﻿using Common.Logging;
+using System;
+using Xunit;
+
+namespace Common.Tests.Logging;
+
+/// <summary>
+/// Verifies packet profiling stays disabled on clients.
+/// </summary>
+[Collection(ModInformationRoleCollection.Name)]
+public sealed class PacketProfilerRoleTests : IDisposable
+{
+    private readonly bool wasServer = ModInformation.IsServer;
+
+    public void Dispose()
+    {
+        ModInformation.IsServer = wasServer;
+    }
+
+    [Fact]
+    public void Record_WhenPacketIsNull_ReturnsOnClientAndThrowsOnServer()
+    {
+        using var profiler = new PacketProfiler(TimeSpan.FromDays(1));
+
+        ModInformation.IsServer = false;
+        profiler.Record(null!, 1);
+
+        ModInformation.IsServer = true;
+        Assert.Throws<ArgumentNullException>(() => profiler.Record(null!, 1));
+    }
+}
