10acf1352866aaf38ade23386ee00068e752234a
georgyrudnev <georgy.rudnev@outlook.de>
2026-07-25 15:57:33 +0200

    Added unpause policy when no players are connected

--- source/Coop.Core/Server/Services/Players/Handlers/DisconnectedPlayersServerHandler.cs
+++ source/Coop.Core/Server/Services/Players/Handlers/DisconnectedPlayersServerHandler.cs
@@ -4,6 +4,7 @@ using Common.Network.Messages;
 using GameInterface.Services.Heroes.Enum;
 using GameInterface.Services.Heroes.Interaces;
 using GameInterface.Services.Players;
+using LiteNetLib.Utils;
 using System.Linq;
 
 namespace Coop.Core.Server.Services.Players.Handlers;
@@ -26,6 +27,7 @@ internal sealed class DisconnectedPlayersServerHandler : IHandler
         this.timeControlInterface = timeControlInterface;
 
         messageBroker.Subscribe<PlayerDisconnected>(HandlePlayerDisconnected);
+        timeControlInterface.AddUnpausePolicy(PlayersConnectedPolicy);
     }
 
     private void HandlePlayerDisconnected(MessagePayload<PlayerDisconnected> _)
@@ -42,8 +44,14 @@ internal sealed class DisconnectedPlayersServerHandler : IHandler
         }
     }
 
+    private bool PlayersConnectedPolicy()
+    {
+        return playerManager.Players.Any(playerManager.IsConnected);
+    }
+
     public void Dispose()
     {
         messageBroker.Unsubscribe<PlayerDisconnected>(HandlePlayerDisconnected);
+        timeControlInterface.RemoveUnpausePolicy(PlayersConnectedPolicy);
     }
 }
