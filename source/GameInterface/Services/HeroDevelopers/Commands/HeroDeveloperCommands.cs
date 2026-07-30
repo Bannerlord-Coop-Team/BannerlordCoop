a4d34d3ef4a880b611d5b5d038d245c9a5667202
jordanbrymora <jordanbrymora@gmail.com>
2026-07-30 20:35:57 +1000

    Couple more hero developer commands

--- source/GameInterface/Services/HeroDevelopers/Commands/HeroDeveloperCommands.cs
+++ source/GameInterface/Services/HeroDevelopers/Commands/HeroDeveloperCommands.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using static TaleWorlds.Library.CommandLineFunctionality;
 
 namespace GameInterface.Services.HeroDevelopers.Commands;
@@ -34,11 +35,69 @@ internal class HeroDeveloperCommands
         StringBuilder stringBuilder = new StringBuilder();
         foreach (var hero in Hero.AllAliveHeroes)
         {
-            if (hero.Name.ToString() == strings[0])
+            if (hero.Name.ToString() == strings[0] || hero.StringId == strings[0])
             {
                 hero.AddSkillXp(skillObject, xpGain);
 
                 stringBuilder.AppendLine($"{strings[0]} was given {xpGain} xp for {skillObject.Name}");
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    /// <summary>
+    /// Add attribute points to a hero.
+    /// Example:
+    /// coop.debug.herodeveloper.addattributepoints RandomPlayer 10
+    /// </summary>
+    [CommandLineArgumentFunction("addattributepoints", "coop.debug.herodeveloper")]
+    public static string AddAttributePointsCommand(List<string> strings)
+    {
+        if (ModInformation.IsClient) return "Command can only be run on the server.";
+
+        if (strings.Count != 2) return "Usage: addattributepoints heroName numPoints";
+
+        if (!int.TryParse(strings[1], out int numPoints)) return "An integer amount of attribute points is required.";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var hero in Hero.AllAliveHeroes)
+        {
+            if (hero.Name.ToString() == strings[0] || hero.StringId == strings[0])
+            {
+                // Use same implementation as vanilla command
+                hero.HeroDeveloper.UnspentAttributePoints = MBMath.ClampInt(hero.HeroDeveloper.UnspentAttributePoints + numPoints, 0, 10000);
+
+                stringBuilder.AppendLine($"{strings[0]} was given {numPoints} attribute points.");
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    /// <summary>
+    /// Add focus points to a hero.
+    /// Example:
+    /// coop.debug.herodeveloper.addfocuspoints RandomPlayer 10
+    /// </summary>
+    [CommandLineArgumentFunction("addfocuspoints", "coop.debug.herodeveloper")]
+    public static string AddFocusPointsCommand(List<string> strings)
+    {
+        if (ModInformation.IsClient) return "Command can only be run on the server.";
+
+        if (strings.Count != 2) return "Usage: addfocuspoints heroName numPoints";
+
+        if (!int.TryParse(strings[1], out int numPoints)) return "An integer amount of focus points is required.";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var hero in Hero.AllAliveHeroes)
+        {
+            if (hero.Name.ToString() == strings[0] || hero.StringId == strings[0])
+            {
+                // Use same implementation as vanilla command
+                hero.HeroDeveloper.UnspentFocusPoints = MBMath.ClampInt(hero.HeroDeveloper.UnspentFocusPoints + numPoints, 0, 10000);
+
+                stringBuilder.AppendLine($"{strings[0]} was given {numPoints} focus points.");
             }
         }
 
