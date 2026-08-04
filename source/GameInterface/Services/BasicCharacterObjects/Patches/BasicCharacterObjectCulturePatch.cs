77b1d5a8cce171437702bfec40c16e7372d8ca7b
jordanbrymora <jordanbrymora@gmail.com>
2026-08-05 02:14:40 +1000

    Fix null culture on player character objects

---
+++ source/GameInterface/Services/BasicCharacterObjects/Patches/BasicCharacterObjectCulturePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace GameInterface.Services.BasicCharacterObjects.Patches;
+
+[HarmonyPatch(nameof(BasicCharacterObject))]
+internal class BasicCharacterObjectCulturePatch
+{
+    [HarmonyPatch(nameof(BasicCharacterObject.Culture), MethodType.Getter)]
+    [HarmonyPostfix]
+    public static void CultureGetterPostfix(BasicCharacterObject __instance, ref BasicCultureObject __result)
+    {
+        if (__result != null) return;
+
+        if (__instance is not CharacterObject characterObject) return;
+
+        var heroCulture = characterObject.HeroObject?.Culture;
+        if (heroCulture == null) return;
+
+        // Cache _culture
+        __instance._culture = heroCulture;
+        __result = heroCulture;
+    }
+}
