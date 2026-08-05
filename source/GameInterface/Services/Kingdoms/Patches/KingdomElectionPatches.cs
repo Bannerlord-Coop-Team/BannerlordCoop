634b9bcce4b2caa9d5a9fa142ca677a5042610ee
NatteTosti69 <100526621+NatteTosti69@users.noreply.github.com>
2026-08-05 10:04:08 +0200

    update to policies: show correct and disable factiondiscontinuation for players rule for playercreated kingdoms,

---
+++ source/GameInterface/Services/Kingdoms/Patches/KingdomElectionPatches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Election;
+
+namespace GameInterface.Services.Kingdoms.Patches;
+
+[HarmonyPatch]
+internal class KingdomElectionPatches
+{
+    [HarmonyPatch(typeof(KingdomElection), nameof(KingdomElection.OnPlayerSupport))]
+    [HarmonyPrefix]
+    private static bool Prefix(KingdomElection __instance, DecisionOutcome decisionOutcome, Supporter.SupportWeights supportWeight)
+    {
+        bool isLocalPlayerChooser = __instance._chooser == Clan.PlayerClan;
+
+        if (!isLocalPlayerChooser)
+        {
+            foreach (DecisionOutcome outcome in __instance._possibleOutcomes)
+            {
+                outcome.ResetSupport(__instance.PlayerAsSupporter);
+            }
+            __instance._hasPlayerVoted = true;
+            if (decisionOutcome != null)
+            {
+                __instance.PlayerAsSupporter.SupportWeight = supportWeight;
+                decisionOutcome.AddSupport(__instance.PlayerAsSupporter);
+            }
+        }
+        else
+        {
+            __instance._chosenOutcome = decisionOutcome;
+        }
+
+        return false;
+    }
+}
