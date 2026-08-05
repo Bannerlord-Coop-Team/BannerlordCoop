c4bb282e142b384e3d7f6bc71fa641388c01fdb9
jordanbrymora <jordanbrymora@gmail.com>
2026-08-05 22:42:25 +1000

    Fix discarding items for xp

---
+++ source/GameInterface/Services/Inventory/Interfaces/DefaultItemDiscardModelInterface.cs
@@ -0,0 +1,60 @@
+﻿using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
+
+namespace GameInterface.Services.Inventory.Interfaces;
+
+public interface IDefaultItemDiscardModelInterface : IGameAbstraction
+{
+    /// <summary>
+    /// Replace vanilla implementation to include a playerParty argument.
+    /// Vanilla just uses the MainParty which doesn't work with multiple players.
+    /// </summary>
+    int GetXpBonusForDiscardingItem(MobileParty playerParty, ItemObject item, int amount = 0);
+}
+
+public class DefaultItemDiscardModelInterface : IDefaultItemDiscardModelInterface
+{
+    public int GetXpBonusForDiscardingItem(MobileParty playerParty, ItemObject item, int amount = 0)
+    {
+        if (!PlayerCanDonateItem(playerParty, item)) return 0;
+
+        int xpBonus;
+        switch (item.Tier)
+        {
+            case ItemObject.ItemTiers.Tier1:
+                xpBonus = 75;
+                break;
+            case ItemObject.ItemTiers.Tier2:
+                xpBonus = 150;
+                break;
+            case ItemObject.ItemTiers.Tier3:
+                xpBonus = 250;
+                break;
+            case ItemObject.ItemTiers.Tier4:
+            case ItemObject.ItemTiers.Tier5:
+            case ItemObject.ItemTiers.Tier6:
+                xpBonus = 300;
+                break;
+            default:
+                xpBonus = 35;
+                break;
+        }
+        return xpBonus * amount;
+    }
+
+    // Use playerParty to check for perks instead of MobileParty.MainParty
+    private bool PlayerCanDonateItem(MobileParty playerParty, ItemObject item)
+    {
+        bool result = false;
+        if (item.HasWeaponComponent)
+        {
+            result = playerParty.HasPerk(DefaultPerks.Steward.GivingHands, false);
+        }
+        else if (item.HasArmorComponent)
+        {
+            result = playerParty.HasPerk(DefaultPerks.Steward.PaidInPromise, true);
+        }
+        return result;
+    }
+}
