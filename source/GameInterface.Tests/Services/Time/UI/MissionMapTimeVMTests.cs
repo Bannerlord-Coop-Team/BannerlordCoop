12894aed6e79c3008b70819ded52a0c0e6632ffa
Dean <deanj.development@gmail.com>
2026-08-06 13:59:39 +0300

    addition of the mission map time view model and tests

---
+++ source/GameInterface.Tests/Services/Time/UI/MissionMapTimeVMTests.cs
@@ -0,0 +1,29 @@
+﻿using GameInterface.Services.Heroes.Enum;
+using GameInterface.Services.Time.UI;
+using Xunit;
+
+namespace GameInterface.Tests.Services.Time.UI;
+
+public class MissionMapTimeVMTests
+{
+    [Theory]
+    [InlineData(TimeControlEnum.Pause, "Map Time: Paused")]
+    [InlineData(TimeControlEnum.Play_1x, "Map Time: Normal")]
+    [InlineData(TimeControlEnum.Play_2x, "Map Time: Fast Forward")]
+    public void ConstructorSetsExpectedText(TimeControlEnum mode, string expectedText)
+    {
+        var viewModel = new MissionMapTimeVM(mode);
+        
+        Assert.Equal(expectedText, viewModel.MapTimeText);
+    }
+
+    [Fact]
+    public void SetTimeControlMode_UpdatesText()
+    {
+        var viewModel = new MissionMapTimeVM(TimeControlEnum.Pause);
+        
+        viewModel.SetTimeControlMode(TimeControlEnum.Play_2x);
+        
+        Assert.Equal("Map Time: Fast Forward", viewModel.MapTimeText);
+    }
+}
---
+++ source/GameInterface/Services/Time/UI/MissionMapTimeVM.cs
@@ -0,0 +1,43 @@
+﻿using GameInterface.Services.Heroes.Enum;
+using System;
+using TaleWorlds.Library;
+
+namespace GameInterface.Services.Time.UI;
+
+public sealed class MissionMapTimeVM : ViewModel
+{
+    private string mapTimeText;
+
+    [DataSourceProperty]
+    public string MapTimeText
+    {
+        get =>  mapTimeText;
+        private set
+        {
+            if (mapTimeText == value) return;
+            mapTimeText = value;
+            OnPropertyChanged(nameof(MapTimeText));
+        }
+    }
+
+    public MissionMapTimeVM(TimeControlEnum initialMode)
+    {
+        SetTimeControlMode(initialMode);
+    }
+
+    public void SetTimeControlMode(TimeControlEnum mode)
+    {
+        MapTimeText = $"Map Time: {GetModeText(mode)}";
+    }
+    
+    internal static string GetModeText(TimeControlEnum mode)
+    {
+        return mode switch
+        {
+            TimeControlEnum.Pause => "Paused",
+            TimeControlEnum.Play_1x => "Normal",
+            TimeControlEnum.Play_2x => "Fast Forward",
+            _ => throw new ArgumentOutOfRangeException(nameof(mode)),
+        };
+    }
+}
