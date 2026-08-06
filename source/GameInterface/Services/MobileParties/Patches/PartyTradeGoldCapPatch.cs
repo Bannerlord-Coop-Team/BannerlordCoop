fa4a073e0086b2097e18b4b5cdb27d6c3b8cc5b3
jordanbrymora <jordanbrymora@gmail.com>
2026-08-07 01:54:40 +1000

    Cap workshop caravan income for disconnected players

---
+++ source/GameInterface/Services/MobileParties/Patches/PartyTradeGoldCapPatch.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using GameInterface.Configuration;
+using GameInterface.Services.Heroes.Extensions;
+using GameInterface.Services.Players;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace GameInterface.Services.MobileParties.Patches;
+
+[HarmonyPatch(typeof(MobileParty))]
+internal class PartyTradeGoldCapPatch
+{
+    private static readonly int PartyTradeGoldIncomeThreshold = 10000;
+
+    [HarmonyPatch(nameof(MobileParty.PartyTradeGold), MethodType.Setter)]
+    [HarmonyPostfix]
+    public static void PartyTradeGoldSetterPostfix(MobileParty __instance)
+    {
+        if (__instance.Owner == null || !__instance.Owner.IsPlayerHero()) return;
+
+        ContainerProvider.TryResolve<IPlayerManager>(out var playerManager);
+
+        // Cap party trade gold for disconnected players based on config
+        // When capped at PartyTradeGoldIncomeThreshold (10000), rejoining players won't see a spike in caravan/party profits
+        if (ModInformation.IsServer
+            && !ModConfigProvider.ModOptions.GoldFoodInfluenceChangeForDisconnectedPlayers
+            && playerManager.IsOwnerOfHeroDisconnected(__instance.Owner)
+            && __instance.PartyTradeGold > PartyTradeGoldIncomeThreshold)
+        {
+            __instance.PartyTradeGold = PartyTradeGoldIncomeThreshold;
+        }
+    }
+}
---
+++ source/GameInterface/Services/Workshops/Patches/WorkshopCapitalPatch.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+using GameInterface.Configuration;
+using GameInterface.Services.Heroes.Extensions;
+using GameInterface.Services.Players;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.Settlements.Workshops;
+
+namespace GameInterface.Services.Workshops.Patches;
+
+[HarmonyPatch(typeof(Workshop))]
+internal class WorkshopCapitalPatch
+{
+    [HarmonyPatch(nameof(Workshop.ChangeGold))]
+    [HarmonyPostfix]
+    public static void ChangeGoldPostfix(Workshop __instance)
+    {
+        if (__instance.Owner == null || !__instance.Owner.IsPlayerHero()) return;
+
+        ContainerProvider.TryResolve<IPlayerManager>(out var playerManager);
+
+        // Cap workshop capital for disconnected players based on config
+        // When capped at InitialCapital (10000), rejoining players won't see a spike in workshop profits
+        if (ModInformation.IsServer
+            && !ModConfigProvider.ModOptions.GoldFoodInfluenceChangeForDisconnectedPlayers
+            && playerManager.IsOwnerOfHeroDisconnected(__instance.Owner)
+            && __instance.Capital > __instance.InitialCapital)
+        {
+            __instance.Capital = __instance.InitialCapital;
+        }
+    }
+}
