ff2b58b556e61ecd9a9fe6745e49a15a4521c4f6
Andrew Orlowski <57453461+uniqueuser-repo@users.noreply.github.com>
2026-08-11 08:17:21 -0500

    Add structured live test protocol
    Expose DEBUG command discovery and structured results so remote scenarios can validate their runtime contract before mutation. Track screenshot completion and endpoint registrations without suppressing authoritative patches.

--- source/GameInterface/Services/LiveTesting/LiveTestCommandDispatcher.cs
+++ source/GameInterface/Services/LiveTesting/LiveTestCommandDispatcher.cs
@@ -1,6 +1,8 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.ExceptionServices;
 using TaleWorlds.Library;
 
@@ -9,6 +11,10 @@ namespace GameInterface.Services.LiveTesting;
 public interface ILiveTestCommandDispatcher
 {
     bool EnsureReady();
+
+#if DEBUG
+    IReadOnlyList<string> GetCommands();
+#endif
 
     LiveTestCommandResult Execute(string command, List<string> arguments);
 }
@@ -38,6 +44,39 @@ public class LiveTestCommandDispatcher : ILiveTestCommandDispatcher
         exception?.Throw();
         return true;
     }
+
+#if DEBUG
+    public IReadOnlyList<string> GetCommands()
+    {
+        IReadOnlyList<string> commands = null;
+        ExceptionDispatchInfo exception = null;
+
+        GameThread.Run(() =>
+        {
+            try
+            {
+                EnsureFunctionsCollected();
+                commands = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(GetLoadableTypes)
+                    .SelectMany(GetStaticMethods)
+                    .SelectMany(GetCommandNames)
+                    .Where(command =>
+                        command.StartsWith(AllowedCommandPrefix, StringComparison.Ordinal) &&
+                        CommandLineFunctionality.HasFunctionForCommand(command))
+                    .Distinct(StringComparer.Ordinal)
+                    .OrderBy(command => command, StringComparer.Ordinal)
+                    .ToArray();
+            }
+            catch (Exception e)
+            {
+                exception = ExceptionDispatchInfo.Capture(e);
+            }
+        }, blocking: true);
+
+        exception?.Throw();
+        return commands;
+    }
+#endif
 
     public LiveTestCommandResult Execute(string command, List<string> arguments)
     {
@@ -78,6 +117,61 @@ public class LiveTestCommandDispatcher : ILiveTestCommandDispatcher
         CommandLineFunctionality.CollectCommandLineFunctions();
         functionsCollected = true;
     }
+
+#if DEBUG
+    private static IEnumerable<Type> GetLoadableTypes(Assembly assembly)
+    {
+        try
+        {
+            return assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException exception)
+        {
+            return exception.Types.Where(type => type != null);
+        }
+    }
+
+    private static IEnumerable<MethodInfo> GetStaticMethods(Type type)
+    {
+        try
+        {
+            return type.GetMethods(
+                BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.Static);
+        }
+        catch (Exception)
+        {
+            return Array.Empty<MethodInfo>();
+        }
+    }
+
+    private static IEnumerable<string> GetCommandNames(MethodInfo method)
+    {
+        try
+        {
+            var commands = new List<string>();
+            foreach (CustomAttributeData attribute in method.GetCustomAttributesData())
+            {
+                if (attribute.AttributeType != typeof(CommandLineFunctionality.CommandLineArgumentFunction) ||
+                    attribute.ConstructorArguments.Count < 2)
+                    continue;
+
+                string name = attribute.ConstructorArguments[0].Value as string;
+                string group = attribute.ConstructorArguments[1].Value as string;
+                if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(group))
+                    continue;
+
+                commands.Add(group + "." + name);
+            }
+            return commands;
+        }
+        catch (Exception)
+        {
+            return Array.Empty<string>();
+        }
+    }
+#endif
 }
 
 public class LiveTestCommandResult
