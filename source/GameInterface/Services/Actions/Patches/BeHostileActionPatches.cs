1b4c5b464f458849324c736fe5e2d4109495ffd2
jordanbrymora <jordanbrymora@gmail.com>
2026-08-12 15:07:05 +1000

    Patch BeHostileAction.ApplyEncounterHostileAction

---
+++ source/GameInterface/Services/Actions/Patches/BeHostileActionPatches.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using GameInterface.Services.MobileParties.Extensions;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace GameInterface.Services.Actions.Patches;
+
+[HarmonyPatch(typeof(BeHostileAction))]
+internal class BeHostileActionPatches
+{
+    [HarmonyPatch(nameof(BeHostileAction.ApplyEncounterHostileAction))]
+    [HarmonyPostfix]
+    public static void ApplyEncounterHostileActionPostfix(PartyBase attackerParty, PartyBase defenderParty)
+    {
+        if (ModInformation.IsClient) return;
+
+        // Re-do check for player parties
+        if (attackerParty.MobileParty != null && attackerParty.MobileParty.IsPlayerParty() && attackerParty.MapFaction != defenderParty.MapFaction && !FactionManager.IsAtWarAgainstFaction(attackerParty.MapFaction, defenderParty.MapFaction))
+        {
+            ChangeRelationAction.ApplyInternal(attackerParty.LeaderHero, defenderParty.MapFaction.Leader, -10, true, ChangeRelationAction.ChangeRelationDetail.Default);
+            DeclareWarAction.ApplyByPlayerHostility(attackerParty.MapFaction, defenderParty.MapFaction);
+        }
+    }
+}
