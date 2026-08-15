fbab3785a48cf350f8b8ed0064ea58ac1ed4fc3e
casey.johnson <onnesh1@gmail.com>
2026-08-15 15:44:34 -0700

    A static method LiveTestControlServer.IsEnabled was called, triggering static initialization of the Logger in LogManager before the logger configuration was completely setup. The fix is to move the block of code until after the log config was set up correctly. I verified that this shouldn't affect anything.

--- source/Coop/CoopMod.cs
+++ source/Coop/CoopMod.cs
@@ -108,12 +108,6 @@ namespace Coop
             }
 
             isAutoConnect = args.Any(a => a.Equals("/autoconnect", StringComparison.OrdinalIgnoreCase));
-#if DEBUG
-            isDeferredClientJoin = args.Any(a =>
-                    a.Equals("/cooptestmanualjoin", StringComparison.OrdinalIgnoreCase)) &&
-                LiveTestControlServer.IsEnabled(Environment.GetCommandLineArgs());
-            isLiveTestRun = LiveTestControlServer.IsEnabled(Environment.GetCommandLineArgs());
-#endif
 
             // GetFullCommandLineString splits on spaces, which would cut a quoted save
             // name apart; the managed-server arguments need real Windows arg parsing.
@@ -128,6 +122,13 @@ namespace Coop
 
             SetupLogging();
             InitializeCrashReporting();
+            
+#if DEBUG
+            isDeferredClientJoin = args.Any(a =>
+                                       a.Equals("/cooptestmanualjoin", StringComparison.OrdinalIgnoreCase)) &&
+                                   LiveTestControlServer.IsEnabled(Environment.GetCommandLineArgs());
+            isLiveTestRun = LiveTestControlServer.IsEnabled(Environment.GetCommandLineArgs());
+#endif
 
             // Creates the handler during launch
             if (!isServer)
