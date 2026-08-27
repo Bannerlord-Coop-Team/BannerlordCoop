3a2d89f45c898584d4618702b87e810b34f3aaa2
jordanbrymora <jordanbrymora@gmail.com>
2026-08-27 22:36:41 +1000

    Load heroes in settlement when connecting

---
+++ source/GameInterface/Services/Locations/Messages/NetworkRequestLocationRosterSnapshot.cs
@@ -0,0 +1,16 @@
+﻿using Common.Messaging;
+using ProtoBuf;
+
+namespace GameInterface.Services.Locations.Messages;
+
+[ProtoContract(SkipConstructor = true)]
+internal readonly struct NetworkRequestLocationRosterSnapshot : ICommand
+{
+    [ProtoMember(1)]
+    public readonly string SettlementId;
+
+    public NetworkRequestLocationRosterSnapshot(string settlementId)
+    {
+        SettlementId = settlementId;
+    }
+}
