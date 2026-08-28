d800f95380dba6008c2e22c8c084f286d4b0cf7d
Garrett Luskey <garrett.luskey@gmail.com>
2026-08-27 20:56:25 -0500

    Add bug report button visibility option

---
+++ source/GameInterface/Services/UI/CoopOptions/Providers/BugReportTab/BugReportOptionsTabProvider.cs
@@ -0,0 +1,41 @@
+﻿using Common.Messaging;
+using GameInterface.Configuration;
+using GameInterface.Services.UI.CoopOptions.Providers.BugReportTab.Sections;
+using System;
+
+namespace GameInterface.Services.UI.CoopOptions.Providers.BugReportTab;
+
+/// <summary>Provides the client bug-report presentation options tab.</summary>
+public class BugReportOptionsTabProvider : ICoopOptionsTabProvider
+{
+    public const string TabId = "BugReporting";
+    public const string TabName = "Bug Report";
+
+    public string Id => TabId;
+    public bool IsAvailable(ModOptions modOptions) => true;
+
+    public CoopOptionsTabVM CreateTab(
+        CoopOptionsData options,
+        IMessageBroker messageBroker,
+        Action<CoopOptionsTabVM> onSelect)
+    {
+        return new CoopOptionsTabVM(
+            Id,
+            TabName,
+            new CoopOptionsSectionVM[]
+            {
+                new BugReportSection(GetShowBugReportButtonOrDefault(options), messageBroker)
+            },
+            onSelect);
+    }
+
+    public static bool GetShowBugReportButtonOrDefault(CoopOptionsData options)
+    {
+        var sectionOptions =
+            (options ?? new CoopOptionsData()).GetSectionOrDefault(
+                TabId,
+                BugReportSection.SectionId,
+                new BugReportSectionOptions());
+        return sectionOptions.GetShowBugReportButtonOrDefault();
+    }
+}
---
+++ source/GameInterface/Services/UI/CoopOptions/Providers/BugReportTab/Sections/BugReportSectionOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace GameInterface.Services.UI.CoopOptions.Providers.BugReportTab.Sections;
+
+/// <summary>Persists the client's bug-report button visibility choice.</summary>
+public class BugReportSectionOptions
+{
+    public const bool DefaultShowBugReportButton = true;
+
+    public bool? ShowBugReportButton { get; set; }
+
+    public bool GetShowBugReportButtonOrDefault()
+    {
+        return ShowBugReportButton ?? DefaultShowBugReportButton;
+    }
+}
