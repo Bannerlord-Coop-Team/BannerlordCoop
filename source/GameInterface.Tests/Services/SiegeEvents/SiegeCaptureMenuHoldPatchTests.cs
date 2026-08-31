7b862c98b95c9c74f14d8933abd1ff66ab56b19d
gamingmenacers-svg <gamingmenacers@gmail.com>
2026-08-31 20:12:10 +0100

    Hold castle capture aftermath menu

---
+++ source/GameInterface.Tests/Services/SiegeEvents/SiegeCaptureMenuHoldPatchTests.cs
@@ -0,0 +1,26 @@
+﻿using GameInterface.Services.SiegeEvents.Patches;
+using HarmonyLib;
+using System.Linq;
+using System.Reflection;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using Xunit;
+
+namespace GameInterface.Tests.Services.SiegeEvents;
+
+/// <summary>Verifies capture aftermath holds cover every fortification entry menu.</summary>
+public class SiegeCaptureMenuHoldPatchTests
+{
+    [Theory]
+    [InlineData(nameof(EncounterGameMenuBehavior.game_menu_town_outside_on_init))]
+    [InlineData(nameof(EncounterGameMenuBehavior.game_menu_castle_outside_on_init))]
+    public void CaptureHold_PatchesEveryFortificationOutsideMenu(string methodName)
+    {
+        var patchedMethods = typeof(SiegeCaptureMenuHoldPatch)
+            .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+            .SelectMany(method => method.GetCustomAttributes<HarmonyPatch>())
+            .Where(patch => patch.info.declaringType == typeof(EncounterGameMenuBehavior))
+            .Select(patch => patch.info.methodName);
+
+        Assert.Contains(methodName, patchedMethods);
+    }
+}
