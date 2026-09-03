a2b3564a8cd152ae3ec3157364fa0a52fe07b68b
jordanbrymora <jordanbrymora@gmail.com>
2026-09-03 15:05:59 +1000

    Map trackers for new clan parties/caravans

---
+++ source/GameInterface/Services/UI/Patches/MapTrackerProviderPatches.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using Common.Messaging;
+using GameInterface.Services.UI.Messages;
+using HarmonyLib;
+using SandBox.ViewModelCollection.Map.Tracker;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace GameInterface.Services.UI.Patches;
+
+[HarmonyPatch(typeof(MapTrackerProvider))]
+internal class MapTrackerProviderPatches
+{
+    [HarmonyPatch(nameof(MapTrackerProvider.OnMobilePartyCreated))]
+    [HarmonyPrefix]
+    public static void OnMobilePartyCreatedPrefix(MapTrackerProvider __instance, MobileParty mobileParty)
+    {
+        if (ModInformation.IsClient) return;
+
+        MessageBroker.Instance.Publish(__instance, new MapTrackerPartyCreated(mobileParty));
+    }
+
+    [HarmonyPatch(nameof(MapTrackerProvider.OnPartyDisbanded))]
+    [HarmonyPrefix]
+    public static void OnPartyDisbandedPrefix(MapTrackerProvider __instance, MobileParty disbandedParty)
+    {
+        if (ModInformation.IsClient) return;
+
+        MessageBroker.Instance.Publish(__instance, new MapTrackerPartyRemoved(disbandedParty));
+    }
+
+    [HarmonyPatch(nameof(MapTrackerProvider.OnPartyDestroyed))]
+    [HarmonyPrefix]
+    public static void OnPartyDestroyedPrefix(MapTrackerProvider __instance, MobileParty mobileParty)
+    {
+        if (ModInformation.IsClient) return;
+
+        MessageBroker.Instance.Publish(__instance, new MapTrackerPartyRemoved(mobileParty));
+    }
+}
